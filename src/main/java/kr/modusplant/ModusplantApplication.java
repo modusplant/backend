30c68134c1b55f084240440be14168de59a62689
JunHyeok <akdnjs0308@gmail.com>
2025-03-06 22:06:06 +0900

    :sparkles: 브랜치 제어를 위한 최초 커밋

---
+++ src/main/java/kr/modusplant/ModusplantApplication.java
@@ -0,0 +1,13 @@
+package kr.modusplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModusplantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModusplantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/modusplant/ModusplantApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.modusplant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModusplantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
