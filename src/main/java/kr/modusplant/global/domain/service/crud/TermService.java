dc82731c65ff513c85da4ba6de751ec42fa5314a
JunHyeok <akdnjs0308@gmail.com>
2025-03-13 23:56:05 +0900

    MP-89 :goal_net: Catch: TermService가 UuidCrudService를 상속하도록 수정

--- src/main/java/kr/modusplant/global/domain/service/crud/TermService.java
+++ src/main/java/kr/modusplant/global/domain/service/crud/TermService.java
@@ -1,11 +1,11 @@
 package kr.modusplant.global.domain.service.crud;
 
 import kr.modusplant.global.domain.model.Term;
-import kr.modusplant.global.domain.service.crud.supers.CrudService;
+import kr.modusplant.global.domain.service.crud.supers.UuidCrudService;
 
 import java.util.Optional;
 
-public interface TermService extends CrudService<Term> {
+public interface TermService extends UuidCrudService<Term> {
     Optional<Term> getByName(String name);
 
     Optional<Term> getByVersion(String version);
