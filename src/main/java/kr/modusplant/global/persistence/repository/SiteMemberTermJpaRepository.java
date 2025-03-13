dfc136a885c68eba187fd72648e4839d54882390
JunHyeok <akdnjs0308@gmail.com>
2025-03-13 18:35:31 +0900

    MP-89 :sparkles: Feat: 리포지토리 계층 구현
    * supers 디렉토리는 중복되는 리포지토리 메소드를 통합하기 위한 상위 인터페이스를 담음
    
    * 구조는 다음의 2가지로 생각할 수 있음:
    1-1. CreatedAt에 대한 CreatedAtRepository
    1-2. LastModifiedAt에 대한 LastModifiedAtRepository
    1-3. 위의 두 인터페이스를 상속하는 CreatedAtAndLastModifiedAtRepository
    2. UUID를 PK로 쓰는 엔터티를 위한 UuidPrimaryKeyRepository

---
+++ src/main/java/kr/modusplant/global/persistence/repository/SiteMemberTermJpaRepository.java
@@ -0,0 +1,19 @@
+package kr.modusplant.global.persistence.repository;
+
+import kr.modusplant.global.persistence.entity.SiteMemberTermEntity;
+import kr.modusplant.global.persistence.repository.supers.LastModifiedAtRepository;
+import kr.modusplant.global.persistence.repository.supers.UuidPrimaryKeyRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface SiteMemberTermJpaRepository extends LastModifiedAtRepository<SiteMemberTermEntity>, UuidPrimaryKeyRepository<SiteMemberTermEntity>, JpaRepository<SiteMemberTermEntity, UUID> {
+    List<SiteMemberTermEntity> findByAgreedTermsOfUseVersion(String agreedTermsOfUseVersion);
+
+    List<SiteMemberTermEntity> findByAgreedPrivacyPolicyVersion(String agreedPrivacyPolicyVersion);
+
+    List<SiteMemberTermEntity> findByAgreedAdInfoReceivingVersion(String agreedAdInfoReceivingVersion);
+}
