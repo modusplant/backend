cf81e8a926ebc1856f9878b834f0c7f824567a3b
JunHyeok <akdnjs0308@gmail.com>
2025-03-13 20:53:50 +0900

    MP-89 :sparkles: Feat: CRUD 서비스 상위 인터페이스 구현
    * 도메인 디렉토리에 저장되며, Persistence 계층의 서비스는 이 서비스를 구현하여 CRUD 기능을 수행해야 함

---
+++ src/main/java/kr/modusplant/global/domain/service/crud/SiteMemberAuthService.java
@@ -0,0 +1,25 @@
+package kr.modusplant.global.domain.service.crud;
+
+import kr.modusplant.global.domain.model.SiteMember;
+import kr.modusplant.global.domain.model.SiteMemberAuth;
+import kr.modusplant.global.domain.service.crud.supers.UuidCrudService;
+import kr.modusplant.global.enums.AuthProvider;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SiteMemberAuthService extends UuidCrudService<SiteMemberAuth> {
+    List<SiteMemberAuth> getByActiveMember(SiteMember activeMember);
+
+    List<SiteMemberAuth> getByEmail(String email);
+
+    List<SiteMemberAuth> getByPw(String pw);
+
+    List<SiteMemberAuth> getByProvider(AuthProvider provider);
+
+    List<SiteMemberAuth> getByProviderId(String providerId);
+
+    List<SiteMemberAuth> getByFailedAttempt(String failedAttempt);
+
+    Optional<SiteMemberAuth> getByOriginalMember(SiteMember originalMember);
+}
