7ab6f4914a25f4c631bc050c61ad2f6e17e5856b
JunHyeok <akdnjs0308@gmail.com>
2025-03-13 23:50:11 +0900

    MP-89 :goal_net: Catch: TermService의 메소드가 Term을 반환하도록 수정

--- src/main/java/kr/modusplant/global/domain/service/crud/TermService.java
+++ src/main/java/kr/modusplant/global/domain/service/crud/TermService.java
@@ -2,12 +2,11 @@ package kr.modusplant.global.domain.service.crud;
 
 import kr.modusplant.global.domain.model.Term;
 import kr.modusplant.global.domain.service.crud.supers.CrudService;
-import kr.modusplant.global.persistence.entity.TermEntity;
 
 import java.util.Optional;
 
 public interface TermService extends CrudService<Term> {
-    Optional<TermEntity> getByName(String name);
+    Optional<Term> getByName(String name);
 
-    Optional<TermEntity> getByVersion(String version);
+    Optional<Term> getByVersion(String version);
 }
