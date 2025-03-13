86e1b02b03e3c0112efaa4159b92d4057b66196c
JunHyeok <akdnjs0308@gmail.com>
2025-03-14 00:16:36 +0900

    MP-89 :goal_net: Catch: TermJpaRepository와 TermService에서 Version과 관련한 메소드 반환 값 수정
    * TermService: getByVersion에 대해 Optional<Term> -> List<Term>
    * TermJpaRepository: findByVersion에 대해 Optional<Term> -> List<Term>

--- src/main/java/kr/modusplant/global/domain/service/crud/TermService.java
+++ src/main/java/kr/modusplant/global/domain/service/crud/TermService.java
@@ -3,10 +3,11 @@ package kr.modusplant.global.domain.service.crud;
 import kr.modusplant.global.domain.model.Term;
 import kr.modusplant.global.domain.service.crud.supers.UuidCrudService;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TermService extends UuidCrudService<Term> {
-    Optional<Term> getByName(String name);
+    List<Term> getByVersion(String version);
 
-    Optional<Term> getByVersion(String version);
+    Optional<Term> getByName(String name);
 }
