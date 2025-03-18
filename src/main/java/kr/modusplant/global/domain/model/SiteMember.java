0afdf190e94e07c0f7d75d200a2fc82de066b271
JunHyeok <akdnjs0308@gmail.com>
2025-03-19 01:11:59 +0900

    MP-89 :sparkles: Feat: 도메인 모델 빌더 메소드 보강
    * 각 도메인 모델에 대해서 클래스 자신의 타입을 매개변수의 타입으로 삼는 빌더 메소드 추가

--- src/main/java/kr/modusplant/global/domain/model/SiteMember.java
+++ src/main/java/kr/modusplant/global/domain/model/SiteMember.java
@@ -26,4 +26,31 @@ public class SiteMember {
     private final Boolean isDeleted;
 
     private final LocalDateTime loggedInAt;
+
+    public static class SiteMemberBuilder {
+        private UUID uuid;
+        private String nickname;
+        private LocalDate birthDate;
+        private Boolean isActive;
+        private Boolean isDisabledByLinking;
+        private Boolean isBanned;
+        private Boolean isDeleted;
+        private LocalDateTime loggedInAt;
+
+        public SiteMemberBuilder member(SiteMember member) {
+            this.uuid = member.getUuid();
+            this.nickname = member.getNickname();
+            this.birthDate = member.getBirthDate();
+            this.isActive = member.getIsActive();
+            this.isDisabledByLinking = member.getIsDisabledByLinking();
+            this.isBanned = member.getIsBanned();
+            this.isDeleted = member.getIsDeleted();
+            this.loggedInAt = member.getLoggedInAt();
+            return this;
+        }
+
+        public SiteMember build() {
+            return new SiteMember(this.uuid, this.nickname, this.birthDate, this.isActive, this.isDisabledByLinking, this.isBanned, this.isDeleted, this.loggedInAt);
+        }
+    }
 }
