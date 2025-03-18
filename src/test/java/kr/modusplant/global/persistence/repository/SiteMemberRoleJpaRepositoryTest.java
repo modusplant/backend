4915777907e9b1f70307d2125198e6f3e5b2bb96
JunHyeok <akdnjs0308@gmail.com>
2025-03-18 19:05:14 +0900

    MP-89 :white_check_mark: Test: 리포지토리 계층 테스트 구현
    * findBy, deleteBy, existsBy 메소드 테스트
    * 따로 save 자체를 테스트하지는 않음

---
+++ src/test/java/kr/modusplant/global/persistence/repository/SiteMemberRoleJpaRepositoryTest.java
@@ -0,0 +1,73 @@
+package kr.modusplant.global.persistence.repository;
+
+import kr.modusplant.global.persistence.entity.SiteMemberRoleEntity;
+import kr.modusplant.support.context.RepositoryOnlyContext;
+import kr.modusplant.support.util.SiteMemberRoleEntityTestUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RepositoryOnlyContext
+class SiteMemberRoleJpaRepositoryTest implements SiteMemberRoleEntityTestUtils {
+
+    private final SiteMemberRoleJpaRepository memberRoleRepository;
+
+    @Autowired
+    SiteMemberRoleJpaRepositoryTest(SiteMemberRoleJpaRepository memberRoleRepository) {
+        this.memberRoleRepository = memberRoleRepository;
+    }
+
+    @DisplayName("uuid로 회원 역할 찾기")
+    @Test
+    void findByUuidTest() {
+        // given
+        SiteMemberRoleEntity memberRole = createMemberRoleUserEntity();
+
+        // when
+        memberRoleRepository.save(memberRole);
+
+        // then
+        assertThat(memberRoleRepository.findByUuid(memberRole.getUuid()).orElseThrow()).isEqualTo(memberRole);
+    }
+
+    @DisplayName("role로 회원 역할 찾기")
+    @Test
+    void findByRoleTest() {
+        // given
+        SiteMemberRoleEntity memberRole = createMemberRoleUserEntity();
+
+        // when
+        memberRoleRepository.save(memberRole);
+
+        // then
+        assertThat(memberRoleRepository.findByRole(memberRole.getRole()).getFirst()).isEqualTo(memberRole);
+    }
+
+    @DisplayName("uuid로 회원 역할 삭제")
+    @Test
+    void deleteByUuidTest() {
+        // given
+        SiteMemberRoleEntity memberRole = memberRoleRepository.save(createMemberRoleUserEntity());
+
+        // when
+        memberRoleRepository.deleteByUuid(memberRole.getUuid());
+
+        // then
+        assertThat(memberRoleRepository.findAll()).isEmpty();
+    }
+
+    @DisplayName("uuid로 회원 역할 확인")
+    @Test
+    void existsByUuidTest() {
+        // given
+        SiteMemberRoleEntity memberRole = createMemberRoleUserEntity();
+
+        // when
+        memberRoleRepository.save(memberRole);
+
+        // then
+        assertThat(memberRoleRepository.existsByUuid(memberRole.getUuid())).isEqualTo(true);
+    }
+}
---
+++ src/test/java/kr/modusplant/global/persistence/repository/SiteMemberTermJpaRepositoryTest.java
@@ -0,0 +1,145 @@
+package kr.modusplant.global.persistence.repository;
+
+import kr.modusplant.global.persistence.entity.SiteMemberTermEntity;
+import kr.modusplant.global.persistence.entity.SiteMemberEntity;
+import kr.modusplant.support.context.RepositoryOnlyContext;
+import kr.modusplant.support.util.SiteMemberTermEntityTestUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RepositoryOnlyContext
+class SiteMemberTermJpaRepositoryTest implements SiteMemberTermEntityTestUtils {
+
+    private final SiteMemberTermJpaRepository memberTermRepository;
+    private final SiteMemberJpaRepository memberJpaRepository;
+
+    @Autowired
+    SiteMemberTermJpaRepositoryTest(SiteMemberTermJpaRepository memberTermRepository, SiteMemberJpaRepository memberJpaRepository) {
+        this.memberTermRepository = memberTermRepository;
+        this.memberJpaRepository = memberJpaRepository;
+    }
+
+    @DisplayName("uuid로 회원 약관 찾기")
+    @Test
+    void findByUuidTest() {
+        // given
+        SiteMemberTermEntity memberTerm = createMemberTermUserEntity();
+        SiteMemberEntity member = memberJpaRepository.save(memberTerm.getMember());
+
+        // when
+        memberTerm = memberTermRepository.save(SiteMemberTermEntity.builder()
+                .memberTermEntity(memberTerm)
+                .member(member)
+                .build());
+
+        // then
+        assertThat(memberTermRepository.findByUuid(memberTerm.getUuid()).orElseThrow()).isEqualTo(memberTerm);
+    }
+
+    @DisplayName("agreedTermsOfUseVersion으로 회원 약관 찾기")
+    @Test
+    void findByAgreedTermsOfUseVersionTest() {
+        // given
+        SiteMemberTermEntity memberTerm = createMemberTermUserEntity();
+        SiteMemberEntity member = memberJpaRepository.save(memberTerm.getMember());
+
+        // when
+        memberTerm = memberTermRepository.save(SiteMemberTermEntity.builder()
+                .memberTermEntity(memberTerm)
+                .member(member)
+                .build());
+
+        // then
+        assertThat(memberTermRepository.findByAgreedTermsOfUseVersion(memberTerm.getAgreedTermsOfUseVersion()).getFirst()).isEqualTo(memberTerm);
+    }
+
+    @DisplayName("agreedPrivacyPolicyVersion으로 회원 약관 찾기")
+    @Test
+    void findByAgreedPrivacyPolicyVersionTest() {
+        // given
+        SiteMemberTermEntity memberTerm = createMemberTermUserEntity();
+        SiteMemberEntity member = memberJpaRepository.save(memberTerm.getMember());
+
+        // when
+        memberTerm = memberTermRepository.save(SiteMemberTermEntity.builder()
+                .memberTermEntity(memberTerm)
+                .member(member)
+                .build());
+
+        // then
+        assertThat(memberTermRepository.findByAgreedPrivacyPolicyVersion(memberTerm.getAgreedPrivacyPolicyVersion()).getFirst()).isEqualTo(memberTerm);
+    }
+
+    @DisplayName("agreedAdInfoReceivingVersion으로 회원 약관 찾기")
+    @Test
+    void findByAgreedAdInfoReceivingVersionTest() {
+        // given
+        SiteMemberTermEntity memberTerm = createMemberTermUserEntity();
+        SiteMemberEntity member = memberJpaRepository.save(memberTerm.getMember());
+
+        // when
+        memberTerm = memberTermRepository.save(SiteMemberTermEntity.builder()
+                .memberTermEntity(memberTerm)
+                .member(member)
+                .build());
+
+        // then
+        assertThat(memberTermRepository.findByAgreedAdInfoReceivingVersion(memberTerm.getAgreedAdInfoReceivingVersion()).getFirst()).isEqualTo(memberTerm);
+    }
+
+    @DisplayName("lastModifiedAt으로 회원 약관 찾기")
+    @Test
+    void findByLastModifiedAtTest() {
+        // given
+        SiteMemberTermEntity memberTerm = createMemberTermUserEntity();
+        SiteMemberEntity member = memberJpaRepository.save(memberTerm.getMember());
+
+        // when
+        memberTerm = memberTermRepository.save(SiteMemberTermEntity.builder()
+                .memberTermEntity(memberTerm)
+                .member(member)
+                .build());
+
+        // then
+        assertThat(memberTermRepository.findByLastModifiedAt(memberTerm.getLastModifiedAt()).getFirst()).isEqualTo(memberTerm);
+    }
+
+    @DisplayName("uuid로 회원 약관 삭제")
+    @Test
+    void deleteByUuidTest() {
+        // given
+        SiteMemberTermEntity memberTerm = createMemberTermUserEntity();
+        SiteMemberEntity member = memberJpaRepository.save(memberTerm.getMember());
+        memberTerm = memberTermRepository.save(SiteMemberTermEntity.builder()
+                .memberTermEntity(memberTerm)
+                .member(member)
+                .build());
+
+        // when
+        memberTermRepository.deleteByUuid(memberTerm.getUuid());
+
+        // then
+        assertThat(memberTermRepository.findAll()).isEmpty();
+    }
+
+    @DisplayName("uuid로 회원 약관 확인")
+    @Test
+    void existsByUuidTest() {
+        // given
+        SiteMemberTermEntity memberTerm = createMemberTermUserEntity();
+        SiteMemberEntity member = memberJpaRepository.save(memberTerm.getMember());
+        memberTerm = memberTermRepository.save(SiteMemberTermEntity.builder()
+                .memberTermEntity(memberTerm)
+                .member(member)
+                .build());
+
+        // when
+        memberTermRepository.save(memberTerm);
+
+        // then
+        assertThat(memberTermRepository.existsByUuid(memberTerm.getUuid())).isEqualTo(true);
+    }
+}
