d70882726c5aa7ff83e4b4b1acb61b9046df98aa
Kormap <kodh10@gmail.com>
2025-03-20 00:26:10 +0900

    MP-57 :bug: Fix: AccessToken 저장방식 수정

--- src/main/java/kr/modusplant/api/crud/model/request/VerifyEmailRequest.java
+++ src/main/java/kr/modusplant/api/crud/model/request/VerifyEmailRequest.java
@@ -5,9 +5,6 @@ import lombok.Getter;
 
 @Getter
 public class VerifyEmailRequest {
-    @Schema(description = "액세스 토큰", example = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
-    String accessToken;
-
     @Schema(description = "검증 코드", example = "123456")
     String verifyCode;
 }
