0fc837185fef0536b04e29508f34deb2f482ad37
JunHyeok <akdnjs0308@gmail.com>
2025-03-23 18:51:52 +0900

    MP-89 :white_check_mark: Test: prePersist와 preUpdate 테스트 코드 작성
    * 단, SiteMemberRoleEntity의 preUpdate 메소드는 작성 불가
    * @MapsId를 통해 PK를 불러오는 엔터티의 경우에는 반드시 불러온(findBy) 엔터티의 필드를 Setter를 통해 직접 변경하는 경우에만 필드 갱신이 가능하기 때문
    * 왜냐하면 @MapsId를 사용하는 경우에는 단순한 1:1 또는 1:N 관계가 아니라 "공유되는 PK 관계"가 성립되기 때문임

---
+++ src/test/java/kr/modusplant/global/persistence/entity/SiteMemberAuthEntityTest.java
@@ -0,0 +1,52 @@
+package kr.modusplant.global.persistence.entity;
+
+import kr.modusplant.support.context.RepositoryOnlyContext;
+import kr.modusplant.support.util.entity.SiteMemberAuthEntityTestUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RepositoryOnlyContext
+class SiteMemberAuthEntityTest implements SiteMemberAuthEntityTestUtils {
+
+    private final TestEntityManager entityManager;
+
+    @Autowired
+    SiteMemberAuthEntityTest(TestEntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @DisplayName("회원 인증 PrePersist")
+    @Test
+    void prePersist() {
+        // given
+        SiteMemberEntity member = createMemberBasicUserEntity();
+        SiteMemberAuthEntity memberAuth = createMemberAuthBasicUserEntityBuilder().activeMember(member).originalMember(member).failedAttempt(1).build();
+
+        // when
+        entityManager.persist(memberAuth);
+        entityManager.flush();
+
+        // then
+        assertThat(memberAuth.getFailedAttempt()).isEqualTo(1);
+    }
+
+    @DisplayName("회원 인증 PreUpdate")
+    @Test
+    void preUpdate() {
+        // given
+        SiteMemberEntity member = createMemberBasicUserEntity();
+        SiteMemberAuthEntity memberAuth = createMemberAuthBasicUserEntityBuilder().activeMember(member).originalMember(member).build();
+        entityManager.persist(memberAuth);
+
+        // when
+        entityManager.merge(SiteMemberAuthEntity.builder().memberAuthEntity(memberAuth).failedAttempt(null).build());
+        entityManager.flush();
+
+        // then
+        assertThat(memberAuth.getFailedAttempt()).isEqualTo(0);
+    }
+}
