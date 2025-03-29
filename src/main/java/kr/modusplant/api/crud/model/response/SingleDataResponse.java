25e0d1f316e62ca4a6983cea2a654c9fb3f82e33
songu1 <syj0002012@naver.com>
2025-03-29 16:38:21 +0900

    MP-62 :recycle: Refactor: Metadata 레코드 적용 및 기존 Metadata 객체 제거
    - SingleDataResponse에서 Metadata 레코드를 사용하도록 변경
    - Metadata 레코드 사용으로 인해 기존 Metadata dto 삭제

--- src/main/java/kr/modusplant/api/crud/model/response/SingleDataResponse.java
+++ src/main/java/kr/modusplant/api/crud/model/response/SingleDataResponse.java
@@ -2,6 +2,7 @@ package kr.modusplant.api.crud.model.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
+import kr.modusplant.global.app.servlet.response.Metadata;
 
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -12,11 +13,7 @@ public class SingleDataResponse<T> {
     // 팩토리 메서드
     public static <T> SingleDataResponse<T> success(int status, String message, T data) {
         SingleDataResponse<T> response = new SingleDataResponse<>();
-        Metadata metadata = new Metadata();
-        metadata.setStatus(status);
-        metadata.setMessage(message);
-
-        response.metadata = metadata;
+        response.metadata = new Metadata(status, message);
         response.data = data;
 
         return response;
@@ -24,11 +21,7 @@ public class SingleDataResponse<T> {
 
     public static SingleDataResponse<Void> fail(int status, String message) {
         SingleDataResponse<Void> response = new SingleDataResponse<>();
-        Metadata metadata = new Metadata();
-        metadata.setStatus(status);
-        metadata.setMessage(message);
-
-        response.metadata = metadata;
+        response.metadata = new Metadata(status, message);
 
         return response;
     }
