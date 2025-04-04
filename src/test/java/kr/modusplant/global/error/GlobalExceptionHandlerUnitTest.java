4b26d30da6ae998280e7a5bccbaadf759f3c9d1f
Jun Hee <umpa9327@gmail.com>
2025-04-04 19:39:55 +0900

    MP-92 :white_check_mark: Test: 전역적 예외 처리 클래스에 대한 테스트 구현
    - GlobalExceptionHandlerUnitTest 클래스 도입

---
+++ src/test/java/kr/modusplant/global/error/GlobalExceptionHandlerUnitTest.java
@@ -0,0 +1,54 @@
+package kr.modusplant.global.error;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class GlobalExceptionHandlerUnitTest {
+
+    @InjectMocks
+    private GlobalExceptionHandler globalExceptionHandler;
+
+    @Test
+    public void handleValidationException_givenValidCondition_thenReturnProblemDetail() {
+        // given
+        MethodArgumentNotValidException ex = mock(MethodArgumentNotValidException.class);
+        BindingResult bindingResult = mock(BindingResult.class);
+
+        List<FieldError> fieldErrorList = new ArrayList<>();
+        fieldErrorList.add(new FieldError("SiteMemberEntity", "isActive", "isActive must not be null"));
+        fieldErrorList.add(new FieldError("SiteMemberEntity", "nickname", "nickname must not be null"));
+
+        given(ex.getBindingResult()).willReturn(bindingResult);
+        given(bindingResult.getFieldErrors()).willReturn(fieldErrorList);
+
+        // when
+        ResponseEntity<ProblemDetail> response = globalExceptionHandler.handleValidationException(ex);
+        ProblemDetail problemDetail = response.getBody();
+
+        // then
+        assertNotNull(problemDetail);
+        assertEquals(HttpStatus.BAD_REQUEST.value(), problemDetail.getStatus());
+        assertEquals("Invalid client data", problemDetail.getTitle());
+        assertEquals(URI.create("about:blank"), problemDetail.getType());
+        assertEquals("required property missing, invalid format, constraint violation, etc", problemDetail.getDetail());
+    }
+}
