470e4508ba0c9fe184f4c90bc36cbee5dc583816
Jun Hee <umpa9327@gmail.com>
2025-04-15 15:50:14 +0900

    MP-92 :sparkles: Feat: 무효한 인자로 인한 예외, 무효한 상태 예외에 대한 예외 핸들링 메서드 추가
    - IllegalArgumentException과 IlegalStateException을 처리하는 예외 처리 메서드 추가

--- src/main/java/kr/modusplant/global/error/GlobalExceptionHandler.java
+++ src/main/java/kr/modusplant/global/error/GlobalExceptionHandler.java
@@ -69,6 +69,22 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 
+    // 메서드의 인자가 무효한 값일 경우 처리
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ProblemDetail> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problemDetail.setTitle("Invalid method argument");
+        Optional.ofNullable(ex.getMessage()).ifPresent(value -> problemDetail.setDetail(ex.getMessage()));
+        return ResponseEntity.badRequest().body(problemDetail);
+    }
+
+    // 호출된 메서드가 정상적으로 작동할 수 없는 경우 처리
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<DataResponse<Void>> handleIllegalStateException(IllegalStateException ex) {
+        DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.CONFLICT.value(), "resource is not available");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
+    }
+
     // JSON 매핑 요청 처리
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<DataResponse<Void>> handleMalformedJsonException(HttpMessageNotReadableException ex) {
