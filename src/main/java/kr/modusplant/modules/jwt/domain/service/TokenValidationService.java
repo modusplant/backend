f2b6e3767260cc57963104373340e76a8b78d570
songu1 <syj0002012@naver.com>
2025-04-18 08:09:15 +0900

    MP-101  :sparkles: Feat: JWT 토큰 관련 검증 서비스 추가
    - 토큰 관련 서비스 및 입력값 검증을 위해 검증 서비스 클래스 구현

---
+++ src/main/java/kr/modusplant/modules/jwt/domain/service/TokenValidationService.java
@@ -0,0 +1,47 @@
+package kr.modusplant.modules.jwt.domain.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import kr.modusplant.domains.member.persistence.entity.SiteMemberEntity;
+import kr.modusplant.domains.member.persistence.repository.SiteMemberCrudJpaRepository;
+import kr.modusplant.modules.jwt.domain.service.supers.RefreshTokenCrudService;
+import kr.modusplant.modules.jwt.persistence.entity.RefreshTokenEntity;
+import kr.modusplant.modules.jwt.persistence.repository.RefreshTokenJpaRepository;
+import kr.modusplant.global.error.EntityNotFoundWithUuidException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static kr.modusplant.global.util.ExceptionUtils.getFormattedExceptionMessage;
+import static kr.modusplant.global.vo.ExceptionMessage.NOT_FOUND_ENTITY;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class TokenValidationService {
+
+    private final RefreshTokenCrudService refreshTokenCrudService;
+    private final RefreshTokenJpaRepository tokenRepository;
+    private final SiteMemberCrudJpaRepository memberRepository;
+
+    public boolean validateNotFoundRefreshToken(String refreshToken) {
+        return refreshTokenCrudService.getByRefreshToken(refreshToken).isEmpty();
+    }
+
+    public boolean validateExistedDeviceId(UUID deviceId) {
+        return refreshTokenCrudService.getByDeviceId(deviceId).isPresent();
+    }
+
+    public void validateNotFoundMemberUuid(String name, UUID memberUuid) {
+        if (memberUuid == null || memberRepository.findByUuid(memberUuid).isEmpty()) {
+            throw new EntityNotFoundException(getFormattedExceptionMessage(NOT_FOUND_ENTITY, name, memberUuid, SiteMemberEntity.class));
+        }
+    }
+
+    public void validateNotFoundTokenUuid(UUID uuid) {
+        if (uuid == null || tokenRepository.findByUuid(uuid).isEmpty()) {
+            throw new EntityNotFoundWithUuidException(uuid, RefreshTokenEntity.class);
+        }
+    }
+}
