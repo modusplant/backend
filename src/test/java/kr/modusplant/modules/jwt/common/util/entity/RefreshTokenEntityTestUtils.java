648a8d0e37a6875c45ecfb59e9116befb3eb08b2
songu1 <syj0002012@naver.com>
2025-04-18 07:24:22 +0900

    MP-101 :white_check_mark: Test: JWT TestUtils 구현
    - 테스트 시나리오 작성이 용이하도록 테스트용 JWT 토큰 생성 메서드 구현

---
+++ src/test/java/kr/modusplant/modules/jwt/common/util/entity/RefreshTokenEntityTestUtils.java
@@ -0,0 +1,25 @@
+package kr.modusplant.modules.jwt.common.util.entity;
+
+import kr.modusplant.domains.member.common.util.entity.SiteMemberEntityTestUtils;
+import kr.modusplant.modules.jwt.common.util.domain.RefreshTokenTestUtils;
+import kr.modusplant.modules.jwt.persistence.entity.RefreshTokenEntity.RefreshTokenEntityBuilder;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+import static kr.modusplant.modules.jwt.persistence.entity.RefreshTokenEntity.builder;
+
+public interface RefreshTokenEntityTestUtils extends SiteMemberEntityTestUtils {
+    default RefreshTokenEntityBuilder createRefreshTokenBasicEntityBuilder() {
+        return builder()
+                .deviceId(RefreshTokenTestUtils.refreshTokenBasicUser.getDeviceId())
+                .refreshToken(RefreshTokenTestUtils.refreshTokenBasicUser.getRefreshToken())
+                .issuedAt(convertToLocalDateTime(RefreshTokenTestUtils.refreshTokenBasicUser.getIssuedAt()))
+                .expiredAt(convertToLocalDateTime(RefreshTokenTestUtils.refreshTokenBasicUser.getExpiredAt()));
+    }
+
+    private static LocalDateTime convertToLocalDateTime(Date date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneOffset.UTC);
+    }
+}
