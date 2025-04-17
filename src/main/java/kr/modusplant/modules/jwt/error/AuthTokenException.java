077a982eb6b824dcefbbfa620b528dc862dbe5c8
songu1 <syj0002012@naver.com>
2025-04-18 08:07:36 +0900

    MP-101  :sparkles: Feat: JWT 인증 예외 클래스 추가
    - 예외 처리를 위한 비대칭키 생성, 토큰 생성 및 검증과 관련된 예외 클래스 생성

---
+++ src/main/java/kr/modusplant/modules/jwt/error/AuthTokenException.java
@@ -0,0 +1,14 @@
+package kr.modusplant.modules.jwt.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class AuthTokenException extends RuntimeException {
+    private final HttpStatus status;
+
+    protected AuthTokenException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+}
