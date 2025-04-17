e062ec8a9ee29e9de2e81cfc4c976f911ee39758
songu1 <syj0002012@naver.com>
2025-04-18 07:32:14 +0900

    MP-101 :white_check_mark: Test: RefreshTokenEntityMapper의 매핑 일관성 테스트 추가
    - 엔티티 → 도메인 → 엔티티 변환 후 일관성 검증
    - 도메인 → 엔티티 → 도메인 변환 후 일관성 검증

---
+++ src/test/java/kr/modusplant/modules/jwt/mapper/entity/RefreshTokenEntityMapperTest.java
@@ -0,0 +1,64 @@
+package kr.modusplant.modules.jwt.mapper.entity;
+
+import kr.modusplant.modules.jwt.common.util.domain.RefreshTokenTestUtils;
+import kr.modusplant.modules.jwt.common.util.entity.RefreshTokenEntityTestUtils;
+import kr.modusplant.domains.member.persistence.entity.SiteMemberEntity;
+import kr.modusplant.domains.member.persistence.repository.SiteMemberCrudJpaRepository;
+import kr.modusplant.modules.jwt.domain.model.RefreshToken;
+import kr.modusplant.modules.jwt.persistence.entity.RefreshTokenEntity;
+import kr.modusplant.modules.jwt.persistence.repository.RefreshTokenJpaRepository;
+import kr.modusplant.global.context.RepositoryOnlyContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RepositoryOnlyContext
+class RefreshTokenEntityMapperTest implements RefreshTokenTestUtils, RefreshTokenEntityTestUtils {
+
+    private final SiteMemberCrudJpaRepository memberRepository;
+    private final RefreshTokenJpaRepository refreshTokenRepository;
+    private final RefreshTokenEntityMapper refreshTokenMapper = new RefreshTokenEntityMapperImpl();
+
+    @Autowired
+    RefreshTokenEntityMapperTest(SiteMemberCrudJpaRepository memberRepository, RefreshTokenJpaRepository refreshTokenRepository) {
+        this.memberRepository = memberRepository;
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    @Test
+    @DisplayName("매퍼 적용 후 일관된 refresh token 엔티티 확인")
+    void checkConsistentEntity() {
+        // given
+        SiteMemberEntity memberEntity = memberRepository.save(createMemberBasicUserEntity());
+
+        // when
+        RefreshTokenEntity refreshTokenEntity = refreshTokenRepository.save(
+                createRefreshTokenBasicEntityBuilder()
+                        .member(memberEntity)
+                        .build()
+        );
+
+        // then
+        assertThat(refreshTokenEntity).isEqualTo(refreshTokenMapper.createRefreshTokenEntity(refreshTokenMapper.toRefreshToken(refreshTokenEntity), memberRepository));
+    }
+
+    @Test
+    @DisplayName("매퍼 적용 후 일관된 refresh token 도메인 확인")
+    void checkConsistentDomain() {
+        // given
+        SiteMemberEntity memberEntity = memberRepository.save(createMemberBasicUserEntity());
+        RefreshTokenEntity refreshTokenEntity = refreshTokenRepository.save(
+                createRefreshTokenBasicEntityBuilder()
+                        .member(memberEntity)
+                        .build()
+        );
+
+        // when
+        RefreshToken refreshToken = refreshTokenMapper.toRefreshToken(refreshTokenEntity);
+
+        // then
+        assertThat(refreshToken).isEqualTo(refreshTokenMapper.toRefreshToken(refreshTokenMapper.updateRefreshTokenEntity(refreshToken, memberRepository)));
+    }
+}
