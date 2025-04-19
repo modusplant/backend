2b7181d1b7e4754830b2e8a4c4b421f503e545f7
songu1 <syj0002012@naver.com>
2025-04-19 14:05:54 +0900

    MP-151 :sparkles: Feat: 소셜 로그인 기능에 JWT 적용
    - SocialAuthController에서 JWT 발행 로직 추가 및 토큰 응답 처리
    - SocialLoginRequest에 deviceId 필드 추가
    - 기존 소셜 로그인 전용 TokenResponse 제거, JWT 기반 TokenResponse로 대체

--- src/main/java/kr/modusplant/modules/auth/social/model/request/SocialLoginRequest.java
+++ src/main/java/kr/modusplant/modules/auth/social/model/request/SocialLoginRequest.java
@@ -1,10 +1,19 @@
 package kr.modusplant.modules.auth.social.model.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+
+import java.util.UUID;
 
 @Getter
 public class SocialLoginRequest {
     @Schema(description = "소셜 로그인 인가 코드", example = "BPAlKjanydCLdDnYdib6MQpDRwPG7hgqgWwECDwlr_jVWR6WpNeIbGlpBKIKKiVOAAABjE6Zt5qBPKUF0hG4dQ")
+    @NotNull(message = "code는 필수 값입니다")
     private String code;
+    @Schema(description = "기기 id", example = "241b327b-30ed-4f06-ba6a-cc0c3cf3b5a4")
+    @NotNull(message = "deviceId는 필수 값입니다.")
+    @JsonProperty("device_id")
+    private UUID deviceId;
 }
