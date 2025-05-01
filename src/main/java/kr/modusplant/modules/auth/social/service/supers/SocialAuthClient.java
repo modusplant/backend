6488b748a9abf35616c6e1d609a883e3e2a2964e
songu1 <syj0002012@naver.com>
2025-05-02 06:25:29 +0900

    MP-151 :truck: Rename: 소셜 로그인 파일 이동
    - 패키지 구조 변경에 따라 기존 파일 이동

--- src/main/java/kr/modusplant/modules/auth/social/service/supers/SocialAuthClient.java
+++
@@ -1,8 +0,0 @@
-package kr.modusplant.modules.auth.social.service.supers;
-
-import kr.modusplant.modules.auth.social.dto.supers.SocialUserInfo;
-
-public interface SocialAuthClient {
-    String getAccessToken(String code);
-    SocialUserInfo getUserInfo(String accessToken);
-}
