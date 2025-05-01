50e0a9c48ede013381cc11f2691e9191c3e5b09c
songu1 <syj0002012@naver.com>
2025-05-02 06:25:29 +0900

    MP-151 :truck: Rename: 소셜 로그인 파일 이동
    - 패키지 구조 변경에 따라 기존 파일 이동

--- src/main/java/kr/modusplant/modules/auth/social/app/http/request/SocialLoginRequest.java
+++ src/main/java/kr/modusplant/modules/auth/social/app/http/request/SocialLoginRequest.java
@@ -1,4 +1,4 @@
-package kr.modusplant.modules.auth.social.model.request;
+package kr.modusplant.modules.auth.social.app.http.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
