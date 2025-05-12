ad8bc9488752fcc34969b212daf9695b853a696a
JunHyeok <akdnjs0308@gmail.com>
2025-05-12 16:42:59 +0900

    MP-166 :recycle: Refactor: kr.modusplant.modules.auth.email 패키지 코드 세부 조정
    * Jwts.builder()와 Jwts.parser()에 대한 연쇄 메소드를 현대적으로 변경
    * 사용되지 않는 payloadEmail 변수 제거
    * SameSite 속성 값을 Lax로 변경
    * 응답을 정적 팩토리 메소드를 사용하여 더욱 깔끔하게 변경

--- src/main/java/kr/modusplant/global/vo/CamelCaseWord.java
+++ src/main/java/kr/modusplant/global/vo/CamelCaseWord.java
@@ -8,8 +8,10 @@ public final class CamelCaseWord {
     public static final String ACTIVE_MEMBER = "activeMember";
     public static final String ACTIVE_MEMBER_UUID = "activeMemberUuid";
     public static final String BIRTH_DATE = "birthDate";
+    public static final String EMAIL = "email";
     public static final String EXPIRED_AT = "expiredAt";
     public static final String FAILED_ATTEMPT = "failedAttempt";
+    public static final String ISSUED_AT = "issuedAt";
     public static final String IS_ACTIVE = "isActive";
     public static final String IS_BANNED = "isBanned";
     public static final String IS_DELETED = "isDeleted";
@@ -18,14 +20,14 @@ public final class CamelCaseWord {
     public static final String LOCKOUT_UNTIL = "lockoutUntil";
     public static final String LOGGED_IN_AT = "loggedInAt";
     public static final String MEMBER = "member";
+    public static final String MEMBER_AUTH = "memberAuth";
     public static final String MEMBER_ROLE_ENTITY = "memberRoleEntity";
     public static final String MEMBER_TERM_ENTITY = "memberTermEntity";
+    public static final String MEMBER_UUID = "memberUuid";
     public static final String NAME = "name";
     public static final String ORIGINAL_MEMBER = "originalMember";
     public static final String ORIGINAL_MEMBER_UUID = "originalMemberUuid";
     public static final String TERM = "term";
     public static final String VER = "ver";
-    public static final String MEMBER_UUID = "memberUuid";
-    public static final String ISSUED_AT = "issuedAt";
-    public static final String MEMBER_AUTH = "memberAuth";
+    public static final String VERIFY_CODE = "verifyCode";
 }
