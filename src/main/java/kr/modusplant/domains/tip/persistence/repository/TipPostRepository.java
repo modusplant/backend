74e2de5d989d8aeb5e83d8da97b833150ce46659
songu1 <syj0002012@naver.com>
2025-05-17 21:22:57 +0900

    MP-110 :sparkles: Feat: 팁 게시글 persistence 계층 구현
    * 팁 게시글 기능을 위한 TipPostEntity 클래스 구현
    - `@ULID` 커스텀 어노테이션을 적용하여 고유 식별자 생성
    - Hypersistence Utils JSON 타입을 사용하여 `content` 필드를 JSONB 타입으로 저장
    
    * 팁 게시글 데이터 접근을 위한 TipPostRepository 및 상위 인터페이스 구현
    - UpdatedAt에 대한 UpdatedAtRepository 생성
    -  생성 및 업데이트 일자 동시 관리를 위한 `CreatedAtAndUpdatedAtRepository` 구현 (상속: `CreatedAtRepository`, `UpdatedAtRepository`)
    - ULID 기반 엔티티를 위한 UlidPrimaryRepository 생성

---
+++ src/main/java/kr/modusplant/domains/tip/persistence/repository/TipPostRepository.java
@@ -0,0 +1,25 @@
+package kr.modusplant.domains.tip.persistence.repository;
+
+import kr.modusplant.domains.common.persistence.repository.supers.CreatedAtAndUpdatedAtRepository;
+import kr.modusplant.domains.common.persistence.repository.supers.UlidPrimaryRepository;
+import kr.modusplant.domains.group.persistence.entity.PlantGroupEntity;
+import kr.modusplant.domains.member.persistence.entity.SiteMemberEntity;
+import kr.modusplant.domains.tip.persistence.entity.TipPostEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public interface TipPostRepository extends UlidPrimaryRepository<TipPostEntity>, CreatedAtAndUpdatedAtRepository<TipPostEntity>, JpaRepository<TipPostEntity,String> {
+    Page<TipPostEntity> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    Page<TipPostEntity> findAllByIsDeletedFalseOrderByCreatedAtDesc(Pageable pageable);
+
+    Page<TipPostEntity> findByGroupAndIsDeletedFalseOrderByCreatedAtDesc(PlantGroupEntity group, Pageable pageable);
+
+    Page<TipPostEntity> findByAuthMemberAndIsDeletedFalseOrderByCreatedAtDesc(SiteMemberEntity authMember, Pageable pageable);
+
+}
