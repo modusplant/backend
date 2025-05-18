9132beebe1841f715a57a2eb7e1d35a6071ce752
Jun Hee <umpa9327@gmail.com>
2025-05-18 22:59:44 +0900

    MP-114 :goal_net: Catch: 팁 댓글의 복합 기본키 유틸리티 클래스 수정
    - 테스트 데이터의 일관성을 위해 ulid 값을 팁 게시글과 맞춤
    - 인자 없이 인스턴스를 생성하는 유틸리티 메서드 삭제

--- src/test/java/kr/modusplant/domains/tip/common/util/entity/compositekey/TipCommentCompositeKeyTestUtils.java
+++ src/test/java/kr/modusplant/domains/tip/common/util/entity/compositekey/TipCommentCompositeKeyTestUtils.java
@@ -2,12 +2,10 @@ package kr.modusplant.domains.tip.common.util.entity.compositekey;
 
 import kr.modusplant.domains.tip.persistence.entity.compositekey.TipCommentCompositeKey;
 
+import static kr.modusplant.domains.tip.common.util.domain.TipPostTestUtils.tipPostWithUlid;
+
 public interface TipCommentCompositeKeyTestUtils {
     default TipCommentCompositeKey createCompositeKeyWithAllArgs() {
-        return new TipCommentCompositeKey("01H5Z7XQ3W4F9K2G1V8R6T0Y5P","/1/6/2");
-    }
-
-    default TipCommentCompositeKey createCompositeKeyWithNoArgs() {
-        return new TipCommentCompositeKey();
+        return new TipCommentCompositeKey(tipPostWithUlid.getUlid(),"/1/6/2");
     }
 }
