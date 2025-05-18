dc66a478e5da0e2852b0dcbaa570ce6cec9b0ac2
JunHyeok <akdnjs0308@gmail.com>
2025-05-19 01:20:32 +0900

    MP-174 :white_check_mark: Test: 테스트 초기 구성
    * 도메인 컨트롤러 스캔 어노테이션 구현
    * 도메인 서비스 Mocking을 위한 포스트 프로세서 구현
    * 도메인 컨트롤러 전용 맥락 구성

---
+++ src/test/java/kr/modusplant/domains/common/postprocessor/MockDomainsServiceBeanFactoryPostProcessor.java
@@ -0,0 +1,43 @@
+package kr.modusplant.domains.common.postprocessor;
+
+import io.micrometer.common.lang.NonNullApi;
+import org.mockito.Mockito;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ClassUtils;
+
+import java.util.Objects;
+
+import static kr.modusplant.domains.common.vo.Reference.NOTATION_DOMAINS;
+
+@NonNullApi
+public class MockDomainsServiceBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false) {
+            @Override
+            protected boolean isCandidateComponent(AnnotatedBeanDefinition beanDefinition) {
+                return beanDefinition.getMetadata().isInterface() && beanDefinition.getMetadata().hasAnnotation(Service.class.getName());
+            }
+        };
+        scanner.addIncludeFilter(new AnnotationTypeFilter(Service.class));
+        ClassLoader classLoader = this.getClass().getClassLoader();
+
+        for (BeanDefinition repositoryDef : scanner.findCandidateComponents(NOTATION_DOMAINS)) {
+            Class<?> clazz;
+            try {
+                clazz = ClassUtils.forName(Objects.requireNonNull(repositoryDef.getBeanClassName()), classLoader);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException("Fail to load the service class: " + repositoryDef.getBeanClassName());
+            }
+            beanFactory.registerSingleton(clazz.getSimpleName(), Mockito.mock(clazz));
+        }
+    }
+}
