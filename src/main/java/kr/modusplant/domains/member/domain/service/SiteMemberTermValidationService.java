1f0097cd071abf78c35d8c62d7bd3bf92f0f59ca
JunHyeok <akdnjs0308@gmail.com>
2025-05-19 16:26:47 +0900

    MP-174 :sparkles: Feat: Spring 빈에 @Primary 추가
    * 카멜 케이스, 파스칼 케이스 이름의 빈 2개가 잡히는 문제 해결

--- src/main/java/kr/modusplant/domains/member/domain/service/SiteMemberTermValidationService.java
+++ src/main/java/kr/modusplant/domains/member/domain/service/SiteMemberTermValidationService.java
@@ -5,12 +5,14 @@ import kr.modusplant.domains.member.persistence.repository.SiteMemberTermReposit
 import kr.modusplant.global.error.EntityExistsWithUuidException;
 import kr.modusplant.global.error.EntityNotFoundWithUuidException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Service
+@Primary
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class SiteMemberTermValidationService {
