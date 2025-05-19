12b6136d22d1f07cf0bcef99ffd7a750c36ab311
JunHyeok <akdnjs0308@gmail.com>
2025-05-19 18:22:59 +0900

    MP-174 :white_check_mark: Test: TermControllerTest에서의 첫 단위 테스트 성공
    * 올바르게 동작하지 않는 기존 테스트 구성 클래스는 모두 제거
    - 아마도 서비스와 리포지토리를 Mocking할 때 리포지토리의 반환 값이 서비스 매소드에서 올바르게 작동하는 데 장애가 되는 것으로 추정됨
    * @SpringBootTest와 @AutoConfigureMockMvc으로 깔끔하게 구성하였으며, 이후 커밋에서 해당 어노테이션 구성을 최적화할 예정

---
+++ src/test/java/kr/modusplant/domains/term/app/controller/TermControllerTest.java
@@ -0,0 +1,62 @@
+package kr.modusplant.domains.term.app.controller;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.modusplant.domains.term.app.http.response.TermResponse;
+import kr.modusplant.domains.term.app.service.TermApplicationService;
+import kr.modusplant.domains.term.common.util.app.http.response.TermResponseTestUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.Map;
+
+import static kr.modusplant.global.vo.CamelCaseWord.DATA;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+class TermControllerTest implements TermResponseTestUtils {
+
+    private final MockMvc mockMvc;
+
+    @MockitoBean
+    private final TermApplicationService termApplicationService;
+
+    @Autowired
+    TermControllerTest(MockMvc mockMvc, TermApplicationService termApplicationService) {
+        this.mockMvc = mockMvc;
+        this.termApplicationService = termApplicationService;
+    }
+
+    @DisplayName("모든 약관 얻기")
+    @Test
+    void getAllTermsTest() throws Exception {
+        // given
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<TermResponse> termsOfUseResponseList = List.of(termsOfUseResponse);
+
+        when(termApplicationService.getAll()).thenReturn(termsOfUseResponseList);
+
+        // when
+        Map<String, Object> responseMap = objectMapper.readValue(
+                mockMvc.perform(get("/api/crud/terms"))
+                        .andExpect(status().isOk())
+                        .andReturn().getResponse().getContentAsString(), new TypeReference<>() {
+                });
+
+        // then
+        assertThat(objectMapper.convertValue(responseMap.get(DATA),
+                new TypeReference<List<TermResponse>>() {
+                })
+        ).isEqualTo(termsOfUseResponseList);
+    }
+}
