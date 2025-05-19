29c77ea18f0d0929525275044db0bd1bfc582705
Jun Hee <umpa9327@gmail.com>
2025-05-19 21:00:08 +0900

    MP-114 :white_check_mark: Test: 댓글 엔티티 테스트 클래스 구현
    - 엔티티 클래스와 db의 상호작용을 검사하기 위해 엔티티 클래스의 prePersist에 대한 테스트 클래스를 구현함

---
+++ src/test/java/kr/modusplant/domains/tip/persistence/entity/TipCommentEntityTest.java
@@ -0,0 +1,58 @@
+package kr.modusplant.domains.tip.persistence.entity;
+
+import kr.modusplant.domains.group.persistence.entity.PlantGroupEntity;
+import kr.modusplant.domains.member.persistence.entity.SiteMemberAuthEntity;
+import kr.modusplant.domains.member.persistence.entity.SiteMemberEntity;
+import kr.modusplant.domains.tip.common.util.domain.TipCommentTestUtils;
+import kr.modusplant.domains.tip.common.util.domain.TipPostTestUtils;
+import kr.modusplant.domains.tip.common.util.entity.TipCommentEntityTestUtils;
+import kr.modusplant.domains.tip.common.util.entity.TipPostEntityTestUtils;
+import kr.modusplant.global.context.RepositoryOnlyContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RepositoryOnlyContext
+public class TipCommentEntityTest implements TipCommentEntityTestUtils, TipPostEntityTestUtils {
+
+    private final TestEntityManager entityManager;
+
+    @Autowired
+    TipCommentEntityTest(TestEntityManager entityManager) {
+        this.entityManager = entityManager;}
+
+    @DisplayName("소통 팁 댓글 PrePersist")
+    @Test
+    void prePersist() {
+        // given
+        SiteMemberEntity member = createMemberBasicUserEntity();
+        PlantGroupEntity plantGroup = createPlantGroupEntity();
+        TipPostEntity postEntity = createTipPostEntityBuilder()
+                .group(plantGroup)
+                .authMember(member)
+                .createMember(member)
+                .recommendationNumber(1)
+                .viewCount(1)
+                .isDeleted(true)
+                .build();
+
+        TipCommentEntity commentEntity = createTipCommentEntityBuilder()
+                .postEntity(postEntity)
+                .authMember(member)
+                .createMember(member)
+                .isDeleted(true)
+                .build();
+
+        // when
+        entityManager.persist(postEntity);
+        entityManager.persist(commentEntity);
+        entityManager.flush();
+
+        // then
+        assertThat(commentEntity.getIsDeleted()).isEqualTo(true);
+    }
+}
