1e9dcd8aa2427385bf1dfd1dd450b1093cbe10e8
Jun Hee <umpa9327@gmail.com>
2025-05-20 19:16:18 +0900

    MP-114 :goal_net: Catch: 댓글 리포지토리 수정
    - Spring Data JPA의 메서드 작성 관례에 맞도록 메서드 구조 및 이름 수정

--- src/main/java/kr/modusplant/domains/tip/persistence/repository/TipCommentRepository.java
+++ src/main/java/kr/modusplant/domains/tip/persistence/repository/TipCommentRepository.java
@@ -1,7 +1,6 @@
 package kr.modusplant.domains.tip.persistence.repository;
 
-import kr.modusplant.domains.conversation.persistence.entity.ConvCommentEntity;
-import kr.modusplant.domains.conversation.persistence.entity.compositekey.ConvCommentCompositeKey;
+import kr.modusplant.domains.member.persistence.entity.SiteMemberEntity;
 import kr.modusplant.domains.tip.persistence.entity.TipCommentEntity;
 import kr.modusplant.domains.tip.persistence.entity.compositekey.TipCommentCompositeKey;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,18 +8,17 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface TipCommentRepository extends JpaRepository<TipCommentEntity, TipCommentCompositeKey> {
 
-    Optional<TipCommentEntity> findById(TipCommentCompositeKey tipCommentCompositeKey);
+    Optional<TipCommentEntity> findByPostUlidAndMaterializedPath(String postUlid, String materializedPath);
 
-    List<TipCommentEntity> findBypostUlid(String postUlid);
+    List<TipCommentEntity> findByPostUlid(String postUlid);
 
-    List<TipCommentEntity> findByAuthMemberUuid(UUID uuid);
+    List<TipCommentEntity> findByAuthMember(SiteMemberEntity authMember);
 
-    List<TipCommentEntity> findByCreateMemberUuid(UUID uuid);
+    List<TipCommentEntity> findByCreateMember(SiteMemberEntity createMember);
 
     List<TipCommentEntity> findByContent(String content);
 }
