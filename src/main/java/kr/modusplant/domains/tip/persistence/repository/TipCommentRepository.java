d1e8642111e922eff2e8ddcfc0f8a51fba19bf4a
Jun Hee <umpa9327@gmail.com>
2025-05-22 19:15:35 +0900

    MP-114 :sparkles: Feat: 팁 댓글 리포지토리 수정
    - 팁 댓글 엔티티의 기본키 중 하나가 게시글 엔티티에 의존하므로 기존의 findByPostUlid를 findByPostEntity로 수정함
    - 팁 댓글 엔티티의 삭제에 사용하기 위해 게시글 ulid와 댓글 경로로 엔티티를 삭제하는 deleteByPostUlidAndMaterializedPath 메서드 추가

--- src/main/java/kr/modusplant/domains/tip/persistence/repository/TipCommentRepository.java
+++ src/main/java/kr/modusplant/domains/tip/persistence/repository/TipCommentRepository.java
@@ -2,6 +2,7 @@ package kr.modusplant.domains.tip.persistence.repository;
 
 import kr.modusplant.domains.member.persistence.entity.SiteMemberEntity;
 import kr.modusplant.domains.tip.persistence.entity.TipCommentEntity;
+import kr.modusplant.domains.tip.persistence.entity.TipPostEntity;
 import kr.modusplant.domains.tip.persistence.entity.compositekey.TipCommentCompositeKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,11 +15,13 @@ public interface TipCommentRepository extends JpaRepository<TipCommentEntity, Ti
 
     Optional<TipCommentEntity> findByPostUlidAndMaterializedPath(String postUlid, String materializedPath);
 
-    List<TipCommentEntity> findByPostUlid(String postUlid);
+    List<TipCommentEntity> findByPostEntity(TipPostEntity postEntity);
 
     List<TipCommentEntity> findByAuthMember(SiteMemberEntity authMember);
 
     List<TipCommentEntity> findByCreateMember(SiteMemberEntity createMember);
 
     List<TipCommentEntity> findByContent(String content);
+
+    void deleteByPostUlidAndMaterializedPath(String postUlid, String materializedPath);
 }
