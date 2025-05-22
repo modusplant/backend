4cbf90a49d533f8b41d30992e6cc1a0942af49f9
Jun Hee <umpa9327@gmail.com>
2025-05-22 23:48:29 +0900

    MP-114 :white_check_mark: Test: 팁 댓글 도메인 테스트 유틸리티 클래스 수정
    - 다양한 테스트에 대비하기 위해 다른 엔티티에 의존하는 값을 제외한 팁 댓글 도메인 인스턴스를 반환하는 메서드를 추가함
    - 테스트 데이터의 구성 맥락을 나타내기 위해 사용자의 정보를 임의의 값이 아닌 사용자 도메인 테스트 유틸리티에서 가져온 값을 사용함

--- src/test/java/kr/modusplant/domains/tip/common/util/domain/TipCommentTestUtils.java
+++ src/test/java/kr/modusplant/domains/tip/common/util/domain/TipCommentTestUtils.java
@@ -2,18 +2,20 @@ package kr.modusplant.domains.tip.common.util.domain;
 
 import kr.modusplant.domains.tip.domain.model.TipComment;
 
-import java.util.UUID;
-
-import static kr.modusplant.domains.tip.common.util.domain.TipPostTestUtils.tipPost;
+import static kr.modusplant.domains.member.common.util.domain.SiteMemberTestUtils.memberBasicUserWithUuid;
 import static kr.modusplant.domains.tip.common.util.domain.TipPostTestUtils.tipPostWithUlid;
 
 public interface TipCommentTestUtils {
 
-    TipComment convComment = TipComment.builder()
+    TipComment tipComment = TipComment.builder()
+            .content("테스트 댓글 내용")
+            .build();
+
+    TipComment tipCommentWithPostUlidAndMaterializedPath = TipComment.builder()
             .postUlid(tipPostWithUlid.getUlid())
             .materializedPath("/1/6/2")
-            .authMemberUuid(UUID.fromString("48c75e56-34fb-4fc2-8e45-ee5669f79fdd"))
-            .createMemberUuid(UUID.fromString("d6b716f1-60f7-4c79-aeaf-37037101f126"))
+            .authMemberUuid(memberBasicUserWithUuid.getUuid())
+            .createMemberUuid(memberBasicUserWithUuid.getUuid())
             .content("테스트 댓글 내용")
             .build();
 }
