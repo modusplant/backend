eab7f11f54cd2f70aeef35de015564bdb09658f8
Jun Hee <umpa9327@gmail.com>
2025-05-22 20:11:51 +0900

    MP-114 :sparkles: Feat: 팁 댓글 애플리케이션 서비스 수정
    - 댓글 엔티티가 게시글 및 사용자 엔티티와 관련성이 있음을 반영하여 단순한 값이 아닌 게시글 및 사용자 엔티티로 댓글 데이터를 가져올 수 있도록 수정함
    - 단순 조회 작업을 위해 복합 키를 구성하는 것은 번거로우므로 복합키로 조회하는 메서드를 게시글 ulid와 댓글 경로 값으로 조회하는 메서드로 대체함
    - insert 메서드에서 매퍼에 필요한 의존성을 전달하는 로직 추가

--- src/main/java/kr/modusplant/domains/tip/app/service/TipCommentApplicationService.java
+++ src/main/java/kr/modusplant/domains/tip/app/service/TipCommentApplicationService.java
@@ -1,12 +1,16 @@
 package kr.modusplant.domains.tip.app.service;
 
+import kr.modusplant.domains.member.persistence.entity.SiteMemberEntity;
+import kr.modusplant.domains.member.persistence.repository.SiteMemberRepository;
 import kr.modusplant.domains.tip.app.http.request.TipCommentInsertRequest;
 import kr.modusplant.domains.tip.app.http.response.TipCommentResponse;
 import kr.modusplant.domains.tip.domain.service.TipCommentValidationService;
 import kr.modusplant.domains.tip.mapper.TipCommentAppInfraMapper;
+import kr.modusplant.domains.tip.mapper.TipCommentAppInfraMapperImpl;
 import kr.modusplant.domains.tip.persistence.entity.TipCommentEntity;
-import kr.modusplant.domains.tip.persistence.entity.compositekey.TipCommentCompositeKey;
+import kr.modusplant.domains.tip.persistence.entity.TipPostEntity;
 import kr.modusplant.domains.tip.persistence.repository.TipCommentRepository;
+import kr.modusplant.domains.tip.persistence.repository.TipPostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
@@ -14,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Service
 @Primary
@@ -23,26 +26,34 @@ import java.util.UUID;
 public class TipCommentApplicationService {
 
     private final TipCommentValidationService tipCommentValidationService;
+    private final TipCommentAppInfraMapper tipCommentAppInfraMapper = new TipCommentAppInfraMapperImpl();
     private final TipCommentRepository tipCommentRepository;
-    private final TipCommentAppInfraMapper tipCommentAppInfraMapper;
+    private final TipPostRepository tipPostRepository;
+    private final SiteMemberRepository memberRepository;
 
     public List<TipCommentResponse> getAll() {
         return tipCommentRepository.findAll()
                 .stream().map(tipCommentAppInfraMapper::toTipCommentResponse).toList();
     }
 
-    public List<TipCommentResponse> getByPostUlid(String postUlid) {
-        return tipCommentRepository.findBypostUlid(postUlid)
+    public List<TipCommentResponse> getByPostEntity(TipPostEntity requestPostEntity) {
+        TipPostEntity postEntity = tipPostRepository.findByUlid(requestPostEntity.getUlid()).orElseThrow();
+
+        return tipCommentRepository.findByPostEntity(postEntity)
                 .stream().map(tipCommentAppInfraMapper::toTipCommentResponse).toList();
     }
 
-    public List<TipCommentResponse> getByAuthMemberUuid(UUID uuid) {
-        return tipCommentRepository.findByAuthMemberUuid(uuid)
+    public List<TipCommentResponse> getByAuthMember(SiteMemberEntity authMember) {
+        SiteMemberEntity memberEntity = memberRepository.findByUuid(authMember.getUuid()).orElseThrow();
+
+        return tipCommentRepository.findByAuthMember(memberEntity)
                 .stream().map(tipCommentAppInfraMapper::toTipCommentResponse).toList();
     }
 
-    public List<TipCommentResponse> getByCreateMemberUuid(UUID uuid) {
-        return tipCommentRepository.findByAuthMemberUuid(uuid)
+    public List<TipCommentResponse> getByCreateMember(SiteMemberEntity createMember) {
+        SiteMemberEntity memberEntity = memberRepository.findByUuid(createMember.getUuid()).orElseThrow();
+
+        return tipCommentRepository.findByCreateMember(memberEntity)
                 .stream().map(tipCommentAppInfraMapper::toTipCommentResponse).toList();
     }
 
@@ -51,10 +62,10 @@ public class TipCommentApplicationService {
                 .stream().map(tipCommentAppInfraMapper::toTipCommentResponse).toList();
     }
 
-    public Optional<TipCommentResponse> getById(String postUlid, String materializedPath) {
+    public Optional<TipCommentResponse> getByPostUlidAndMaterializedPath(String postUlid, String materializedPath) {
         return Optional.of(
                 tipCommentAppInfraMapper.toTipCommentResponse(
-                        tipCommentRepository.findById(new TipCommentCompositeKey(postUlid, materializedPath)).orElseThrow()
+                        tipCommentRepository.findByPostUlidAndMaterializedPath(postUlid, materializedPath).orElseThrow()
                 ));
     }
 
@@ -62,19 +73,18 @@ public class TipCommentApplicationService {
     public TipCommentResponse insert(TipCommentInsertRequest commentInsertRequest) {
         String postUlid = commentInsertRequest.postUlid();
         String materializedPath = commentInsertRequest.materializedPath();
-        tipCommentValidationService.validateExistence(postUlid, materializedPath);
-        tipCommentValidationService.validateNotFoundEntity(postUlid, materializedPath);
+        tipCommentValidationService.validateFoundTipCommentEntity(postUlid, materializedPath);
 
-        TipCommentEntity commentEntity = tipCommentAppInfraMapper.toTipCommentEntity(commentInsertRequest);
+        TipCommentEntity commentEntity =
+                tipCommentAppInfraMapper.toTipCommentEntity(commentInsertRequest, tipPostRepository, memberRepository);
 
         return tipCommentAppInfraMapper.toTipCommentResponse(
                 tipCommentRepository.save(commentEntity));
     }
 
     @Transactional
-    public void removeById(String postUlid, String materializedPath) {
-        tipCommentValidationService.validateExistence(postUlid, materializedPath);
-        tipCommentValidationService.validateNotFoundEntity(postUlid, materializedPath);
-        tipCommentRepository.deleteById(new TipCommentCompositeKey(postUlid, materializedPath));
+    public void removeByPostUlidAndMaterializedPath(String postUlid, String materializedPath) {
+        tipCommentValidationService.validateNotFoundTipCommentEntity(postUlid, materializedPath);
+        tipCommentRepository.deleteByPostUlidAndMaterializedPath(postUlid, materializedPath);
     }
 }
