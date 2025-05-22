fc3f781c9f6ef7da828dc685e89d71d745bbfc4c
Jun Hee <umpa9327@gmail.com>
2025-05-22 19:47:58 +0900

    MP-114 :white_check_mark: Test: 팁 댓글의 삽입 요청 및 응답 테스트 유틸리티 클래스 수정
    - 삽입 요청과 응답이 이뤄지는 맥락을 반영하기 위해 완성된 객체를 파라미터를 받는 메서드로 대체함

--- src/test/java/kr/modusplant/domains/tip/common/util/app/http/request/TipCommentInsertRequestTestUtils.java
+++ src/test/java/kr/modusplant/domains/tip/common/util/app/http/request/TipCommentInsertRequestTestUtils.java
@@ -1,13 +1,16 @@
 package kr.modusplant.domains.tip.common.util.app.http.request;
 
 import kr.modusplant.domains.tip.app.http.request.TipCommentInsertRequest;
+import kr.modusplant.domains.tip.common.util.entity.TipCommentEntityTestUtils;
+import kr.modusplant.domains.tip.persistence.entity.TipCommentEntity;
 
 import java.util.UUID;
 
-import static kr.modusplant.domains.tip.common.util.domain.TipPostTestUtils.tipPostWithUlid;
+public interface TipCommentInsertRequestTestUtils extends TipCommentEntityTestUtils {
+    default TipCommentInsertRequest createTipCommentInsertRequest(String postUlid, UUID createMemberUuid) {
+        TipCommentEntity commentEntity = createTipCommentEntityBuilder().build();
 
-public interface TipCommentInsertRequestTestUtils {
-    TipCommentInsertRequest tipCommentInsertRequest = new TipCommentInsertRequest(
-            tipPostWithUlid.getUlid(), "/1/6/2",
-            UUID.fromString("d6b716f1-60f7-4c79-aeaf-37037101f126"), "테스트 댓글 내용");
+        return new TipCommentInsertRequest(postUlid, commentEntity.getMaterializedPath(),
+                createMemberUuid, commentEntity.getContent());
+    }
 }
--- src/test/java/kr/modusplant/domains/tip/common/util/app/http/response/TipCommentResponseTestUtils.java
+++ src/test/java/kr/modusplant/domains/tip/common/util/app/http/response/TipCommentResponseTestUtils.java
@@ -1,13 +1,16 @@
 package kr.modusplant.domains.tip.common.util.app.http.response;
 
 import kr.modusplant.domains.tip.app.http.response.TipCommentResponse;
+import kr.modusplant.domains.tip.common.util.entity.TipCommentEntityTestUtils;
+import kr.modusplant.domains.tip.persistence.entity.TipCommentEntity;
 
 import java.util.UUID;
 
-public interface TipCommentResponseTestUtils {
-    TipCommentResponse tipCommentResponse = new TipCommentResponse(
-            "01H5Z7XQ3W4F9K2G1V8R6T0Y5P", "/1/6/2",
-            UUID.fromString("48c75e56-34fb-4fc2-8e45-ee5669f79fdd"), UUID.fromString("d6b716f1-60f7-4c79-aeaf-37037101f126"),
-             "테스트 댓글 내용"
-    );
+public interface TipCommentResponseTestUtils extends TipCommentEntityTestUtils {
+    default TipCommentResponse createTipCommentResponse(String ulid, UUID authMemberUuid, UUID createMemberUuid) {
+        TipCommentEntity commentEntity = createTipCommentEntityBuilder().build();
+
+        return new TipCommentResponse(ulid, commentEntity.getMaterializedPath(),
+                authMemberUuid, createMemberUuid, commentEntity.getContent());
+    }
 }
