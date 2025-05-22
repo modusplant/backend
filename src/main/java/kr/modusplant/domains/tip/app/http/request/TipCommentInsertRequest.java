b95c68e93458755c34bf9dc09ededf96eb61ee5c
Jun Hee <umpa9327@gmail.com>
2025-05-22 19:38:46 +0900

    MP-114 :sparkles: Feat: 팁 댓글의 삽입 요청 레코드 수정
    - 삽입 요청 시 댓글을 삽입하는 사용자의 정보가 필요하므로 createMemberUuid 필드를 추가함

--- src/main/java/kr/modusplant/domains/tip/app/http/request/TipCommentInsertRequest.java
+++ src/main/java/kr/modusplant/domains/tip/app/http/request/TipCommentInsertRequest.java
@@ -1,7 +1,10 @@
 package kr.modusplant.domains.tip.app.http.request;
 
+import java.util.UUID;
+
 public record TipCommentInsertRequest(
         String postUlid,
         String materializedPath,
+        UUID createMemberUuid,
         String content
 ) {}
