9794dc8fc86f17bd017b4a83215c9fe02c8d0373
Jun Hee <umpa9327@gmail.com>
2025-05-22 19:24:16 +0900

    MP-114 :sparkles: Feat: 팁 댓글의 복합 기본키 클래스 수정
    - 불필요한 인스턴스화를 방지하기 위해 no-args 생성자의 접근 단계를 protected로 변경
    - equals()에 잘못 적용된 TipCommentEntity를 TipComentCompositeKey로 변경
    - 테스트 데이터를 구성하기 위한 용도로 builder 메서드 추가

--- src/main/java/kr/modusplant/domains/tip/persistence/entity/compositekey/TipCommentCompositeKey.java
+++ src/main/java/kr/modusplant/domains/tip/persistence/entity/compositekey/TipCommentCompositeKey.java
@@ -1,6 +1,6 @@
 package kr.modusplant.domains.tip.persistence.entity.compositekey;
 
-import kr.modusplant.domains.tip.persistence.entity.TipCommentEntity;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +10,7 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import java.io.Serializable;
 
 @AllArgsConstructor
-@NoArgsConstructor(force = true)
+@NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
 @Getter
 public class TipCommentCompositeKey implements Serializable {
 
@@ -20,7 +20,7 @@ public class TipCommentCompositeKey implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof TipCommentEntity that)) return false;
+        if (!(o instanceof TipCommentCompositeKey that)) return false;
 
         return new EqualsBuilder()
                 .append(getPostUlid(), that.getPostUlid())
@@ -34,5 +34,34 @@ public class TipCommentCompositeKey implements Serializable {
                 .append(getPostUlid())
                 .append(getMaterializedPath())
                 .toHashCode();
+    }
+
+    public static TipCommentCompositeKeyBuilder builder() {
+        return new TipCommentCompositeKeyBuilder();
+    }
+
+    public static final class TipCommentCompositeKeyBuilder {
+        private String postUlid;
+        private String materializedPath;
+
+        public TipCommentCompositeKeyBuilder postUlid(final String postUlid) {
+            this.postUlid = postUlid;
+            return this;
+        }
+
+        public TipCommentCompositeKeyBuilder materializedPath(final String materializedPath) {
+            this.materializedPath = materializedPath;
+            return this;
+        }
+
+        public TipCommentCompositeKeyBuilder TipCommentCompositeKey(final TipCommentCompositeKey compositeKey) {
+            this.postUlid = compositeKey.postUlid;
+            this.materializedPath = compositeKey.getMaterializedPath();
+            return this;
+        }
+
+        public TipCommentCompositeKey build() {
+            return new TipCommentCompositeKey(this.postUlid, this.materializedPath);
+        }
     }
 }
