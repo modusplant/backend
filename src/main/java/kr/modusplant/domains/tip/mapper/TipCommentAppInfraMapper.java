a218b69c152e78f68fa64596a1341dcc3bd583f1
Jun Hee <umpa9327@gmail.com>
2025-05-22 19:28:53 +0900

    MP-114 :sparkles: Feat: 팁 댓글의 인프라 매퍼 수정
    - 팁 댓글 엔티티에서 다른 엔티티에 의존하는 필드를 반영하고 즉시 매핑될 수 없는 필드를 매핑하기 위해 매핑 메서드를 구축함

--- src/main/java/kr/modusplant/domains/tip/mapper/TipCommentAppInfraMapper.java
+++ src/main/java/kr/modusplant/domains/tip/mapper/TipCommentAppInfraMapper.java
@@ -1,18 +1,53 @@
 package kr.modusplant.domains.tip.mapper;
 
-import kr.modusplant.domains.conversation.app.http.request.ConvCommentInsertRequest;
-import kr.modusplant.domains.conversation.app.http.response.ConvCommentResponse;
-import kr.modusplant.domains.conversation.persistence.entity.ConvCommentEntity;
+import kr.modusplant.domains.member.persistence.entity.SiteMemberEntity;
+import kr.modusplant.domains.member.persistence.repository.SiteMemberRepository;
 import kr.modusplant.domains.tip.app.http.request.TipCommentInsertRequest;
 import kr.modusplant.domains.tip.app.http.response.TipCommentResponse;
-import kr.modusplant.domains.tip.domain.model.TipComment;
 import kr.modusplant.domains.tip.persistence.entity.TipCommentEntity;
+import kr.modusplant.domains.tip.persistence.entity.TipPostEntity;
+import kr.modusplant.domains.tip.persistence.repository.TipPostRepository;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.UUID;
 
 @Mapper
 public interface TipCommentAppInfraMapper {
 
+    @Mapping(source = "postEntity", target = "postUlid", qualifiedByName = "getPostUlid")
+    @Mapping(source = "authMember", target = "authMemberUuid", qualifiedByName = "getMemberUuid")
+    @Mapping(source = "createMember", target = "createMemberUuid", qualifiedByName = "getMemberUuid")
     TipCommentResponse toTipCommentResponse(TipCommentEntity tipCommentEntity);
 
-    TipCommentEntity toTipCommentEntity(TipCommentInsertRequest insertRequest);
+    @Mapping(target = "TipCommentEntity", ignore = true)
+    @Mapping(source = "createMemberUuid", target = "authMember", qualifiedByName = "toMemberEntity")
+    @Mapping(source = "createMemberUuid", target = "createMember", qualifiedByName = "toMemberEntity")
+    @Mapping(target = "isDeleted", ignore = true)
+    @Mapping(source = "postUlid", target = "postEntity", qualifiedByName = "toTipPostEntity")
+    TipCommentEntity toTipCommentEntity(TipCommentInsertRequest insertRequest,
+                                        @Context TipPostRepository tipPostRepository, @Context SiteMemberRepository memberRepository);
+
+    @Named("getPostUlid")
+    default String getPostUlid(TipPostEntity postEntity) {
+        return postEntity.getUlid();
+    }
+
+    @Named("getMemberUuid")
+    default UUID getMemberUuid(SiteMemberEntity member) {
+        return member.getUuid();
+    }
+
+    @Named("toMemberEntity")
+    default SiteMemberEntity toMemberEntity(UUID memberUuid, @Context SiteMemberRepository memberRepository) {
+        return memberRepository.findByUuid(memberUuid).orElseThrow();
+    }
+
+    @Named("toTipPostEntity")
+    default TipPostEntity toTipPostEntity(String ulid, @Context TipPostRepository tipPostRepository) {
+        System.out.println("mapper is running");
+        return tipPostRepository.findByUlid(ulid).orElseThrow();
+    }
 }
