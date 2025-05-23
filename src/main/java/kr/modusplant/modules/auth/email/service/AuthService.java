2f187198d7d145104454dc0894c7239aacb0f79b
Kormap <kodh10@gmail.com>
2025-05-23 15:35:15 +0900

    MP-86 :recycle: Refactor: 이메일 인증 관련 리팩토링(서비스 레이어 구현)

--- src/main/java/kr/modusplant/modules/auth/email/service/AuthService.java
+++ src/main/java/kr/modusplant/modules/auth/email/service/AuthService.java
@@ -1,9 +1,5 @@
 package kr.modusplant.modules.auth.email.service;
 
-import kr.modusplant.domains.member.app.http.response.SiteMemberAuthResponse;
-import kr.modusplant.domains.member.app.service.SiteMemberAuthApplicationService;
-import kr.modusplant.global.middleware.redis.RedisHelper;
-import kr.modusplant.global.middleware.redis.RedisKeys;
 import kr.modusplant.modules.auth.email.model.request.EmailRequest;
 import kr.modusplant.modules.auth.email.model.request.VerifyEmailRequest;
 import kr.modusplant.modules.jwt.app.service.TokenProvider;
@@ -11,22 +7,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
-import java.util.List;
-
-import static kr.modusplant.global.middleware.redis.RedisKeys.RESET_PASSWORD_PREFIX;
-import static kr.modusplant.global.vo.CamelCaseWord.RESET_PASSWORD_EMAIL;
-import static kr.modusplant.global.vo.CamelCaseWord.SIGNUP_VERIFY_EMAIL;
-
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class AuthService {
     private final TokenProvider tokenProvider;
-    private final RedisHelper redisHelper;
-
     private final MailService mailService;
-    private final SiteMemberAuthApplicationService siteMemberAuthService;
 
     public String sendVerifyEmail(EmailRequest request) {
         String email = request.getEmail();
@@ -35,7 +21,6 @@ public class AuthService {
         // JWT 토큰 생성
         String accessToken = tokenProvider.generateVerifyAccessToken(email, verifyCode);
 
-        mailService.callSendEmail(email, verifyCode, SIGNUP_VERIFY_EMAIL);
         return accessToken;
     }
 
@@ -44,22 +29,7 @@ public class AuthService {
     }
 
     public void sendResetPasswordCode(EmailRequest request) {
-        // 가입한 유저인지 확인
-        String email = request.getEmail();
-        List<SiteMemberAuthResponse> list = siteMemberAuthService.getByEmail(email);
-        if (list.isEmpty()) {
-            throw new RuntimeException("Email not found");
-        }
 
-        // 이메일 인증코드 생성
-        String verifyCode = tokenProvider.generateVerifyCode();
-
-        // 인증코드 Redis 저장
-        String redisKey = RedisKeys.generateRedisKey(RESET_PASSWORD_PREFIX, email);
-        redisHelper.setString(redisKey, verifyCode, Duration.ofMinutes(5));
-
-        // 인증코드 메일 발신
-        mailService.callSendEmail(email, verifyCode, RESET_PASSWORD_EMAIL);
     }
 
 }
