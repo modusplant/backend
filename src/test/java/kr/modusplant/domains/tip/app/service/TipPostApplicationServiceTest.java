87f7e2f8f9443ff743b9a609c0d3546529992cfb
songu1 <syj0002012@naver.com>
2025-05-23 11:26:59 +0900

    MP-110 :white_check_mark: Test: 팁 게시글 서비스 계층 테스트 코드 추가
    - 팁 게시글 애플리케이션 서비스, 검증 서비스, 미디어 파일 처리 서비스 테스트 코드 구현

---
+++ src/test/java/kr/modusplant/domains/tip/app/service/TipPostApplicationServiceTest.java
@@ -0,0 +1,261 @@
+package kr.modusplant.domains.tip.app.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.modusplant.domains.group.common.util.entity.PlantGroupEntityTestUtils;
+import kr.modusplant.domains.group.persistence.entity.PlantGroupEntity;
+import kr.modusplant.domains.group.persistence.repository.PlantGroupRepository;
+import kr.modusplant.domains.member.common.util.entity.SiteMemberEntityTestUtils;
+import kr.modusplant.domains.member.persistence.entity.SiteMemberEntity;
+import kr.modusplant.domains.member.persistence.repository.SiteMemberRepository;
+import kr.modusplant.domains.tip.app.http.request.TipPostRequest;
+import kr.modusplant.domains.tip.app.http.response.TipPostResponse;
+import kr.modusplant.domains.tip.common.util.entity.TipPostEntityTestUtils;
+import kr.modusplant.domains.tip.common.util.http.request.TipPostRequestTestUtils;
+import kr.modusplant.domains.tip.persistence.entity.TipPostEntity;
+import kr.modusplant.domains.tip.persistence.repository.TipPostRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class TipPostApplicationServiceTest implements SiteMemberEntityTestUtils, PlantGroupEntityTestUtils, TipPostRequestTestUtils, TipPostEntityTestUtils {
+    @Autowired
+    private TipPostApplicationService tipPostApplicationService;
+
+    @Autowired
+    private SiteMemberRepository siteMemberRepository;
+
+    @Autowired
+    private PlantGroupRepository plantGroupRepository;
+
+    @Autowired
+    private TipPostRepository tipPostRepository;
+
+    @Autowired
+    private MediaContentService mediaContentService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private UUID memberUuid;
+    private Integer groupOrder;
+
+    @BeforeEach
+    void setUp() {
+        SiteMemberEntity member = createMemberBasicUserEntity();
+        siteMemberRepository.save(member);
+        memberUuid = member.getUuid();
+
+        PlantGroupEntity group = createPlantGroupEntity();
+        plantGroupRepository.save(group);
+        groupOrder = group.getOrder();
+    }
+
+    @Test
+    @DisplayName("전체 팁 게시글 목록 조회하기")
+    void getAllTest() throws IOException {
+        // given
+        TipPostRequest tipPostRequest1 = insertRequestAllTypes;
+        TipPostRequest tipPostRequest2 = insertRequestAllTypes;
+        TipPostRequest tipPostRequest3 = insertRequestAllTypes;
+        tipPostApplicationService.insert(tipPostRequest1,memberUuid);
+        tipPostApplicationService.insert(tipPostRequest2,memberUuid);
+        tipPostApplicationService.insert(tipPostRequest3,memberUuid);
+
+        // when
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<TipPostResponse> result = tipPostApplicationService.getAll(pageable);
+
+        assertThat(result.getContent()).hasSize(2);
+        assertThat(result.getNumber()).isEqualTo(0);
+        assertThat(result.getSize()).isEqualTo(2);
+        assertThat(result.getTotalElements()).isEqualTo(3);
+        assertThat(result.getTotalPages()).isEqualTo(2);
+        List<TipPostResponse> posts = result.getContent();
+        assertThat(posts.get(0).getCreatedAt()).isAfterOrEqualTo(posts.get(1).getCreatedAt());
+        assertThat(posts.get(0).getAuthMemberUuid()).isEqualTo(memberUuid);
+        assertThat(posts.get(0).getGroupOrder()).isEqualTo(groupOrder);
+    }
+
+
+    @Test
+    @DisplayName("사이트 회원별 팁 게시글 목록 조회하기")
+    void getByMemberUuidTest() throws IOException {
+        // given
+        SiteMemberEntity member = createMemberKakaoUserEntity();
+        siteMemberRepository.save(member);
+        UUID memberUuid2 = member.getUuid();
+        PlantGroupEntity group = createOtherGroupEntity();
+        plantGroupRepository.save(group);
+        Integer groupOrder2 = group.getOrder();
+        TipPostRequest tipPostRequest1 = insertRequestAllTypes;
+        TipPostRequest tipPostRequest2 = insertRequestAllTypes;
+        TipPostRequest tipPostRequest3 = insertRequestBasicTypes;
+        tipPostApplicationService.insert(tipPostRequest1,memberUuid);
+        tipPostApplicationService.insert(tipPostRequest2,memberUuid2);
+        tipPostApplicationService.insert(tipPostRequest3,memberUuid);
+
+        // when
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<TipPostResponse> result = tipPostApplicationService.getByMemberUuid(memberUuid,pageable);
+
+        assertThat(result.getContent()).hasSize(2);
+        assertThat(result.getNumber()).isEqualTo(0);
+        assertThat(result.getSize()).isEqualTo(2);
+        assertThat(result.getTotalElements()).isEqualTo(2);
+        assertThat(result.getTotalPages()).isEqualTo(1);
+        List<TipPostResponse> posts = result.getContent();
+        assertThat(posts.get(0).getCreatedAt()).isAfterOrEqualTo(posts.get(1).getCreatedAt());
+        assertThat(posts.get(0).getGroupOrder()).isEqualTo(groupOrder2);
+        assertThat(posts.get(1).getGroupOrder()).isEqualTo(groupOrder);
+    }
+
+    @Test
+    @DisplayName("식물 그룹별 팁 게시글 목록 조회하기")
+    void getByGroupOrderTest() throws IOException {
+        // given
+        SiteMemberEntity member = createMemberKakaoUserEntity();
+        siteMemberRepository.save(member);
+        UUID memberUuid2 = member.getUuid();
+        PlantGroupEntity group = createOtherGroupEntity();
+        plantGroupRepository.save(group);
+        Integer groupOrder2 = group.getOrder();
+        TipPostRequest tipPostRequest1 = insertRequestAllTypes;
+        TipPostRequest tipPostRequest2 = insertRequestAllTypes;
+        TipPostRequest tipPostRequest3 = insertRequestBasicTypes;
+        tipPostApplicationService.insert(tipPostRequest1,memberUuid);
+        tipPostApplicationService.insert(tipPostRequest2,memberUuid2);
+        tipPostApplicationService.insert(tipPostRequest3,memberUuid);
+
+        // when
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<TipPostResponse> result = tipPostApplicationService.getByGroupOrder(groupOrder,pageable);
+
+        assertThat(result.getContent()).hasSize(2);
+        assertThat(result.getNumber()).isEqualTo(0);
+        assertThat(result.getSize()).isEqualTo(2);
+        assertThat(result.getTotalElements()).isEqualTo(2);
+        assertThat(result.getTotalPages()).isEqualTo(1);
+        List<TipPostResponse> posts = result.getContent();
+        assertThat(posts.get(0).getCreatedAt()).isAfterOrEqualTo(posts.get(1).getCreatedAt());
+        assertThat(posts.get(0).getAuthMemberUuid()).isEqualTo(memberUuid2);
+        assertThat(posts.get(1).getAuthMemberUuid()).isEqualTo(memberUuid);
+    }
+
+    @Test
+    @DisplayName("제목+본문 검색어로 게시글 목록 조회하기")
+    void searchByKeywordTest() throws IOException {
+        // given
+        PlantGroupEntity group = createOtherGroupEntity();
+        plantGroupRepository.save(group);
+        Integer groupOrder2 = group.getOrder();
+        TipPostRequest tipPostRequest1 = insertRequestAllTypes;
+        TipPostRequest tipPostRequest2 = insertRequestBasicTypes;
+        tipPostApplicationService.insert(tipPostRequest1,memberUuid);
+        tipPostApplicationService.insert(tipPostRequest2,memberUuid);
+        Pageable pageable = PageRequest.of(0, 10);
+
+        // when
+        String keyword1 = "기르기";
+        String keyword2 = "test";
+        Page<TipPostResponse> result1 = tipPostApplicationService.searchByKeyword(keyword1,pageable);
+        Page<TipPostResponse> result2 = tipPostApplicationService.searchByKeyword(keyword2,pageable);
+
+        assertThat(result1.getTotalElements()).isEqualTo(1);
+        assertThat(result2.getTotalElements()).isEqualTo(2);
+        TipPostResponse post = result1.getContent().get(0);
+        assertThat(post.getTitle()).isEqualTo(tipPostRequest2.title());
+        assertThat(post.getContent().get(1).has("data")).isEqualTo(true);
+    }
+
+
+    @Test
+    @DisplayName("특정 팁 게시글 조회하기")
+    void getByUlidTest() throws IOException {
+        // given
+        SiteMemberEntity siteMember = siteMemberRepository.findByUuid(memberUuid).orElseThrow();
+        TipPostRequest tipPostRequest = insertRequestAllTypes;
+        PlantGroupEntity plantGroupEntity = plantGroupRepository.findByOrder(tipPostRequest.groupOrder()).orElseThrow();
+        TipPostEntity tipPostEntity = TipPostEntity.builder()
+                .group(plantGroupEntity)
+                .authMember(siteMember)
+                .createMember(siteMember)
+                .title(tipPostRequest.title())
+                .content(mediaContentService.saveFilesAndGenerateContentJson(tipPostRequest.content()))
+                .build();
+        tipPostRepository.save(tipPostEntity);
+
+        // when
+        Optional<TipPostResponse> result = tipPostApplicationService.getByUlid(tipPostEntity.getUlid());
+
+        assertThat(result).isPresent();
+        TipPostResponse response = result.get();
+        assertThat(response.getNickname()).isEqualTo(siteMember.getNickname());
+        assertThat(response.getCategory()).isEqualTo(plantGroupEntity.getCategory());
+        assertThat(response.getTitle()).isEqualTo(tipPostRequest.title());
+    }
+
+    @Test
+    @DisplayName("특정 팁 게시글 수정하기")
+    void updateTest() throws IOException {
+        // given
+        SiteMemberEntity siteMember = siteMemberRepository.findByUuid(memberUuid).orElseThrow();
+        TipPostRequest tipPostRequest = insertRequestAllTypes;
+        PlantGroupEntity plantGroupEntity = plantGroupRepository.findByOrder(tipPostRequest.groupOrder()).orElseThrow();
+        TipPostEntity tipPostEntity = TipPostEntity.builder()
+                .group(plantGroupEntity)
+                .authMember(siteMember)
+                .createMember(siteMember)
+                .title(tipPostRequest.title())
+                .content(mediaContentService.saveFilesAndGenerateContentJson(tipPostRequest.content()))
+                .build();
+        tipPostRepository.save(tipPostEntity);
+
+        // when
+        TipPostRequest tipPostUpdateRequest = updateRequestWithContentAndOrderInfo;
+        tipPostApplicationService.update(tipPostUpdateRequest,tipPostEntity.getUlid(),memberUuid);
+
+        // then
+        TipPostEntity result = tipPostRepository.findByUlid(tipPostEntity.getUlid()).orElseThrow();
+        assertThat(result.getContent().get(2).get("filename").asText()).isEqualTo(tipPostUpdateRequest.content().get(2).getOriginalFilename());
+    }
+
+    @Test
+    @DisplayName("특정 팁 게시글 삭제하기")
+    void removeByUlidTest() throws IOException {
+        // given
+        SiteMemberEntity siteMember = siteMemberRepository.findByUuid(memberUuid).orElseThrow();
+        TipPostRequest tipPostRequest = insertRequestAllTypes;
+        PlantGroupEntity plantGroupEntity = plantGroupRepository.findByOrder(tipPostRequest.groupOrder()).orElseThrow();
+        TipPostEntity tipPostEntity = TipPostEntity.builder()
+                .group(plantGroupEntity)
+                .authMember(siteMember)
+                .createMember(siteMember)
+                .title(tipPostRequest.title())
+                .content(mediaContentService.saveFilesAndGenerateContentJson(tipPostRequest.content()))
+                .build();
+        tipPostRepository.save(tipPostEntity);
+
+        // when
+        tipPostApplicationService.removeByUlid(tipPostEntity.getUlid(),memberUuid);
+
+        // then
+        TipPostEntity result = tipPostRepository.findByUlid(tipPostEntity.getUlid()).orElseThrow();
+        assertThat(result.getIsDeleted()).isTrue();
+    }
+}
