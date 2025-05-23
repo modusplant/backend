3c35ba3012850c61632b3866ce04efd37ab625b3
Kormap <kodh10@gmail.com>
2025-05-23 15:35:15 +0900

    MP-86 :recycle: Refactor: 이메일 인증 관련 리팩토링(서비스 레이어 구현)

--- src/main/java/kr/modusplant/modules/auth/email/app/controller/EmailAuthController.java
+++ src/main/java/kr/modusplant/modules/auth/email/app/controller/EmailAuthController.java
@@ -1,76 +1,59 @@
-package kr.modusplant.modules.auth.email.app.controller;
+package kr.modusplant.modules.auth.email.controller;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import kr.modusplant.global.app.http.response.DataResponse;
-import kr.modusplant.modules.auth.email.app.http.request.EmailRequest;
-import kr.modusplant.modules.auth.email.app.http.request.VerifyEmailRequest;
-import kr.modusplant.modules.auth.email.app.service.MailService;
-import kr.modusplant.modules.jwt.app.service.TokenProvider;
+import kr.modusplant.global.app.servlet.response.DataResponse;
+import kr.modusplant.modules.auth.email.model.request.EmailRequest;
+import kr.modusplant.modules.auth.email.model.request.VerifyEmailRequest;
+import kr.modusplant.modules.auth.email.service.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Random;
 
-import static kr.modusplant.global.vo.CamelCaseWord.VERIFY_CODE;
-import static kr.modusplant.global.vo.FieldName.EMAIL;
-
-@Tag(name = "이메일 인증 API", description = "이메일 인증 메일 발송과 검증을 다루는 API입니다.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-public class EmailAuthController {
-    private final TokenProvider tokenProvider;
-    private final MailService mailService;
+public class AuthController {
 
+    private final AuthService authService;
 
-    @Operation(summary = "본인 인증 메일 전송 API", description = "본인 인증을 위해 메일을 전송합니다.")
+    @Operation(
+            summary = "본인인증 메일 전송 API",
+            description = "회원가입 시 본인인증 메일 전송을 합니다."
+    )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK: Succeeded"),
-            @ApiResponse(responseCode = "400", description = "Bad Request: Failed email handling due to client error")
+            @ApiResponse(responseCode = "400", description = "Bad Request: Invalid input data.")
     })
     @PostMapping("/members/verify-email/send")
     public ResponseEntity<DataResponse<?>> verify(
             @RequestBody @Valid EmailRequest request,
             HttpServletResponse httpResponse
     ) {
-        String email = request.getEmail();
-        // 인증코드 생성
-        String verifyCode = tokenProvider.generateVerifyCode();
-        // JWT 토큰 생성
-        String accessToken = tokenProvider.generateVerifyAccessToken(email, verifyCode);
-
-        mailService.callSendVerifyEmail(email, verifyCode);
+        String accessToken = authService.sendVerifyEmail(request);
 
         // JWT AccessToken 설정
         setHttpOnlyCookie(accessToken, httpResponse);
         return ResponseEntity.ok(DataResponse.of(200, "OK: Succeeded"));
     }
 
-    @Operation(summary = "본인 인증 메일 검증 API", description = "본인 인증을 위해 코드를 검증합니다.")
+    @Operation(summary = "본인인증 메일 검증 API", description = "회원가입 시 본인인증 코드를 검증합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "OK: Succeeded"),
-            @ApiResponse(responseCode = "400", description = "Bad Request: Failed verification due to client error")
+            @ApiResponse(responseCode = "400", description = "Bad Request: Invalid input data.")
     })
     @PostMapping("/members/verify-email")
     public ResponseEntity<DataResponse<?>> verifyEmail(
             @RequestBody VerifyEmailRequest verifyEmailRequest,
             @CookieValue(value = "Authorization", required = false) String accessToken
     ) {
-        tokenProvider.validateVerifyAccessToken(accessToken, verifyEmailRequest.getVerifyCode());
+        authService.verifyEmail(verifyEmailRequest, accessToken);
 
         return ResponseEntity.ok(
                 DataResponse.ok(new HashMap<>() {{
@@ -87,18 +70,8 @@ public class EmailAuthController {
     public ResponseEntity<DataResponse<?>> sendResetPasswordCode(
             @RequestBody @Valid EmailRequest request
     ) {
-
+        authService.sendResetPasswordCode(request);
         return ResponseEntity.ok().body(DataResponse.of(200, "OK: Succeeded"));
-    }
-
-    public void setHttpOnlyCookie(String accessToken, HttpServletResponse httpResponse) {
-        Cookie accessTokenCookie = new Cookie("Authorization", accessToken);
-        accessTokenCookie.setHttpOnly(true); // HTTP-Only 설정: JavaScript에서 접근 불가
-        accessTokenCookie.setSecure(false);  // Secure 설정
-        accessTokenCookie.setPath("/");    // 모든 경로에서 유효
-        accessTokenCookie.setMaxAge(5 * 60); // 유효 기간: 5분 (Access 토큰의 유효 기간과 동일)
-        accessTokenCookie.setAttribute("SameSite", "Lax");
-        httpResponse.addCookie(accessTokenCookie);
     }
 
     public void setHttpOnlyCookie(String accessToken, HttpServletResponse httpResponse) {
