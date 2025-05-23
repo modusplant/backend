85073b98976b82daafc1857e2b06e3f7d152e4fc
Kormap <kodh10@gmail.com>
2025-05-23 14:56:08 +0900

    MP-86 :recycle: Refactor: 이메일 인증 관련 JWT 토큰 메소드 이동

--- src/main/java/kr/modusplant/modules/auth/email/app/controller/EmailAuthController.java
+++ src/main/java/kr/modusplant/modules/auth/email/app/controller/EmailAuthController.java
@@ -15,6 +15,7 @@ import kr.modusplant.global.app.http.response.DataResponse;
 import kr.modusplant.modules.auth.email.app.http.request.EmailRequest;
 import kr.modusplant.modules.auth.email.app.http.request.VerifyEmailRequest;
 import kr.modusplant.modules.auth.email.app.service.MailService;
+import kr.modusplant.modules.jwt.app.service.TokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -32,11 +33,9 @@ import static kr.modusplant.global.vo.FieldName.EMAIL;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class EmailAuthController {
+    private final TokenProvider tokenProvider;
     private final MailService mailService;
 
-    // 비밀키 설정
-    @Value("${mail-api.jwt-secret-key}")
-    private String JWT_SECRET_KEY;
 
     @Operation(summary = "본인 인증 메일 전송 API", description = "본인 인증을 위해 메일을 전송합니다.")
     @ApiResponses(value = {
@@ -50,16 +49,15 @@ public class EmailAuthController {
     ) {
         String email = request.getEmail();
         // 인증코드 생성
-        String verifyCode = generateVerifyCode();
+        String verifyCode = tokenProvider.generateVerifyCode();
         // JWT 토큰 생성
-        String accessToken = generateVerifyAccessToken(email, verifyCode);
+        String accessToken = tokenProvider.generateVerifyAccessToken(email, verifyCode);
 
-        // TODO : 메일 발송
         mailService.callSendVerifyEmail(email, verifyCode);
 
         // JWT AccessToken 설정
         setHttpOnlyCookie(accessToken, httpResponse);
-        return ResponseEntity.ok(DataResponse.ok());
+        return ResponseEntity.ok(DataResponse.of(200, "OK: Succeeded"));
     }
 
     @Operation(summary = "본인 인증 메일 검증 API", description = "본인 인증을 위해 코드를 검증합니다.")
@@ -72,9 +70,7 @@ public class EmailAuthController {
             @RequestBody VerifyEmailRequest verifyEmailRequest,
             @CookieValue(value = "Authorization", required = false) String accessToken
     ) {
-
-        // JwtToken 에 담긴 데이터 조회 테스트용
-        validateVerifyAccessToken(accessToken, verifyEmailRequest.getVerifyCode());
+        tokenProvider.validateVerifyAccessToken(accessToken, verifyEmailRequest.getVerifyCode());
 
         return ResponseEntity.ok(
                 DataResponse.ok(new HashMap<>() {{
@@ -82,64 +78,27 @@ public class EmailAuthController {
                 }}));
     }
 
-    // TODO : JWT Util or Provider 구현 완료 시 옮기는 것을 예상하고 작업 중
-    public String generateVerifyAccessToken(String email, String verifyCode) {
-        // 만료 시간 설정 (5분 뒤)
-        Date now = new Date();
-        Date expirationDate = new Date(now.getTime() + 5 * 60 * 1000);
+    @Operation(summary = "비밀번호 재설정 요청 API", description = "비밀번호 재설정 시 본인인증 코드를 메일로 발송합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK: Succeeded"),
+            @ApiResponse(responseCode = "400", description = "Bad Request: Invalid input data.")
+    })
+    @PostMapping("/auth/reset-password-request/send")
+    public ResponseEntity<DataResponse<?>> sendResetPasswordCode(
+            @RequestBody @Valid EmailRequest request
+    ) {
 
-        return Jwts.builder()
-                .header()
-                .type("JWT")
-                .and()
-                .claims()
-                .issuedAt(now)
-                .expiration(expirationDate)
-                .add(EMAIL, email)
-                .add(VERIFY_CODE, verifyCode)
-                .and()
-                .signWith(Keys.hmacShaKeyFor(JWT_SECRET_KEY.getBytes()))
-                .compact();
+        return ResponseEntity.ok().body(DataResponse.of(200, "OK: Succeeded"));
     }
 
-    // TODO : Spring Security 적용 후 필터에서 쿠키 검증 로직 추가된 후 테스트 필요
-    public void validateVerifyAccessToken(String jwtToken, String verifyCode) {
-        if (jwtToken != null && jwtToken.startsWith("Bearer ")) {
-            jwtToken = jwtToken.substring(7);
-        }
-
-        try {
-            // JWT 토큰 파싱
-            Claims claims = Jwts.parser()
-                    .verifyWith(Keys.hmacShaKeyFor(JWT_SECRET_KEY.getBytes()))
-                    .build()
-                    .parseSignedClaims(jwtToken)
-                    .getPayload();
-
-            // JWT 토큰 검증
-            String payloadVerifyCode = claims.get(VERIFY_CODE, String.class);
-
-            // 발급된 인증코드와 메일 인증코드 일치 검증
-            if (!verifyCode.equals(payloadVerifyCode)) {
-                throw new RuntimeException("Invalid verification code");
-            }
-        } catch (ExpiredJwtException e) {
-            throw new RuntimeException("Expired JWT token");
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage());
-        }
-    }
-
-    public String generateVerifyCode() {
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        Random random = new Random();
-
-        StringBuilder code = new StringBuilder();
-        for (int i = 0; i < 6; i++) {
-            int index = random.nextInt(characters.length());
-            code.append(characters.charAt(index));
-        }
-        return code.toString();
+    public void setHttpOnlyCookie(String accessToken, HttpServletResponse httpResponse) {
+        Cookie accessTokenCookie = new Cookie("Authorization", accessToken);
+        accessTokenCookie.setHttpOnly(true); // HTTP-Only 설정: JavaScript에서 접근 불가
+        accessTokenCookie.setSecure(false);  // Secure 설정
+        accessTokenCookie.setPath("/");    // 모든 경로에서 유효
+        accessTokenCookie.setMaxAge(5 * 60); // 유효 기간: 5분 (Access 토큰의 유효 기간과 동일)
+        accessTokenCookie.setAttribute("SameSite", "Lax");
+        httpResponse.addCookie(accessTokenCookie);
     }
 
     public void setHttpOnlyCookie(String accessToken, HttpServletResponse httpResponse) {
