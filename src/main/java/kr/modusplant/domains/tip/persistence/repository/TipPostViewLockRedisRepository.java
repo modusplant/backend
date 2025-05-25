87d3e55a876861af9f871d142b3e5643ed62c5c3
songu1 <syj0002012@naver.com>
2025-05-25 23:10:13 +0900

    MP-110 :sparkles: Feat: Redis 기반 조회수 관리 및 조회수 중복 방지 락 기능 리포지토리 구현
    * redis 기반 조회수를 관리하는 TipPostViewCountRedisRepository 추가
    - Redis 키 패턴(viewCount::tip_post::{ulid}::view_count) 기반 조회수 읽기, 증가, 쓰기 기능 구현
    - 모든 조회수 키를 조회해 ulid별 조회수 맵으로 반환하는 findAll() 메서드 추가
    - 팁 게시글 조회수를 Redis에 효율적으로 관리하여 빠른 읽기 및 증가 처리
    
    * 조회수 어뷰징 방지를 위한 TipPostViewLockRedisRepository 추가
    - Redis 키 패턴(viewCount::tip_post::{ulid}::member::{member_uuid}::lock) 기반 조회수 중복 방지를 위한 락 기능 구현
    - Redis에 TTL동안 데이터를 저장하고, 데이터가 존재하지 않을 경우만 조회수를 증가시키는 lock 메서드 추가
    - 특정 사용자의 중복 조회수 증가를 바지하기 위한 락 기능 구현으로 조회수 정확성 향상

---
+++ src/main/java/kr/modusplant/domains/tip/persistence/repository/TipPostViewLockRedisRepository.java
@@ -0,0 +1,26 @@
+package kr.modusplant.domains.tip.persistence.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class TipPostViewLockRedisRepository {
+    private final StringRedisTemplate stringRedisTemplate;
+
+    // viewCount::tip_post::{ulid}::member::{member_uuid}::lock
+    private static final String KEY_FORMAT = "viewCount::tip_post::%s::member::%s::lock";
+
+    public boolean lock(String ulid, UUID memberUuid, long ttlMinutes) {
+        String key = generateKey(ulid,memberUuid);
+        return stringRedisTemplate.opsForValue().setIfAbsent(key,"",Duration.ofMinutes(ttlMinutes));
+    }
+
+    private String generateKey(String ulid, UUID memberUuid) {
+        return KEY_FORMAT.formatted(ulid, memberUuid);
+    }
+}
