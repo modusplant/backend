52d7840534598b32bd89c44276e0c38b6e382335
songu1 <syj0002012@naver.com>
2025-05-25 10:09:33 +0900

    MP-110 :truck: Rename: MediaContentService, MediaContentServiceTest 파일 이동
    - domains내에서 전역적인 역할을 수행함에 따라 tip.app.service에서 common.domain.service로 이동

--- src/test/java/kr/modusplant/domains/common/domain/service/MediaContentServiceTest.java
+++ src/test/java/kr/modusplant/domains/common/domain/service/MediaContentServiceTest.java
@@ -1,4 +1,4 @@
-package kr.modusplant.domains.tip.app.service;
+package kr.modusplant.domains.common.domain.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
