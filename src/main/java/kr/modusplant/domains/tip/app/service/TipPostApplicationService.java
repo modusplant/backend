09400d01ce5a0e08105ebee136d11b2265f3f751
songu1 <syj0002012@naver.com>
2025-05-25 23:25:07 +0900

    MP-110 :sparkles: Feat: ApplicationService의 조회수 관련 메서드 및 시간 단위 조회수 DB 백업 스케줄러 추가
    * TipPostApplicationService의 조회수 조회 및 증가 메서드 추가
    
    * Redis 조회수 시간 단위 DB 백업 TipPostViewCountBackUpScheduler 생성
    - 특정 시간 주기로 Redis에 저장된 TipPost 조회수를 DB에 동기화하는 스케줄러 구현
    - Redis에서 전체 조회수 조회 후, 각 게시글의 DB 조회수를 업데이트하는 트랜잭션 처리 적용
    - Redis에 저장된 조회수를 주기적으로 DB에 반영하여 데이터 일관성 및 영속성 보장
    
    * 스케줄링 활성화 설정을 위한 SchedulerConfig 추가
    - Spring 스케줄링 기능 활성화를 위한 @EnableScheduling 설정 추가

--- src/main/java/kr/modusplant/domains/tip/app/service/TipPostApplicationService.java
+++ src/main/java/kr/modusplant/domains/tip/app/service/TipPostApplicationService.java
@@ -13,8 +13,12 @@ import kr.modusplant.domains.tip.domain.service.TipPostValidationService;
 import kr.modusplant.domains.tip.mapper.TipPostAppInfraMapper;
 import kr.modusplant.domains.tip.mapper.TipPostAppInfraMapperImpl;
 import kr.modusplant.domains.tip.persistence.entity.TipPostEntity;
+import kr.modusplant.domains.tip.persistence.repository.TipPostViewCountRedisRepository;
+import kr.modusplant.domains.tip.persistence.repository.TipPostViewLockRedisRepository;
 import kr.modusplant.domains.tip.persistence.repository.TipPostRepository;
+import kr.modusplant.global.error.EntityNotFoundWithUlidException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -37,8 +41,12 @@ public class TipPostApplicationService {
     private final TipPostRepository tipPostRepository;
     private final SiteMemberRepository siteMemberRepository;
     private final PlantGroupRepository plantGroupRepository;
+    private final TipPostViewCountRedisRepository tipPostViewCountRedisRepository;
+    private final TipPostViewLockRedisRepository tipPostViewLockRedisRepository;
     private final TipPostAppInfraMapper tipPostAppInfraMapper = new TipPostAppInfraMapperImpl();
 
+    @Value("${REDIS_TTL.VIEW_COUNT}")
+    private long ttlMinutes;
 
     public Page<TipPostResponse> getAll(Pageable pageable) {
         return tipPostRepository.findAllByIsDeletedFalseOrderByCreatedAtDesc(pageable).map(entity -> {
@@ -136,4 +144,25 @@ public class TipPostApplicationService {
         tipPostRepository.save(tipPostEntity);
     }
 
+    public Long countViews(String ulid) {
+        Long redisViewCount = tipPostViewCountRedisRepository.read(ulid);
+        if (redisViewCount != null) {
+            return redisViewCount;
+        }
+        Long dbViewCount = tipPostRepository.findByUlid(ulid)
+                .map(tipPostEntity -> Optional.ofNullable(tipPostEntity.getViewCount()).orElse(0L))
+                .orElseThrow(() -> new EntityNotFoundWithUlidException(ulid,String.class));
+        tipPostViewCountRedisRepository.write(ulid, dbViewCount);
+        return dbViewCount;
+    }
+
+    @Transactional
+    public Long increase(String ulid, UUID memberUuid) {
+        // 조회수 어뷰징 정책 - 사용자는 게시글 1개당 ttl에 1번 조회수 증가
+        if (!tipPostViewLockRedisRepository.lock(ulid,memberUuid,ttlMinutes)) {
+            return tipPostViewCountRedisRepository.read(ulid);
+        }
+        // 조회수 증가
+        return tipPostViewCountRedisRepository.increase(ulid);
+    }
 }
