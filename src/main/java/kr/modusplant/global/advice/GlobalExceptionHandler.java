c550d0d97149227e208cb4b337d7767ac6bdfd96
JunHyeok <akdnjs0308@gmail.com>
2025-05-26 18:29:20 +0900

    MP-184 :art: Format: 전역 예외 핸들러 메소드 순서 조정 및 주석 일부 수정
    * 더 범위가 넓은 예외가 더 위에 위치하도록 순서 변경
    * 주석의 서식을 통일

--- src/main/java/kr/modusplant/global/advice/GlobalExceptionHandler.java
+++ src/main/java/kr/modusplant/global/advice/GlobalExceptionHandler.java
@@ -16,13 +16,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // OAuthException 처리
-    @ExceptionHandler(OAuthException.class)
-    public ResponseEntity<DataResponse<Void>> handleOAuthException(OAuthException ex) {
-        return ResponseEntity.status(ex.getStatus()).body(DataResponse.of(ex.getStatus().value(),ex.getMessage()));
+    // Exception
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<DataResponse<Void>> handleGenericException(HttpServletRequest request, Exception ex) {
+        DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal server error occurred");
+        return ResponseEntity.internalServerError().body(errorResponse);
     }
 
-    // RuntimeException 처리
+    // RuntimeException
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<DataResponse<Void>> handleRuntimeException(HttpServletRequest request, RuntimeException ex) {
         DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "An unexpected error occurred");
@@ -30,35 +31,34 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
-    // 그 외 모든 Exception 처리
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<DataResponse<Void>> handleGenericException(HttpServletRequest request, Exception ex) {
-        DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal server error occurred");
-        return ResponseEntity.internalServerError().body(errorResponse);
+    // OAuth 이용 간 예외가 발생한 경우
+    @ExceptionHandler(OAuthException.class)
+    public ResponseEntity<DataResponse<Void>> handleOAuthException(OAuthException ex) {
+        return ResponseEntity.status(ex.getStatus()).body(DataResponse.of(ex.getStatus().value(), ex.getMessage()));
     }
 
-    // 검증로직 실패 시 예외 처리
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<DataResponse<Void>> handleValidationException(MethodArgumentNotValidException ex) {
-        DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Invalid client data");
-        return ResponseEntity.badRequest().body(errorResponse);
-    }
-
-    // 메서드의 인자가 무효한 값일 경우 처리
+    // 메서드의 인자가 유효하지 않은 값일 경우
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<DataResponse<Void>> handleIllegalArgumentException(IllegalArgumentException ex) {
         DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Invalid method argument");
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
-    // 호출된 메서드가 정상적으로 작동할 수 없는 경우 처리
+    // 검증이 실패한 경우
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<DataResponse<Void>> handleValidationException(MethodArgumentNotValidException ex) {
+        DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Invalid client data");
+        return ResponseEntity.badRequest().body(errorResponse);
+    }
+
+    // 호출된 메서드가 정상적으로 작동할 수 없는 경우
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<DataResponse<Void>> handleIllegalStateException(IllegalStateException ex) {
         DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.CONFLICT.value(), "resource is not available");
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 
-    // JSON 매핑 요청 처리
+    // JSON 매핑 요청 처리 간 예외가 발생한 경우
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<DataResponse<Void>> handleMalformedJsonException(HttpMessageNotReadableException ex) {
 
