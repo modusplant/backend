e8652ceebdb7e7f3fca8b9254172d25fdda5a10a
JunHyeok <akdnjs0308@gmail.com>
2025-05-27 01:14:54 +0900

    MP-184 :boom: BREAKING!: 전역 예외 핸들러 예외 추가 및 예외 메시지 일부 수정
    * 예외 메시지 서식 통일(대문자로 시작, 나머지는 소문자)
    * HttpMessageNotWritableException에 대한 예외 핸들러 추가
    * JsonParseException 예외 처리 로직 추가
    * 각 예외에 알맞은 예외 메시지가 포함되도록 함
    * 예외 핸들러 메소드명과 처리하는 예외 일치화
    * handleHttpMessageNotReadableException에서 Root Cause를 사용하도록 로직 수정
    * 테스트 메소드에 DisplayName 추가
    * 테스트 메소드 네이밍 통일(handle + 예외명 + test)
    * HttpMessageNotReadableException 테스트 메소드 삭제

--- src/main/java/kr/modusplant/global/advice/GlobalExceptionHandler.java
+++ src/main/java/kr/modusplant/global/advice/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package kr.modusplant.global.advice;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
@@ -9,6 +10,7 @@ import kr.modusplant.modules.auth.social.app.error.OAuthException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -40,46 +42,78 @@ public class GlobalExceptionHandler {
     // 메서드의 인자가 유효하지 않은 값일 경우
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<DataResponse<Void>> handleIllegalArgumentException(IllegalArgumentException ex) {
-        DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Invalid method argument");
+        DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Invalid client data");
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
     // 검증이 실패한 경우
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<DataResponse<Void>> handleValidationException(MethodArgumentNotValidException ex) {
-        DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Invalid client data");
+    public ResponseEntity<DataResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Invalid method argument");
         return ResponseEntity.badRequest().body(errorResponse);
     }
 
     // 호출된 메서드가 정상적으로 작동할 수 없는 경우
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<DataResponse<Void>> handleIllegalStateException(IllegalStateException ex) {
-        DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.CONFLICT.value(), "resource is not available");
+        DataResponse<Void> errorResponse = DataResponse.of(HttpStatus.CONFLICT.value(), "Not available resource");
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 
-    // JSON 매핑 요청 처리 간 예외가 발생한 경우
+    // 요청 처리 간 예외가 발생한 경우
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ResponseEntity<DataResponse<Void>> handleMalformedJsonException(HttpMessageNotReadableException ex) {
+    public ResponseEntity<DataResponse<Void>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
 
-        Throwable cause = ex.getCause();
+        Throwable cause = ex.getRootCause();
         DataResponse<Void> errorResponse;
 
         switch (cause) {
             case InvalidFormatException ignored -> {
-                errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "value cannot be deserialized to expected type");
+                errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Value cannot be deserialized to expected type");
             }
             case UnrecognizedPropertyException ignored -> {
-                errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "body has property that target class do not know");
+                errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Body has property that target class do not know");
             }
             case JsonMappingException ignored -> {
-                errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "parsing body and Java object failed");
+                errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Mapping to body and Java object failed");
             }
-            case null, default -> {
-                errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "malformed request body");
+            case JsonParseException ignored -> {
+                errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Parsing body and Java object failed");
+            }
+            default -> {
+                errorResponse = DataResponse.of(HttpStatus.BAD_REQUEST.value(), "Malformed request body");
             }
         }
 
         return ResponseEntity.badRequest().body(errorResponse);
     }
+
+    // 응답 처리 간 예외가 발생한 경우
+    @ExceptionHandler(HttpMessageNotWritableException.class)
+    public ResponseEntity<DataResponse<Void>> handleHttpMessageNotWritableException(HttpMessageNotWritableException ex) {
+
+        Throwable cause = ex.getRootCause();
+        DataResponse<Void> errorResponse;
+
+        switch (cause) {
+            case InvalidFormatException ignored -> {
+                errorResponse = DataResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Value cannot be deserialized to expected type");
+            }
+            case UnrecognizedPropertyException ignored -> {
+                errorResponse = DataResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Body has property that target class do not know");
+            }
+            case JsonMappingException ignored -> {
+                errorResponse = DataResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Mapping to body and Java object failed");
+            }
+            case JsonParseException ignored -> {
+                errorResponse = DataResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Parsing body and Java object failed");
+            }
+            default -> {
+                errorResponse = DataResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Malformed request body");
+            }
+        }
+
+        return ResponseEntity.internalServerError().body(errorResponse);
+    }
+
 }
