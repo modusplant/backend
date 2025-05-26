6d54df52b5ba449fdcc153d2f7de30edff22568d
JunHyeok <akdnjs0308@gmail.com>
2025-05-27 01:14:54 +0900

    MP-184 :boom: BREAKING!: 전역 예외 핸들러 예외 추가 및 예외 메시지 일부 수정
    * 예외 메시지 서식 통일(대문자로 시작, 나머지는 소문자)
    * HttpMessageNotWritableException에 대한 예외 핸들러 추가
    * JsonParseException 예외 처리 로직 추가
    * 각 예외에 알맞은 예외 메시지가 포함되도록 함
    * 예외 핸들러 메소드명과 처리하는 예외 일치화
    * handleHttpMessageNotReadableException에서 Root Cause를 사용하도록 로직 수정
    * 테스트 메소드에 DisplayName 추가
    * 테스트 메소드 네이밍 통일(handle + 예외명 + test)
    * HttpMessageNotReadableException 테스트 메소드 삭제

--- src/test/java/kr/modusplant/global/advice/GlobalExceptionHandlerUnitTest.java
+++ src/test/java/kr/modusplant/global/advice/GlobalExceptionHandlerUnitTest.java
@@ -1,17 +1,23 @@
 package kr.modusplant.global.advice;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import jakarta.servlet.http.HttpServletRequest;
 import kr.modusplant.global.app.servlet.response.DataResponse;
+import kr.modusplant.modules.auth.social.app.error.OAuthException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpInputMessage;
+import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,25 +29,9 @@ public class GlobalExceptionHandlerUnitTest {
     @InjectMocks
     private GlobalExceptionHandler globalExceptionHandler;
 
+    @DisplayName("Exception 처리")
     @Test
-    public void handleRuntimeException_givenValidCondition_thenReturnMap() {
-        // given
-        RuntimeException ex = mock(RuntimeException.class);
-        HttpServletRequest servletRequest = mock(HttpServletRequest.class);
-
-        // when
-        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleRuntimeException(servletRequest, ex);
-        DataResponse<Void> errorResponse = response.getBody();
-
-        // then
-        assertNotNull(errorResponse);
-        assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
-        assertNotNull(errorResponse.getMessage());
-        assertNull(errorResponse.getData());
-    }
-
-    @Test
-    public void handleGenericException_givenValidCondition_thenReturnMap() {
+    public void handleGenericExceptionTest() {
         // given
         HttpServletRequest servletRequest = mock(HttpServletRequest.class);
         Exception ex = mock(Exception.class);
@@ -57,8 +47,43 @@ public class GlobalExceptionHandlerUnitTest {
         assertNull(errorResponse.getData());
     }
 
+    @DisplayName("RuntimeException 처리")
     @Test
-    public void handleIllegalArgumentException_givenValidCondition_thenReturnProblemDetail() {
+    public void handleRuntimeExceptionTest() {
+        // given
+        RuntimeException ex = mock(RuntimeException.class);
+        HttpServletRequest servletRequest = mock(HttpServletRequest.class);
+
+        // when
+        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleRuntimeException(servletRequest, ex);
+        DataResponse<Void> errorResponse = response.getBody();
+
+        // then
+        assertNotNull(errorResponse);
+        assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
+        assertNotNull(errorResponse.getMessage());
+        assertNull(errorResponse.getData());
+    }
+
+    @DisplayName("OAuthException 처리")
+    @Test
+    public void handleOAuthExceptionTest() {
+        // given
+        OAuthException ex = new OAuthException(HttpStatus.BAD_REQUEST);
+
+        // when
+        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleOAuthException(ex);
+        DataResponse<Void> errorResponse = response.getBody();
+
+        // then
+        assertNotNull(errorResponse);
+        assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
+        assertEquals(ex.getMessage(), errorResponse.getMessage());
+    }
+
+    @DisplayName("IllegalArgumentException 처리")
+    @Test
+    public void handleIllegalArgumentExceptionTest() {
         // given
         IllegalArgumentException ex = mock(IllegalArgumentException.class);
 
@@ -69,11 +94,29 @@ public class GlobalExceptionHandlerUnitTest {
         // then
         assertNotNull(errorResponse);
         assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
-        assertEquals("Invalid method argument", errorResponse.getMessage());
+        assertEquals("Invalid client data", errorResponse.getMessage());
     }
 
+    @DisplayName("MethodArgumentNotValidException 처리")
     @Test
-    public void handleIllegalStateException_givenValidCondition_thenReturnProblemDetail() {
+    public void handleMethodArgumentNotValidExceptionTest() {
+        // given
+        MethodArgumentNotValidException ex = mock(MethodArgumentNotValidException.class);
+
+        // when
+        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleMethodArgumentNotValidException(ex);
+        DataResponse<Void> errorResponse = response.getBody();
+
+        // then
+        assertNotNull(errorResponse);
+        assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
+        assertEquals("Invalid method argument", errorResponse.getMessage());
+        assertNull(errorResponse.getData());
+    }
+
+    @DisplayName("IllegalStateException 처리")
+    @Test
+    public void handleIllegalStateExceptionTest() {
         // given
         IllegalStateException ex = mock(IllegalStateException.class);
 
@@ -84,89 +127,158 @@ public class GlobalExceptionHandlerUnitTest {
         // then
         assertNotNull(errorResponse);
         assertEquals(HttpStatus.CONFLICT.value(), errorResponse.getStatus());
-        assertEquals("resource is not available", errorResponse.getMessage());
+        assertEquals("Not available resource", errorResponse.getMessage());
     }
 
+    @DisplayName("요청 간 InvalidFormatException 처리")
     @Test
-    public void handleValidationException_givenValidCondition_thenReturnProblemDetail() {
-        // given
-        MethodArgumentNotValidException ex = mock(MethodArgumentNotValidException.class);
-
-        // when
-        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleValidationException(ex);
-        DataResponse<Void> errorResponse = response.getBody();
-
-        // then
-        assertNotNull(errorResponse);
-        assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
-        assertEquals("Invalid client data", errorResponse.getMessage());
-        assertNull(errorResponse.getData());
-    }
-
-    @Test
-    public void handleValidationException_givenEmptyFieldErrors_thenReturnProblemDetail() {
-        // given
-        MethodArgumentNotValidException ex = mock(MethodArgumentNotValidException.class);
-
-        // when
-        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleValidationException(ex);
-        DataResponse<Void> errorResponse = response.getBody();
-
-        // then
-        assertNotNull(errorResponse);
-        assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
-        assertEquals("Invalid client data", errorResponse.getMessage());
-        assertNull(errorResponse.getData());
-    }
-
-    @Test
-    public void handleMalformedJsonException_givenValidCondition_thenReturnProblemDetail() {
-        // given
-        HttpMessageNotReadableException ex = mock(HttpMessageNotReadableException.class);
-
-        // when
-        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleMalformedJsonException(ex);
-        DataResponse<Void> errorResponse = response.getBody();
-
-        // then
-        assertNotNull(errorResponse);
-        assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
-        assertEquals("malformed request body", errorResponse.getMessage());
-        assertNull(errorResponse.getData());
-    }
-
-    @Test
-    public void handleMalformedJsonException_givenInvalidFormatException_thenReturnProblemDetail() {
+    public void handleInvalidFormatExceptionOnRequestTest() {
         // given
         InvalidFormatException ifx = new InvalidFormatException(null, "Invalid format", "value", Integer.class);
         HttpInputMessage inputMessage = mock(HttpInputMessage.class);
         HttpMessageNotReadableException ex = new HttpMessageNotReadableException("error", ifx, inputMessage);
 
         // when
-        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleMalformedJsonException(ex);
+        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleHttpMessageNotReadableException(ex);
         DataResponse<Void> errorResponse = response.getBody();
 
         // then
         assertNotNull(errorResponse);
         assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
-        assertEquals("value cannot be deserialized to expected type", errorResponse.getMessage());
+        assertEquals("Value cannot be deserialized to expected type", errorResponse.getMessage());
         assertNull(errorResponse.getData());
     }
 
-    @Test void handleMalformedJsonException_givenUnrecognizedPropertyException_thenReturnProblemDetail() {
+    @DisplayName("요청 간 UnrecognizedPropertyException 처리")
+    @Test
+    void handleUnrecognizedPropertyExceptionOnRequestTest() {
         // given
         UnrecognizedPropertyException upx = new UnrecognizedPropertyException(null, null, null, null, null, null);
         HttpInputMessage inputMessage = mock(HttpInputMessage.class);
         HttpMessageNotReadableException ex = new HttpMessageNotReadableException("error", upx, inputMessage);
 
         // when
-        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleMalformedJsonException(ex);
+        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleHttpMessageNotReadableException(ex);
         DataResponse<Void> errorResponse = response.getBody();
 
         // then
         assertNotNull(errorResponse);
         assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
-        assertEquals("body has property that target class do not know", errorResponse.getMessage());
+        assertEquals("Body has property that target class do not know", errorResponse.getMessage());
+        assertNull(errorResponse.getData());
+    }
+
+    @DisplayName("요청 간 JsonMappingException 처리")
+    @Test
+    void handleJsonMappingExceptionOnRequestTest() {
+        // given
+        JsonMappingException jmx = mock(JsonMappingException.class);
+        HttpInputMessage inputMessage = mock(HttpInputMessage.class);
+        HttpMessageNotReadableException ex = new HttpMessageNotReadableException("error", jmx, inputMessage);
+
+        // when
+        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleHttpMessageNotReadableException(ex);
+        DataResponse<Void> errorResponse = response.getBody();
+
+        // then
+        assertNotNull(errorResponse);
+        assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
+        assertEquals("Mapping to body and Java object failed", errorResponse.getMessage());
+        assertNull(errorResponse.getData());
+    }
+
+    @DisplayName("요청 간 JsonParseException 처리")
+    @Test
+    void handleJsonParseExceptionOnRequestTest() {
+        // given
+        JsonParseException jpx = mock(JsonParseException.class);
+        HttpInputMessage inputMessage = mock(HttpInputMessage.class);
+        HttpMessageNotReadableException ex = new HttpMessageNotReadableException("error", jpx, inputMessage);
+
+        // when
+        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleHttpMessageNotReadableException(ex);
+        DataResponse<Void> errorResponse = response.getBody();
+
+        // then
+        assertNotNull(errorResponse);
+        assertEquals(HttpStatus.BAD_REQUEST.value(), errorResponse.getStatus());
+        assertEquals("Parsing body and Java object failed", errorResponse.getMessage());
+        assertNull(errorResponse.getData());
+    }
+
+    @DisplayName("응답 간 InvalidFormatException 처리")
+    @Test
+    public void handleInvalidFormatExceptionOnResponseTest() {
+        // given
+        InvalidFormatException ifx = new InvalidFormatException(null, "Invalid format", "value", Integer.class);
+        HttpOutputMessage outputMessage = mock(HttpOutputMessage.class);
+        HttpMessageNotWritableException ex = new HttpMessageNotWritableException(outputMessage.toString(), ifx);
+
+        // when
+        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleHttpMessageNotWritableException(ex);
+        DataResponse<Void> errorResponse = response.getBody();
+
+        // then
+        assertNotNull(errorResponse);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), errorResponse.getStatus());
+        assertEquals("Value cannot be deserialized to expected type", errorResponse.getMessage());
+        assertNull(errorResponse.getData());
+    }
+
+    @DisplayName("응답 간 UnrecognizedPropertyException 처리")
+    @Test
+    void handleUnrecognizedPropertyExceptionOnResponseTest() {
+        // given
+        UnrecognizedPropertyException upx = new UnrecognizedPropertyException(null, null, null, null, null, null);
+        HttpOutputMessage outputMessage = mock(HttpOutputMessage.class);
+        HttpMessageNotWritableException ex = new HttpMessageNotWritableException(outputMessage.toString(), upx);
+
+        // when
+        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleHttpMessageNotWritableException(ex);
+        DataResponse<Void> errorResponse = response.getBody();
+
+        // then
+        assertNotNull(errorResponse);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), errorResponse.getStatus());
+        assertEquals("Body has property that target class do not know", errorResponse.getMessage());
+        assertNull(errorResponse.getData());
+    }
+
+    @DisplayName("응답 간 JsonMappingException 처리")
+    @Test
+    void handleJsonMappingExceptionOnResponseTest() {
+        // given
+        JsonMappingException jmx = mock(JsonMappingException.class);
+        HttpOutputMessage outputMessage = mock(HttpOutputMessage.class);
+        HttpMessageNotWritableException ex = new HttpMessageNotWritableException(outputMessage.toString(), jmx);
+
+        // when
+        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleHttpMessageNotWritableException(ex);
+        DataResponse<Void> errorResponse = response.getBody();
+
+        // then
+        assertNotNull(errorResponse);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), errorResponse.getStatus());
+        assertEquals("Mapping to body and Java object failed", errorResponse.getMessage());
+        assertNull(errorResponse.getData());
+    }
+
+    @DisplayName("응답 간 JsonParseException 처리")
+    @Test
+    void handleJsonParseExceptionOnResponseTest() {
+        // given
+        JsonParseException jpx = mock(JsonParseException.class);
+        HttpOutputMessage outputMessage = mock(HttpOutputMessage.class);
+        HttpMessageNotWritableException ex = new HttpMessageNotWritableException(outputMessage.toString(), jpx);
+
+        // when
+        ResponseEntity<DataResponse<Void>> response = globalExceptionHandler.handleHttpMessageNotWritableException(ex);
+        DataResponse<Void> errorResponse = response.getBody();
+
+        // then
+        assertNotNull(errorResponse);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR.value(), errorResponse.getStatus());
+        assertEquals("Parsing body and Java object failed", errorResponse.getMessage());
         assertNull(errorResponse.getData());
     }
 }
