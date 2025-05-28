bbcf5958d70f92f77c80666711810459d08d7051
JunHyeok <akdnjs0308@gmail.com>
2025-05-28 18:58:23 +0900

    MP-188 :recycle: Refactor: 요청 및 응답 생성 테스트 클래스 내 객체명 수정
    * Test라는 단어를 추가하여 충돌 가능성 방지

--- src/test/java/kr/modusplant/domains/communication/qna/common/util/app/http/request/QnaCategoryRequestTestUtils.java
+++ src/test/java/kr/modusplant/domains/communication/qna/common/util/app/http/request/QnaCategoryRequestTestUtils.java
@@ -4,5 +4,5 @@ import kr.modusplant.domains.communication.qna.app.http.request.QnaCategoryInser
 import kr.modusplant.domains.communication.qna.common.util.domain.QnaCategoryTestUtils;
 
 public interface QnaCategoryRequestTestUtils extends QnaCategoryTestUtils {
-    QnaCategoryInsertRequest qnaCategoryInsertRequest = new QnaCategoryInsertRequest(qnaCategory.getOrder(), qnaCategory.getCategory());
+    QnaCategoryInsertRequest qnaCategoryTestInsertRequest = new QnaCategoryInsertRequest(qnaCategory.getOrder(), qnaCategory.getCategory());
 }
