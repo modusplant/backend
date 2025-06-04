c01eda1469c4704b42180bf3ced2e7fb60273374
Jun Hee <umpa9327@gmail.com>
2025-06-04 22:03:04 +0900

    MP-160 :white_check_mark: Test: 일반 로그인의 테스트 클래스에 성공 인증 핸들러를 반영함

--- src/test/java/kr/modusplant/modules/auth/normal/login/app/controller/NormalLoginControllerIntegrationTest.java
+++ src/test/java/kr/modusplant/modules/auth/normal/login/app/controller/NormalLoginControllerIntegrationTest.java
@@ -1,11 +1,13 @@
 package kr.modusplant.modules.auth.normal.login.app.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import kr.modusplant.domains.member.enums.AuthProvider;
 import kr.modusplant.global.middleware.security.SiteMemberUserDetailsService;
+import kr.modusplant.global.middleware.security.common.util.SiteMemberUserDetailsTestUtils;
 import kr.modusplant.global.middleware.security.config.SecurityConfig;
 import kr.modusplant.global.middleware.security.models.SiteMemberUserDetails;
 import kr.modusplant.modules.auth.normal.login.app.http.NormalLoginRequest;
+import kr.modusplant.modules.jwt.app.service.RefreshTokenApplicationService;
+import kr.modusplant.modules.jwt.domain.service.TokenValidationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,30 +15,23 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.List;
-import java.util.UUID;
-
-import static org.awaitility.Awaitility.given;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Import(SecurityConfig.class)
-public class NormalLoginControllerIntegrationTest {
+public class NormalLoginControllerIntegrationTest implements SiteMemberUserDetailsTestUtils {
 
     private NormalLoginRequest testLoginRequest;
 
@@ -55,11 +50,15 @@ public class NormalLoginControllerIntegrationTest {
     @MockitoBean
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @MockitoBean
+    private TokenValidationService tokenValidationService;
+
+    @MockitoBean
+    private RefreshTokenApplicationService refreshTokenApplicationService;
+
     @BeforeEach
     void setUp() {
-        UUID testActiveUuid = UUID.randomUUID();
-        UUID testDeviceId = UUID.randomUUID();
-        testLoginRequest = new NormalLoginRequest("akdnjs0308@gmail.com", "userPw2!", testDeviceId.toString());
+        testLoginRequest = new NormalLoginRequest("akdnjs0308@gmail.com", "userPw2!");
 
         when(bCryptPasswordEncoder.encode("userPw2!"))
                 .thenReturn("$2a$10$N9qo8uLOickgx2ZMRZoMy.M3qaW7D3oEfgL7iPTGgR7P92FpZ9X1e");
@@ -69,18 +68,7 @@ public class NormalLoginControllerIntegrationTest {
         when(bCryptPasswordEncoder.matches(anyString(), anyString()))
                 .thenReturn(true);
 
-        SiteMemberUserDetails testUserDetails = SiteMemberUserDetails.builder()
-                .email("akdnjs0308@gmail.com")
-                .password(bCryptPasswordEncoder.encode("userPw2!"))
-                .activeUuid(testActiveUuid)
-                .nickname("테스트닉네임")
-                .provider(AuthProvider.BASIC)
-                .isActive(true)
-                .isDisabledByLinking(false)
-                .isBanned(false)
-                .isDeleted(false)
-                .authorities(List.of(new SimpleGrantedAuthority("ROLE_USER")))
-                .build();
+        SiteMemberUserDetails testUserDetails = testSiteMemberUserDetails;
 
         when(memberUserDetailsService.loadUserByUsername(testLoginRequest.email()))
                 .thenReturn(testUserDetails);
@@ -88,6 +76,11 @@ public class NormalLoginControllerIntegrationTest {
 
     @Test
     public void processLogin_givenValidUser_thenReturnDataResponse() throws Exception {
+
+        doNothing().when(tokenValidationService).validateNotFoundMemberUuid(any(), any());
+        doNothing().when(tokenValidationService).validateExistedDeviceId(any());
+        given(refreshTokenApplicationService.insert(any())).willReturn(any());
+
         mockMvc.perform(post("/api/auth/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(testLoginRequest)).characterEncoding("UTF-8"))
