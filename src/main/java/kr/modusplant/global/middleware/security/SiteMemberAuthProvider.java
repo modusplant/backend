88c050b3bd2755f17cc1cf6c99afd840f2515594
Jun Hee <umpa9327@gmail.com>
2025-06-04 21:58:27 +0900

    MP-160 :sparkles: Feat: 커스텀 토큰의 필드의 역할이 변경된 사항을 반영함
    - credential과 principal이 담는 데이터가 바뀐 점을 인증 과정에 반영함

--- src/main/java/kr/modusplant/global/middleware/security/SiteMemberAuthProvider.java
+++ src/main/java/kr/modusplant/global/middleware/security/SiteMemberAuthProvider.java
@@ -16,15 +16,15 @@ public class SiteMemberAuthProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String email = authentication.getCredentials().toString();
-        String password = authentication.getPrincipal().toString();
-        // TODO: 테스트가 완료된 후에 출력 코드를 지우고 인증 로직도 복구하 것.
+        String email = authentication.getPrincipal().toString();
+        String password = authentication.getCredentials().toString();
+        // TODO: 테스트가 완료된 후에 출력 코드를 지우고 인증 로직도 복구할 것.
         System.out.println("The email from authProvider: " + email);
 
         SiteMemberUserDetails userDetails = memberUserDetailsService.loadUserByUsername(email);
 
 //        if(passwordEncoder.matches(password, userDetails.getPassword())) {
-            System.out.println("The credential was valid.");
+            System.out.println("The credentials are valid.");
 //            return new SiteMemberAuthToken(userDetails, userDetails.getAuthorities());
 //        } else {
 //            System.out.println("The credential was invalid.");
