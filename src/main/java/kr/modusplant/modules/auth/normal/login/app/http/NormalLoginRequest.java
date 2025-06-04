6c9d3c60cbb5935eea46fbb848a04b263ad12044
Jun Hee <umpa9327@gmail.com>
2025-06-04 21:52:22 +0900

    MP-160 :rewind: Revert: 일반 로그인 요청에 기기 id 제거
    - 백엔드에서 기기 id를 제공해주기로 합의된 점을 반영함

--- src/main/java/kr/modusplant/modules/auth/normal/login/app/http/NormalLoginRequest.java
+++ src/main/java/kr/modusplant/modules/auth/normal/login/app/http/NormalLoginRequest.java
@@ -2,10 +2,9 @@ package kr.modusplant.modules.auth.normal.login.app.http;
 
 public record NormalLoginRequest(
         String email,
-        String password,
-        String deviceID
+        String password
 ) {
     public boolean checkFieldValidation() {
-        return email != null && password != null && deviceID != null;
+        return email != null && password != null;
     }
 }
