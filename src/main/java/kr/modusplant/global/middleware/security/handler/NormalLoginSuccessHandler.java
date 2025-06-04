4b5b570a2126a8621c9e2e20cbaa057c6e1add0f
Jun Hee <umpa9327@gmail.com>
2025-06-04 21:54:21 +0900

    MP-160 :sparkles: Feat: 인증 성공 핸들러와 실패 핸들러 추가
    - 커스텀 필터에서 인증을 완료한 후 인증 마무리 과정을 구성하기 위해 생성함

---
+++ src/main/java/kr/modusplant/global/middleware/security/handler/NormalLoginSuccessHandler.java
@@ -0,0 +1,51 @@
+package kr.modusplant.global.middleware.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.modusplant.global.enums.Role;
+import kr.modusplant.global.middleware.security.models.SiteMemberUserDetails;
+import kr.modusplant.modules.jwt.app.dto.TokenPair;
+import kr.modusplant.modules.jwt.app.service.TokenApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class NormalLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final TokenApplicationService tokenApplicationService;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+
+        SiteMemberUserDetails currentUser =
+                (SiteMemberUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        UUID deviceIdToSend = UUID.randomUUID();
+
+        TokenPair loginTokenPair = tokenApplicationService.issueToken(
+                currentUser.getActiveUuid(), currentUser.getNickname(), getMemberRole(currentUser), deviceIdToSend
+        );
+
+        request.setAttribute("authentication", authentication);
+        request.setAttribute("accessToken", loginTokenPair);
+        request.setAttribute("refreshToken", loginTokenPair);
+
+        request.getRequestDispatcher("/api/auth/login/initial").forward(request, response);
+    }
+
+    private Role getMemberRole(SiteMemberUserDetails memberUserDetails) {
+        GrantedAuthority memberRole = memberUserDetails.getAuthorities().stream()
+                .filter(auth -> auth.getAuthority().startsWith("ROLE_"))
+                .findFirst().orElseThrow(() -> new IllegalArgumentException("The authenticated user does not have role"));
+
+        return Role.valueOf(memberRole.getAuthority());
+    }
+}
