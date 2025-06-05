0e226571ae5f2f0d69ac73748c4f4f096e761339
JunHyeok <akdnjs0308@gmail.com>
2025-06-05 23:55:33 +0900

    MP-202 :fire: Remove: 미사용 파일 제거

--- src/main/java/kr/modusplant/global/vo/EscapedWord.java
+++
@@ -1,9 +0,0 @@
-package kr.modusplant.global.vo;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class EscapedWord {
-    public static final String ORDER = "`order`";
-}
