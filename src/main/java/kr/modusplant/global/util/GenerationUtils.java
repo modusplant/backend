c134b6f40f66c9218dfd2ac0e5d1d036160672a8
JunHyeok <akdnjs0308@gmail.com>
2025-06-07 19:54:39 +0900

    MP-202 :fire: Remove: 더이상 필요하지 않은 GenerationUtils 제거

--- src/main/java/kr/modusplant/global/util/GenerationUtils.java
+++
@@ -1,9 +0,0 @@
-package kr.modusplant.global.util;
-
-import java.util.UUID;
-
-public abstract class GenerationUtils {
-    public static UUID generateDeviceId() {
-        return UUID.randomUUID();
-    }
-}
