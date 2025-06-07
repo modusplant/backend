5896bddc221181c586f285cb32a5ad5d1b63e3db
JunHyeok <akdnjs0308@gmail.com>
2025-06-07 21:14:21 +0900

    MP-202 :wrench: Chore: 일부 경고 사항 조치

--- src/main/java/kr/modusplant/modules/example/ExampleService.java
+++ src/main/java/kr/modusplant/modules/example/ExampleService.java
@@ -30,12 +30,12 @@ public class ExampleService {
 
             // 만료시간 : 10초
             String redisKey2 = "test-redis-expire-key-10sec";
-            String value2 = "Test String Value 2(10sec), Expire Time : " + LocalDateTime.now().plusSeconds(10).toString();
+            String value2 = "Test String Value 2(10sec), Expire Time : " + LocalDateTime.now().plusSeconds(10);
             redisHelper.setString(redisKey2, value2, Duration.ofSeconds(10));
 
             // 만료시간 : 1분
             String redisKey3 = "test-redis-expire-key-1min";
-            String value3 = "Test String Value 3(1min), Expire Time : " + LocalDateTime.now().plusMinutes(1).toString();
+            String value3 = "Test String Value 3(1min), Expire Time : " + LocalDateTime.now().plusMinutes(1);
             redisHelper.setString(redisKey3, value3, Duration.ofMinutes(1));
         } catch (Exception e) {
             throw new RuntimeException("Redis storage test error occurred."); // 예외 발생
