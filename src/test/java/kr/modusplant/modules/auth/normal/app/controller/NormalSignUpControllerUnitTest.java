46f0c0edbc38f29855ecf332e392a9a5896c5637
JunHyeok <akdnjs0308@gmail.com>
2025-06-08 15:43:08 +0900

    MP-202 :white_check_mark: Test: @SpringBootTest에 @Transactional 추가
    * 테스트에서 롤백이 이루어지지 않는 문제 해결

--- src/test/java/kr/modusplant/modules/auth/normal/app/controller/NormalSignUpControllerUnitTest.java
+++ src/test/java/kr/modusplant/modules/auth/normal/app/controller/NormalSignUpControllerUnitTest.java
@@ -11,12 +11,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Transactional
 @AutoConfigureMockMvc(addFilters = false)
 public class NormalSignUpControllerUnitTest implements SiteMemberResponseTestUtils, SiteMemberAuthResponseTestUtils, SiteMemberTermResponseTestUtils {
 
