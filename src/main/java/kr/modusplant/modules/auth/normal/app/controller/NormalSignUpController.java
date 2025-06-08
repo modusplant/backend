d93c0105c3a456fec8ef9d9635ae5410ee109e00
JunHyeok <akdnjs0308@gmail.com>
2025-06-08 11:57:14 +0900

    MP-202 :fire: Remove: 이용약관 정보 전달 API 관련 기능 완전 제거
    * 기획 변경으로 인한 미사용 코드 폐기

--- src/main/java/kr/modusplant/modules/auth/normal/app/controller/NormalSignUpController.java
+++ src/main/java/kr/modusplant/modules/auth/normal/app/controller/NormalSignUpController.java
@@ -3,20 +3,15 @@ package kr.modusplant.modules.auth.normal.app.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import kr.modusplant.domains.term.app.http.response.TermResponse;
 import kr.modusplant.global.app.http.response.DataResponse;
 import kr.modusplant.modules.auth.normal.app.http.request.NormalSignUpRequest;
 import kr.modusplant.modules.auth.normal.app.service.NormalSignUpApplicationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @Slf4j
@@ -24,26 +19,6 @@ import java.util.Map;
 public class NormalSignUpController {
 
     private final NormalSignUpApplicationService normalSignUpApplicationService;
-
-    @Operation(
-            summary = "이용약관 정보 전달 API",
-            description = "회원가입 시 이용약관 정보를 클라이언트에 전달합니다."
-    )
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "OK: Succeeded"),
-            @ApiResponse(responseCode = "500", description = "Internal Server Error: Fail to fetch data")
-    })
-    @GetMapping("/api/terms")
-    public ResponseEntity<DataResponse<?>> sendTerms(){
-
-        List<TermResponse> terms = normalSignUpApplicationService.getAllTerms();
-        List<Map<String, Object>> termMapList = normalSignUpApplicationService.createTermMapList(terms);
-
-        DataResponse<List<Map<String, Object>>> successDataResponse =
-                DataResponse.of(200, "terms info successfully fetched", termMapList);
-
-        return ResponseEntity.ok(successDataResponse);
-    }
 
     @Operation(
             summary = "일반 회원가입 API",
--- src/test/java/kr/modusplant/modules/auth/normal/app/controller/NormalSignUpControllerUnitTest.java
+++ src/test/java/kr/modusplant/modules/auth/normal/app/controller/NormalSignUpControllerUnitTest.java
@@ -1,31 +1,17 @@
 package kr.modusplant.modules.auth.normal.app.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import kr.modusplant.domains.member.app.service.SiteMemberApplicationService;
-import kr.modusplant.domains.member.app.service.SiteMemberAuthApplicationService;
-import kr.modusplant.domains.member.app.service.SiteMemberTermApplicationService;
 import kr.modusplant.domains.member.common.util.app.http.response.SiteMemberAuthResponseTestUtils;
 import kr.modusplant.domains.member.common.util.app.http.response.SiteMemberResponseTestUtils;
 import kr.modusplant.domains.member.common.util.app.http.response.SiteMemberTermResponseTestUtils;
-import kr.modusplant.domains.member.common.util.domain.SiteMemberTestUtils;
-import kr.modusplant.domains.member.enums.AuthProvider;
-import kr.modusplant.domains.term.app.service.TermApplicationService;
-import kr.modusplant.domains.term.common.util.app.http.response.TermResponseTestUtils;
 import kr.modusplant.modules.auth.normal.app.http.request.NormalSignUpRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-import java.util.UUID;
-
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.argThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -38,32 +24,6 @@ public class NormalSignUpControllerUnitTest implements SiteMemberResponseTestUti
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
-    @MockitoBean
-    private TermApplicationService termApplicationService;
-    @MockitoBean
-    private SiteMemberApplicationService siteMemberApplicationService;
-    @MockitoBean
-    private SiteMemberAuthApplicationService siteMemberAuthApplicationService;
-    @MockitoBean
-    private SiteMemberTermApplicationService siteMemberTermApplicationService;
-
-    @Test
-    public void sendTerms_givenRequestingTerm_thenReturn200WithTerm() throws Exception {
-        // given
-        given(termApplicationService.getAll())
-                .willReturn(List.of(TermResponseTestUtils.termsOfUseResponse));
-
-        // when
-        mockMvc.perform(get("/api/terms"))
-
-                // then
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status").value(200))
-                .andExpect(jsonPath("$.message").exists())
-                .andExpect(jsonPath("$.data").isArray())
-                .andExpect(jsonPath("$.data[*].termsOfUse").exists());
-
-    }
 
     @Test
     public void saveMember_givenValidInput_thenReturn200() throws Exception {
@@ -72,7 +32,6 @@ public class NormalSignUpControllerUnitTest implements SiteMemberResponseTestUti
                 "akdnjs0308@gmail.com", "userPw2!",
                 "테스트닉네임", "v1.0.0", "v1.0.0", "v1.0.0");
         String testRequestBody = objectMapper.writeValueAsString(validData);
-        setupServiceStubbing();
 
         //when
         mockMvc.perform(post("/api/members/register")
@@ -83,28 +42,5 @@ public class NormalSignUpControllerUnitTest implements SiteMemberResponseTestUti
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.status").value(200))
                 .andExpect(jsonPath("$.message").exists());
-    }
-
-    private void setupServiceStubbing() {
-        UUID consistentMemberUuid = SiteMemberTestUtils.memberBasicUserWithUuid.getUuid();
-
-        given(siteMemberApplicationService
-                .insert(argThat(member ->
-                        member != null &&
-                                member.nickname() != null)))
-                .willReturn(memberBasicUserResponse);
-
-        given(siteMemberAuthApplicationService
-                .insert(argThat(auth ->
-                        auth != null &&
-                                auth.originalMemberUuid().equals(consistentMemberUuid) &&
-                                auth.provider().equals(AuthProvider.BASIC))))
-                .willReturn(memberAuthBasicUserResponse);
-
-        given(siteMemberTermApplicationService
-                .insert(argThat(memberTerm ->
-                        memberTerm != null &&
-                                memberTerm.uuid().equals(consistentMemberUuid))))
-                .willReturn(memberTermUserResponse);
     }
 }
