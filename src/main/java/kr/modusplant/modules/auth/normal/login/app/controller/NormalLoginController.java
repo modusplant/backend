f4b3671028f7ac2f74604e0c1617e7a1b8512985
Jun Hee <umpa9327@gmail.com>
2025-06-08 18:43:18 +0900

    MP-160 :sparkles: Feat: 로그인 컨트롤러에 인증 성공과 실패에 따른 메서드들을 개별적으로 구현함

--- src/main/java/kr/modusplant/modules/auth/normal/login/app/controller/NormalLoginController.java
+++ src/main/java/kr/modusplant/modules/auth/normal/login/app/controller/NormalLoginController.java
@@ -1,9 +1,10 @@
 package kr.modusplant.modules.auth.normal.login.app.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import kr.modusplant.global.app.servlet.response.DataResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,13 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class NormalLoginController {
 
-    @PostMapping("/login")
-    public DataResponse<Void> processLogin(HttpServletRequest request) {
+    @PostMapping("/login-success")
+    public ResponseEntity sendLoginSuccess(HttpServletRequest request) {
+        System.out.println("Arrived at the success controller method.");
+
         Authentication auth = (Authentication) request.getAttribute("authentication");
-        System.out.println("The auth from the filters: {}" + auth);
+        System.out.println("The auth from the filters: " + auth);
 
+        return ResponseEntity.ok().build();
+    }
 
-        // TODO: JWT 토큰 생성 후 반환
-        return DataResponse.ok();
+    @PostMapping("/login-fail")
+    public ResponseEntity sendLoginFailure(HttpServletRequest request) {
+        System.out.println("Arrived at the failure controller method.");
+        AuthenticationException authException = (AuthenticationException) request.getAttribute("exception");
+        System.out.println("The error message: " + authException.getMessage());
+
+        return ResponseEntity.badRequest().build();
     }
 }
