8b08fbdf44f8d6865c1df3b92e1e68c5e89821cb
Jun Hee <umpa9327@gmail.com>
2025-06-10 16:53:05 +0900

    MP-160 :truck: Rename: 일반 로그인 테스트 유틸리티 클래스의 위치를 common의 util 하부로 이동함

--- src/test/java/kr/modusplant/modules/auth/normal/login/app/http/NormalLoginRequestTestUtils.java
+++
@@ -1,6 +0,0 @@
-package kr.modusplant.modules.auth.normal.login.app.http;
-
-public interface NormalLoginRequestTestUtils {
-
-    NormalLoginRequest testLoginRequest = new NormalLoginRequest("akdnjs0308@gmail.com", "userPw2!");
-}
