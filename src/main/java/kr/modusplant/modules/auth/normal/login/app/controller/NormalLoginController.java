7feb52e6bea45cfc6ac54f1b42d489f3e09def06
Jun Hee <umpa9327@gmail.com>
2025-06-10 12:14:48 +0900

    MP-160 :sparkles: Feat: 인증 성공 컨트롤러 메서드에서 응답 생성

--- src/main/java/kr/modusplant/modules/auth/normal/login/app/controller/NormalLoginController.java
+++ src/main/java/kr/modusplant/modules/auth/normal/login/app/controller/NormalLoginController.java
@@ -1,27 +1,52 @@
 package kr.modusplant.modules.auth.normal.login.app.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import kr.modusplant.global.app.servlet.response.DataResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
 @Slf4j
 public class NormalLoginController {
 
+    @Value("${jwt.refresh_duration}")
+    private long refreshDuration;
+
     @PostMapping("/login-success")
-    public ResponseEntity sendLoginSuccess(HttpServletRequest request) {
-        System.out.println("Arrived at the success controller method.");
+    public ResponseEntity<DataResponse<Map<String, Object>>> sendLoginSuccess(
+            @RequestAttribute("accessToken") String accessToken,
+            @RequestAttribute("refreshToken") String refreshToken,
+            @RequestAttribute("accessTokenExpirationTime") long accessTokenExpirationTime,
+            @RequestAttribute("refreshTokenExpirationTime") long refreshTokenExpirationTime
+    ) {
 
-        Authentication auth = (Authentication) request.getAttribute("authentication");
-        System.out.println("The auth from the filters: " + auth);
+        ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", refreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(refreshDuration)
+                .sameSite("Lax")
+                .build();
 
-        return ResponseEntity.ok().build();
+        Map<String, Object> accessTokenData = Map.of(
+                "accessToken", accessToken,
+                "accessTokenExpirationTime", accessTokenExpirationTime,
+                "refreshTokenExpirationTime", refreshTokenExpirationTime);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
+                .body(DataResponse.ok(accessTokenData));
     }
 
     @PostMapping("/login-fail")
@@ -30,6 +55,7 @@ public class NormalLoginController {
         AuthenticationException authException = (AuthenticationException) request.getAttribute("exception");
         System.out.println("The error message: " + authException.getMessage());
 
+        // 메시지, status를 어떻게 줄까?
         return ResponseEntity.badRequest().build();
     }
 }
