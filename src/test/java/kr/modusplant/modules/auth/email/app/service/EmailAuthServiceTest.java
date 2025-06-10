a1229677d8ce91f4ad0a9253f2dd158f95f5d192
JunHyeok <akdnjs0308@gmail.com>
2025-06-10 15:51:07 +0900

    MP-202 :bug: Fix: deprecated된 임포트 제거

--- src/test/java/kr/modusplant/modules/auth/email/app/service/EmailAuthServiceTest.java
+++ src/test/java/kr/modusplant/modules/auth/email/app/service/EmailAuthServiceTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
