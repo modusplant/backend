64e3436748fe7e9ec222c7ef26674c081cbca6cc
Jun Hee <umpa9327@gmail.com>
2025-06-10 16:02:43 +0900

    MP-160 :sparkles: Feat: 인증 실패를 처리할 컨트롤러 메서드를 구현함

--- src/main/java/kr/modusplant/modules/auth/normal/login/app/controller/NormalLoginController.java
+++ src/main/java/kr/modusplant/modules/auth/normal/login/app/controller/NormalLoginController.java
@@ -1,13 +1,12 @@
 package kr.modusplant.modules.auth.normal.login.app.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
 import kr.modusplant.global.app.servlet.response.DataResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,12 +49,10 @@ public class NormalLoginController {
     }
 
     @PostMapping("/login-fail")
-    public ResponseEntity sendLoginFailure(HttpServletRequest request) {
-        System.out.println("Arrived at the failure controller method.");
-        AuthenticationException authException = (AuthenticationException) request.getAttribute("exception");
-        System.out.println("The error message: " + authException.getMessage());
-
-        // 메시지, status를 어떻게 줄까?
-        return ResponseEntity.badRequest().build();
+    public ResponseEntity<DataResponse<Void>> sendLoginFailure(
+            @RequestAttribute("errorMessage") String errorMessage
+    ) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(DataResponse.of(HttpStatus.UNAUTHORIZED.value(), errorMessage));
     }
 }
