5d87b60c0a0d707fdfb15303034d2b4b1d95f96d
Jun Hee <umpa9327@gmail.com>
2025-06-11 22:11:22 +0900

    MP-160 :sparkles: Feat: 로그아웃 JWT 삭제 핸들러 로직 수정
     - public key를 얻기 위해 리프레시 토큰 엔티티를 가져오는 로직 추가
     - 리프레시 토큰 삭제 로직을 간결화 및 변수 이름을 명확하게 수정

--- src/main/java/kr/modusplant/global/middleware/security/handler/JwtClearingLogoutHandler.java
+++ src/main/java/kr/modusplant/global/middleware/security/handler/JwtClearingLogoutHandler.java
@@ -4,35 +4,40 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.modusplant.modules.jwt.app.service.RefreshTokenApplicationService;
-import kr.modusplant.modules.jwt.app.service.TokenApplicationService;
+import kr.modusplant.modules.jwt.persistence.entity.RefreshTokenEntity;
+import kr.modusplant.modules.jwt.persistence.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 public class JwtClearingLogoutHandler implements LogoutHandler {
 
-//    private final TokenApplicationService tokenApplicationService;
-    private final RefreshTokenApplicationService refreshTokenApplicationService;
+    private final RefreshTokenRepository tokenRepository;
+    private final RefreshTokenApplicationService tokenApplicationService;
 
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        String refreshToken = request.getHeader("Authorization").substring(7);
 
-        // TODO: DB에서 꺼낸 문자열 값 public key를 사용한다고 가정한다.
+        String refreshTokenFromClient = request.getHeader("Authorization").substring(7);
+
+        // TODO: DB에서 가져온 리프레시 토큰 엔티티에서 public key를 가져올 수 있다고 가정한다.
+        RefreshTokenEntity refreshTokenEntity = tokenRepository.findByRefreshToken(refreshTokenFromClient)
+                .orElseThrow(() -> new EntityNotFoundException("no refresh token with string token"));
+
         // public key가 PEM format으로 저장되어 있다고 가정한다.
+        // 리프레시 토큰 생성 시 사용된 public key가 저장되는 방식에 따라 로직이 변경될 수 있다.
+        // String pemPublicKey = refreshTokenEntity.getPublicKey();
         String pemPublicKey = """
                 -----BEGIN PUBLIC KEY-----
                 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu1SU1LfVLPHCozMxH2Mo
@@ -44,14 +49,13 @@ public class JwtClearingLogoutHandler implements LogoutHandler {
                 mwIDAQAB
                 -----END PUBLIC KEY-----
                 """;
-        PublicKey publicKey = getPublicKey(pemPublicKey);
 
         Jws<Claims> parsedRefreshToken = Jwts.parser()
-                .verifyWith(publicKey)
+                .verifyWith(getPublicKey(pemPublicKey))
                 .build()
-                .parseSignedClaims(refreshToken);
+                .parseSignedClaims(refreshTokenFromClient);
 
-        refreshTokenApplicationService.removeByUuid(
+        tokenApplicationService.removeByUuid(
                 UUID.fromString(parsedRefreshToken.getPayload().getSubject())
         );
 
