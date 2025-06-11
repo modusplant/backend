545f1de02c41d408cf1310ef59a217a5e0af468c
Jun Hee <umpa9327@gmail.com>
2025-06-11 20:50:46 +0900

    MP-160 :white_check_mark: Test: 사용자의 UserDetails 유틸리티 클래스의 테스트 데이터 수정
     - 테스트 데이터 간 통일성을 유지하기 위해 데이터 구성 시 SiteMemberTestUtils의 데이터를 사용하도록 수정함
     - 테스트 데이터로서의 의미를 명확히 하기 위해 테스트 이메일 값을 수정함

--- src/test/java/kr/modusplant/global/middleware/security/common/util/SiteMemberUserDetailsTestUtils.java
+++ src/test/java/kr/modusplant/global/middleware/security/common/util/SiteMemberUserDetailsTestUtils.java
@@ -1,5 +1,6 @@
 package kr.modusplant.global.middleware.security.common.util;
 
+import kr.modusplant.domains.member.common.util.domain.SiteMemberTestUtils;
 import kr.modusplant.domains.member.enums.AuthProvider;
 import kr.modusplant.global.middleware.security.models.SiteMemberUserDetails;
 import kr.modusplant.global.middleware.security.models.SiteMemberUserDetails.SiteMemberUserDetailsBuilder;
@@ -7,17 +8,16 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
-import java.util.UUID;
 
-public interface SiteMemberUserDetailsTestUtils {
+public interface SiteMemberUserDetailsTestUtils extends SiteMemberTestUtils {
 
     BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     SiteMemberUserDetailsBuilder testSiteMemberUserDetailsBuilder = SiteMemberUserDetails.builder()
-            .email("akdnjs0308@gmail.com")
+            .email("test123@example.com")
             .password(passwordEncoder.encode("userPw2!"))
-            .activeUuid(UUID.fromString("f56aca35-d225-4ac2-8c0a-e927cf88dc6e"))
-            .nickname("테스트닉네임")
+            .activeUuid(memberBasicUserWithUuid.getUuid())
+            .nickname(memberBasicUserWithUuid.getNickname())
             .provider(AuthProvider.BASIC)
             .authorities(List.of(new SimpleGrantedAuthority("ROLE_USER")));
 }
