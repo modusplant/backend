5342a6182d98fafca5376adcf6c2146c624e5a8b
Jun Hee <umpa9327@gmail.com>
2025-06-13 13:18:38 +0900

    MP-160 :recycle: Refactor: 로그인 필터의 구조 수정
    - 필터가 촉발되는 HTTP 메서드를 가시적으로 표시함
    - 폐기된 deviceId를 에러 메시지에서 제거

--- src/main/java/kr/modusplant/global/middleware/security/handler/JwtClearingLogoutHandler.java
+++ src/main/java/kr/modusplant/global/middleware/security/handler/JwtClearingLogoutHandler.java
@@ -1,25 +1,11 @@
 package kr.modusplant.global.middleware.security.handler;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import kr.modusplant.modules.jwt.app.service.RefreshTokenApplicationService;
 import kr.modusplant.modules.jwt.app.service.TokenApplicationService;
-import kr.modusplant.modules.jwt.persistence.entity.RefreshTokenEntity;
-import kr.modusplant.modules.jwt.persistence.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-
-import java.security.KeyFactory;
-import java.security.PublicKey;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.UUID;
 
 @RequiredArgsConstructor
 public class JwtClearingLogoutHandler implements LogoutHandler {
