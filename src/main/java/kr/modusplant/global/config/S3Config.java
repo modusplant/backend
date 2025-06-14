ec072fed016738c986e4b3b95f773f56ae011c81
songu1 <syj0002012@naver.com>
2025-06-15 02:31:24 +0900

    MP-168 :sparkles: Feat: S3/Wasabi 연결 설정 추가
    - S3/Wasabi 연결을 위한 application.yml 설정 추가
    - S3Client 생성을 위한 S3Config 클래스 추가

---
+++ src/main/java/kr/modusplant/global/config/S3Config.java
@@ -0,0 +1,40 @@
+package kr.modusplant.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
+
+@Configuration
+public class S3Config {
+    @Value("${cloud.aws.s3.endpoint}")
+    private String endpoint;
+
+    @Value("${cloud.aws.s3.region}")
+    private String region;
+
+    @Value("${cloud.aws.s3.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.s3.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public S3Client s3Client() {
+        AwsBasicCredentials basicCredentials = AwsBasicCredentials.create(accessKey,secretKey);
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(basicCredentials))
+                .serviceConfiguration(S3Configuration.builder()
+                        .pathStyleAccessEnabled(true)
+                        .build())
+                .build();
+    }
+}
