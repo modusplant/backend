dd47d755adcc7d9e690d6f85915ee4c5f624c07a
songu1 <syj0002012@naver.com>
2025-06-16 10:37:06 +0900

    MP-183 :white_check_mark: Test: 누락된 JWT 테스트 코드 추가
    - getClaimsFromToken, getMemberUuidFromToken, getIssuedAtFromToken, getExpirationFromToken 테스트 코드 추가
    - init() 예외 흐름에 대한 테스트 코드 추가

--- src/test/java/kr/modusplant/modules/jwt/app/service/TokenProviderTest.java
+++ src/test/java/kr/modusplant/modules/jwt/app/service/TokenProviderTest.java
@@ -1,15 +1,23 @@
 package kr.modusplant.modules.jwt.app.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
+import kr.modusplant.modules.jwt.error.TokenKeyCreationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
+import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TokenProviderTest {
@@ -32,6 +40,21 @@ class TokenProviderTest {
     void testInit() {
         assertNotNull(ReflectionTestUtils.getField(tokenProvider,"privateKey"));
         assertNotNull(ReflectionTestUtils.getField(tokenProvider,"publicKey"));
+    }
+
+    @Test
+    @DisplayName("비대칭키 생성 실패 테스트")
+    void testInitFail() {
+        try (MockedStatic<KeyPairGenerator> mockedStatic = Mockito.mockStatic(KeyPairGenerator.class)){
+            mockedStatic.when(() -> KeyPairGenerator.getInstance("EC"))
+                    .thenThrow(new NoSuchAlgorithmException("NoSuchAlgorithm"));
+
+            TokenProvider tokenProvider = new TokenProvider();
+
+            assertThatThrownBy(tokenProvider::init)
+                    .isInstanceOf(TokenKeyCreationException.class)
+                    .hasMessageContaining("Failed to create RefreshToken KeyPair");
+        }
     }
 
     @Test
@@ -135,6 +158,76 @@ class TokenProviderTest {
         assertThrows(RuntimeException.class, () -> tokenProvider.validateToken(tamperedToken));
     }
 
+    @Test
+    @DisplayName("토큰에서 정보 가져오기 테스트")
+    void getClaimsFromTokenTest() {
+        // Given
+        UUID uuid = UUID.randomUUID();
+        Map<String, String> claims = createDefaultClaims();
+        String token = tokenProvider.generateAccessToken(uuid,claims);
+
+        // When
+        Claims extractedClaims = tokenProvider.getClaimsFromToken(token);
+
+        // Then
+        assertThat(extractedClaims.getIssuer()).isEqualTo("test-issuer");
+        assertThat(extractedClaims.getAudience()).contains("test-audience");
+        assertThat(extractedClaims.getSubject()).isEqualTo(String.valueOf(uuid));
+
+        Date extractedIssuedAt = extractedClaims.getIssuedAt();
+        Date extractedExpiration = extractedClaims.getExpiration();
+        assertThat(extractedIssuedAt).isNotNull();
+        assertThat(extractedExpiration).isNotNull();
+        assertThat(extractedExpiration.getTime()).isEqualTo(extractedIssuedAt.getTime()+900000L);
+
+        assertThat(extractedClaims.get("nickname",String.class)).isEqualTo(claims.get("nickname"));
+        assertThat(extractedClaims.get("role",String.class)).isEqualTo(claims.get("role"));
+    }
+
+    @Test
+    @DisplayName("토큰에서 member UUID 추출")
+    void getMemberUuidFromTokenTest() {
+        // Given
+        UUID uuid = UUID.randomUUID();
+        String token = tokenProvider.generateRefreshToken(uuid);
+
+        // When
+        UUID memberUuid = tokenProvider.getMemberUuidFromToken(token);
+
+        // Then
+        assertThat(memberUuid).isEqualTo(uuid);
+    }
+
+    @Test
+    @DisplayName("토큰에서 issuedAt 추출")
+    void getIssuedAtFromTokenTest() {
+        // Given
+        Date now = new Date();
+        UUID uuid = UUID.randomUUID();
+        String token = tokenProvider.generateRefreshToken(uuid);
+
+        // When
+        Date issuedAt = tokenProvider.getIssuedAtFromToken(token);
+
+        // Then
+        assertThat(issuedAt.getTime()).isCloseTo(now.getTime(),within(5000L));
+    }
+
+    @Test
+    @DisplayName("토큰에서 expiration 추출")
+    void getExpirationFromTokenTest() {
+        // Given
+        UUID uuid = UUID.randomUUID();
+        String token = tokenProvider.generateRefreshToken(uuid);
+
+        // When
+        Date issuedAt = tokenProvider.getIssuedAtFromToken(token);
+        Date expiration = tokenProvider.getExpirationFromToken(token);
+
+        // Then
+        long expectedExpiration = issuedAt.getTime() + 3600000L;
+        assertThat(expiration.getTime()).isEqualTo(expectedExpiration);
+    }
 
     private Map<String,String> createDefaultClaims() {
         return Map.of(
