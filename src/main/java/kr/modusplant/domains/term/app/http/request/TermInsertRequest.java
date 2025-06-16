c4b7889c811abc2f7c66b1bf4aa7760d9977115c
JunHyeok <akdnjs0308@gmail.com>
2025-06-16 16:57:46 +0900

    MP-208 :recycle: Refactor: 약관 요청에 Swagger 스키마 추가

--- src/main/java/kr/modusplant/domains/term/app/http/request/TermInsertRequest.java
+++ src/main/java/kr/modusplant/domains/term/app/http/request/TermInsertRequest.java
@@ -1,4 +1,14 @@
 package kr.modusplant.domains.term.app.http.request;
 
-public record TermInsertRequest(String name, String content, String version) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record TermInsertRequest(
+        @Schema(description = "약관 이름", example = "이용약관")
+        String name,
+
+        @Schema(description = "약관 컨텐츠", example = "이용약관 컨텐츠")
+        String content,
+
+        @Schema(description = "약관 버전", example = "v1.0.4")
+        String version) {
 }
--- src/main/java/kr/modusplant/domains/term/app/http/request/TermUpdateRequest.java
+++ src/main/java/kr/modusplant/domains/term/app/http/request/TermUpdateRequest.java
@@ -1,6 +1,16 @@
 package kr.modusplant.domains.term.app.http.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.UUID;
 
-public record TermUpdateRequest(UUID uuid, String content, String version) {
+public record TermUpdateRequest(
+        @Schema(description = "약관 식별을 위한 약관 식별자", example = "cad0f836-22f0-4913-8eac-d0507ec7218b")
+        UUID uuid,
+
+        @Schema(description = "갱신을 위한 약관 컨텐츠", example = "개인정보처리방침 컨텐츠")
+        String content,
+
+        @Schema(description = "갱신을 위한 약관 버전", example = "v1.0.5")
+        String version) {
 }
