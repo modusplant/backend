450b85badc627793d039630f5ad0547f139c0710
JunHyeok <akdnjs0308@gmail.com>
2025-06-20 23:23:11 +0900

    MP-160 :recycle: Refactor: Swagger 재구성 및 부족한 검증 부분 적용
    * NormalLoginFilter에는 org.springframework.validation 패키지의 객체를 적용하여 더욱 확실한 검증을 수행

--- src/main/java/kr/modusplant/modules/auth/normal/login/app/http/request/NormalLoginRequest.java
+++ src/main/java/kr/modusplant/modules/auth/normal/login/app/http/request/NormalLoginRequest.java
@@ -1,12 +1,19 @@
 package kr.modusplant.modules.auth.normal.login.app.http.request;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record NormalLoginRequest(
+        @Schema(description = "이메일", example = "flowers32@gmail.com")
+        @NotBlank(message = "이메일이 비어 있습니다.")
+        @Email(message = "이메일 서식이 올바르지 않습니다.")
         String email,
+
+        @Schema(description = "비밀번호", example = "12!excellent")
+        @NotBlank(message = "비밀번호가 비어 있습니다.")
+        @Pattern(regexp = "^(?=\\S*[a-zA-Z])(?=\\S*[!@#$%^&*+\\-=_])(?=\\S*[0-9])\\S{8,64}$",
+                message = "비밀번호는 8 ~ 64자까지 가능하며, 최소 하나 이상의 영문, 숫자, 그리고 특수문자를 포함해야 합니다(공백 제외).")
         String password) {
-    @JsonIgnore
-    public boolean isEmailOrPasswordNull() {
-        return email == null || password == null;
-    }
 }
