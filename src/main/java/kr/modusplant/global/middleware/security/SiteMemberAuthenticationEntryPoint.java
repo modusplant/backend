006150c46deb87ffc55376623f24a80fa8048b51
Jun Hee <umpa9327@gmail.com>
2025-06-20 18:15:08 +0900

    MP-209 :sparkles: Feat: 커스텀 AuthenticationEntryPoint 구현
    - Jwt 검증 필터 등 인증 필터가 작동하기 전에 발생하는 예외들을 처리하기 위해 구현함

---
+++ src/main/java/kr/modusplant/global/middleware/security/SiteMemberAuthenticationEntryPoint.java
@@ -0,0 +1,31 @@
+package kr.modusplant.global.middleware.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.modusplant.global.app.http.response.DataResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class SiteMemberAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(
+                objectMapper.writeValueAsString(DataResponse
+                        .of(HttpStatus.UNAUTHORIZED.value(), authException.getMessage())
+                )
+        );
+    }
+}
