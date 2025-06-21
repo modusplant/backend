644bab3b41d63b2f37b1c67b3935d5a41ee66a74
songu1 <syj0002012@naver.com>
2025-06-21 17:30:53 +0900

    MP-205 :sparkles: Feat: Access Token 블랙리스트 기능의 Repository, ApplicationService 구현
    - access token 블랙리스트 구현을 위한 redis repository의 저장, 확인, 삭제 메서드 추가
    - TokenApplicationService의 블랙리스트 추가/삭제 메서드 구현 및 토큰 검증 메서드에 블랙리스트 로직 추가

--- src/main/java/kr/modusplant/modules/jwt/app/service/TokenApplicationService.java
+++ src/main/java/kr/modusplant/modules/jwt/app/service/TokenApplicationService.java
@@ -11,9 +11,12 @@ import kr.modusplant.modules.jwt.domain.model.RefreshToken;
 import kr.modusplant.modules.jwt.domain.service.TokenValidationService;
 import kr.modusplant.modules.jwt.error.InvalidTokenException;
 import kr.modusplant.modules.jwt.error.TokenNotFoundException;
+import kr.modusplant.modules.jwt.persistence.repository.TokenRedisRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -27,6 +30,7 @@ public class TokenApplicationService {
     private final RefreshTokenApplicationService refreshTokenApplicationService;
     private final TokenValidationService tokenValidationService;
     private final SiteMemberValidationService memberValidationService;
+    private final TokenRedisRepository tokenRedisRepository;
 
     // 토큰 생성
     public TokenPair issueToken(UUID memberUuid, String nickname, Role role) {
@@ -47,15 +51,56 @@ public class TokenApplicationService {
                         .expiredAt(tokenProvider.getExpirationFromToken(refreshToken))
                         .build());
 
-
         return new TokenPair(accessToken,refreshToken);
     }
 
-    // 토큰 갱신
-    public TokenPair reissueToken(String refreshToken) {
-        // refresh token 검증
-        if(!tokenProvider.validateToken(refreshToken))
+    // 토큰 삭제
+    public void removeToken(String refreshToken) {
+        // 토큰 검증
+        tokenProvider.validateToken(refreshToken);
+        tokenValidationService.validateNotFoundRefreshToken(refreshToken);
+        UUID memberUuid = tokenProvider.getMemberUuidFromToken(refreshToken);
+        RefreshToken token = refreshTokenApplicationService.getByMemberUuidAndRefreshToken(memberUuid,refreshToken)
+                .orElseThrow(TokenNotFoundException::new);
+        // 토큰 삭제
+        refreshTokenApplicationService.removeByUuid(token.getUuid());
+    }
+
+    // 토큰 검증 및 재발급
+    public TokenPair verifyAndReissueToken(String accessToken, String refreshToken) {
+        // 블랙리스트 확인
+        if (tokenRedisRepository.isBlacklisted(accessToken)) {
             throw new InvalidTokenException();
+        }
+        // refresh token 유효성 검증
+        if (!tokenProvider.validateToken(refreshToken)) {
+            throw new InvalidTokenException();
+        }
+        // access token 유효성 검증
+        if (tokenProvider.validateToken(accessToken)) {
+            return new TokenPair(accessToken,refreshToken);
+        }
+        // access token 만료 시 토큰 갱신
+        return reissueTokenWithValidRefreshToken(refreshToken);
+    }
+
+    // access token 블랙리스트
+    public void blacklistAccessToken(String accessToken) {
+        if (tokenProvider.validateToken(accessToken)) {
+            Instant expiration = tokenProvider.getExpirationFromToken(accessToken).toInstant();
+            Long expirationSeconds = Duration.between(Instant.now(),expiration).getSeconds();
+            tokenRedisRepository.addToBlacklist(accessToken, expirationSeconds);
+        }
+    }
+
+    // 블랙리스트에서 access token 제거
+    public void removeAccessTokenFromBlacklist(String accessToken) {
+        tokenRedisRepository.removeFromBlacklist(accessToken);
+    }
+
+    // 토큰 갱신
+    private TokenPair reissueTokenWithValidRefreshToken(String refreshToken) {
+        // refresh token 검증
         tokenValidationService.validateNotFoundRefreshToken(refreshToken);
 
         // token에서 사용자 정보 가져오기
@@ -81,28 +126,6 @@ public class TokenApplicationService {
         String accessToken = tokenProvider.generateAccessToken(memberUuid,claims);
 
         return new TokenPair(accessToken,reissuedRefreshToken);
-    }
-
-    // 토큰 삭제
-    public void removeToken(String refreshToken) {
-        // 토큰 검증
-        tokenProvider.validateToken(refreshToken);
-        tokenValidationService.validateNotFoundRefreshToken(refreshToken);
-        UUID memberUuid = tokenProvider.getMemberUuidFromToken(refreshToken);
-        RefreshToken token = refreshTokenApplicationService.getByMemberUuidAndRefreshToken(memberUuid,refreshToken)
-                .orElseThrow(TokenNotFoundException::new);
-        // 토큰 삭제
-        refreshTokenApplicationService.removeByUuid(token.getUuid());
-    }
-
-    // 토큰 검증
-    public TokenPair verifyAndReissueToken(String accessToken, String refreshToken) {
-        if (tokenProvider.validateToken(accessToken)) {
-            return new TokenPair(accessToken,refreshToken);
-        }
-        tokenProvider.validateToken(refreshToken);
-
-        return reissueToken(refreshToken);
     }
 
     private Map<String,String> createClaims(String nickname, Role role) {
