102bb890d252f9aa3cf59176d828f3f1bf4cd055
songu1 <syj0002012@naver.com>
2025-06-26 23:35:55 +0900

    MP-210 :sparkles: Feat: 락아웃 기능을 위한 리포지토리 구현
    - failedAttempt값을 저장하는 락아웃 redis 리포지토리 생성
    - 기존 lockout_refresh_at을 ttl로 지정

---
+++ src/main/java/kr/modusplant/modules/auth/normal/login/persistence/repository/LockOutRedisRepository.java
@@ -0,0 +1,41 @@
+package kr.modusplant.modules.auth.normal.login.persistence.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+import static kr.modusplant.global.common.util.EncryptUtils.encryptWithSha256;
+
+@Repository
+@RequiredArgsConstructor
+public class LockOutRedisRepository {
+
+    // lockOut:email:{sha256 암호화한 email}
+    private static final String KEY_FORMAT = "lockOut:email:%s";
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public int getFailedAttempts(String email) {
+        String result = stringRedisTemplate.opsForValue().get(generateKey(email));
+        return result == null ? 0 : Integer.parseInt(result);
+    }
+
+    public int increaseFailedAttempt(String email, Duration ttl) {
+        String key = generateKey(email);
+        Long result = stringRedisTemplate.opsForValue().increment(key);
+
+        if (result == 1L) {
+            stringRedisTemplate.expire(key, ttl);
+        }
+        return result.intValue();
+    }
+
+    public void removeFailedAttempt(String email) {
+        stringRedisTemplate.delete(generateKey(email));
+    }
+
+    private String generateKey(String email) {
+        return KEY_FORMAT.formatted(encryptWithSha256(email));
+    }
+}
