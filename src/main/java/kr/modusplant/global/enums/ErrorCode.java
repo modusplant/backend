2418854bcf6989f390e3f59ddda410eb28510189
Jun Hee <umpa9327@gmail.com>
2025-07-02 20:20:13 +0900

    MP-221 :sparkles: Feat: 에러 코드를 정의하는 enum과 엔티티 이름을 나타내는 정적 클래스 구현
    - 모든 애플리케이션에 걸쳐서 일관적인 에러 응답을 제공할 목적으로 에러 코드를 정의하는 enum을 생성함
    - 자주 사용되는 엔티티들의 문자열 값에 대한 오타 방지용으로 엔티티 이름을 나타내는 정적 클래스를 구현함

---
+++ src/main/java/kr/modusplant/global/enums/ErrorCode.java
@@ -0,0 +1,48 @@
+package kr.modusplant.global.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    // common errors
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "internal_server_error", "서버에 문제가 발생했습니다"),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "invalid_input_value", "입력값이 올바르지 않습니다"),
+    NO_INPUT_VALUE(HttpStatus.BAD_REQUEST, "no_input_value", "입력값이 존재하지 않습니다"),
+
+    // business errors
+    SITEMEMBER_EXISTS(HttpStatus.CONFLICT, "member_exists", "사용자의 계정이 이미 존재합니다"),
+    SITEMEMBER_AUTH_EXISTS(HttpStatus.CONFLICT, "member_auth_exists", "사용자의 권한 정보가 이미 존재합니다"),
+    SITEMEMBER_ROLE_EXISTS(HttpStatus.CONFLICT, "member_role_exists", "사용자의 역할 정보가 이미 존재합니다"),
+    SITEMEMBER_TERM_EXISTS(HttpStatus.CONFLICT, "member_term_exists", "사용자가 동의한 약관 정보가 이미 존재합니다"),
+    SITEMEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "member_not_found", "사용자의 계정이 존재하지 않습니다"),
+    SITEMEMBER_AUTH_NOT_FOUND(HttpStatus.NOT_FOUND, "member_auth_not_found", "사용자의 권한 정보가 존재하지 않습니다"),
+    SITEMEMBER_ROLE_NOT_FOUND(HttpStatus.NOT_FOUND, "member_role_not_found", "사용자의 역할 정보가 존재하지 않습니다"),
+    SITEMEMBER_TERM_NOT_FOUND(HttpStatus.NOT_FOUND, "member_term_not_found", "사용자가 동의한 약관 정보가 존재하지 않습니다"),
+
+    TERM_EXISTS(HttpStatus.CONFLICT, "term_exists", "약관 정보가 이미 존재합니다"),
+    TERM_NOT_FOUND(HttpStatus.NOT_FOUND, "term_not_found", "약관 정보가 존재하지 않습니다"),
+
+    CATEGORY_EXISTS(HttpStatus.CONFLICT, "category_exists", "항목이 이미 존재합니다"),
+    COMMENT_EXISTS(HttpStatus.CONFLICT, "comment_exists", "댓글이 이미 존재합니다"),
+    LIKE_EXISTS(HttpStatus.CONFLICT, "like_exists", "좋아요를 이미 눌렀습니다"),
+    POST_EXISTS(HttpStatus.CONFLICT, "post_exists", "게시글이 이미 존재합니다"),
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "category_not_found", "항목을 찾을 수 없습니다"),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "comment_not_found", "댓글을 찾을 수 없습니다"),
+    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "like_not_found", "좋아요를 누르지 않았습니다"),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "post_not_found", "게시글을 찾을 수 없습니다"),
+
+    EMPTY_POSTULID_INPUT(HttpStatus.BAD_REQUEST, "empty_postulid_input", "게시글 ulid의 값이 비어 있습니다"),
+    EMPTY_PATH_INPUT(HttpStatus.BAD_REQUEST, "empty_path_input", "경로의 값이 비어 있습니다");
+
+
+    // auth errors
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+}
---
+++ src/main/java/kr/modusplant/global/vo/EntityName.java
@@ -0,0 +1,19 @@
+package kr.modusplant.global.vo;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class EntityName {
+    public static final String SITE_MEMBER = "SiteMember";
+    public static final String SITE_MEMBER_AUTH = "SiteMemberAuth";
+    public static final String SITE_MEMBER_ROLE = "SiteMemberRole";
+    public static final String SITE_MEMBER_TERM = "SiteMemberTerm";
+
+    public static final String TERM = "Term";
+
+    public static final String CATEGORY = "Category";
+    public static final String COMMENT = "Comment";
+    public static final String LIKE = "Like";
+    public static final String POST = "Post";
+}
