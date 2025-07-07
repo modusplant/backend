8825723f8eca120ba4bc42d9faca9956995b10ef
Jun Hee <umpa9327@gmail.com>
2025-07-07 11:52:59 +0900

    MP-221 :sparkles: Feat: AuthTokenException에 ErrorCode를 적용함
    - JWT 토큰과 관련된 커스텀 예외들이 애플리케이션에서 사용하는 예외 구조를 따르도록 강제함

--- src/main/java/kr/modusplant/modules/jwt/error/AuthTokenException.java
+++ src/main/java/kr/modusplant/modules/jwt/error/AuthTokenException.java
@@ -1,12 +1,12 @@
 package kr.modusplant.modules.jwt.error;
 
-import kr.modusplant.global.error.DomainException;
+import kr.modusplant.global.enums.ErrorCode;
+import kr.modusplant.global.error.BusinessException;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
-public class AuthTokenException extends DomainException {
-    protected AuthTokenException(HttpStatus status, String code, String message) {
-        super(status, code, message);
+public class AuthTokenException extends BusinessException {
+    public AuthTokenException(ErrorCode errorCode) {
+        super(errorCode);
     }
 }
