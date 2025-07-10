0ee3267d14d2a4a4dced752ba81df211108ed53d
Jun Hee <umpa9327@gmail.com>
2025-07-10 21:05:09 +0900

    MP-221 :wrench: Chore: DeletedException에서 사용하지 않는 import문 제거

--- src/main/java/kr/modusplant/global/middleware/security/error/DeletedException.java
+++ src/main/java/kr/modusplant/global/middleware/security/error/DeletedException.java
@@ -2,7 +2,6 @@ package kr.modusplant.global.middleware.security.error;
 
 import kr.modusplant.global.middleware.security.enums.SecurityErrorCode;
 import lombok.Getter;
-import org.springframework.security.core.AuthenticationException;
 
 @Getter
 public class DeletedException extends BusinessAuthenticationException {
