67bbadcd18573847bbeff3ac1980b3150c02e694
Jun Hee <umpa9327@gmail.com>
2025-07-10 17:42:32 +0900

    MP-221 :sparkles: Feat: DefaultAccessDeniedHandler에 SecurityErrorCode 적용
    - 커스텀 AccessDeniedException을 구현하는 방법이 있지만 현재 진행하기에는 시간이 과하게 소요되므로 유일하게 AccessDeniedException이 사용되는 핸들러에 SecurityErrorCode를 직접 도입함

--- src/main/java/kr/modusplant/global/middleware/security/enums/SecurityErrorCode.java
+++ src/main/java/kr/modusplant/global/middleware/security/enums/SecurityErrorCode.java
@@ -12,7 +12,8 @@ public enum SecurityErrorCode {
     BANNED(HttpStatus.UNAUTHORIZED, "banned", "밴 처리 된 계정입니다"),
     DELETED(HttpStatus.UNAUTHORIZED, "deleted", "삭제된 계정입니다"),
     DISABLED_BY_LINKING(HttpStatus.UNAUTHORIZED, "disabled_by_linking", "계정 연동으로 인해 비활성화된 계정입니다"),
-    INACTIVE(HttpStatus.UNAUTHORIZED, "inactive", "비활성화된 계정입니다");
+    INACTIVE(HttpStatus.UNAUTHORIZED, "inactive", "비활성화된 계정입니다"),
+    ACCESS_DENIED(HttpStatus.UNAUTHORIZED, "access_denied", "접근이 거부되었습니다");
 
     private final HttpStatus httpStatus;
     private final String code;
--- src/main/java/kr/modusplant/global/middleware/security/handler/DefaultAccessDeniedHandler.java
+++ src/main/java/kr/modusplant/global/middleware/security/handler/DefaultAccessDeniedHandler.java
@@ -4,9 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.modusplant.global.app.http.response.DataResponse;
-import kr.modusplant.global.enums.ResponseCode;
+import kr.modusplant.global.middleware.security.enums.SecurityErrorCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
@@ -22,10 +21,12 @@ public class DefaultAccessDeniedHandler implements AccessDeniedHandler {
                        HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException {
 
-        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setStatus(SecurityErrorCode.ACCESS_DENIED.getHttpStatus().value());
         response.getWriter().write(
                 objectMapper.writeValueAsString(DataResponse
-                        .of(HttpStatus.FORBIDDEN.value(), ResponseCode.FORBIDDEN.getValue(), "접근이 거부되었습니다.")
+                        .of(SecurityErrorCode.ACCESS_DENIED.getHttpStatus().value(),
+                                SecurityErrorCode.ACCESS_DENIED.getCode(),
+                                SecurityErrorCode.ACCESS_DENIED.getMessage())
                 )
         );
     }
