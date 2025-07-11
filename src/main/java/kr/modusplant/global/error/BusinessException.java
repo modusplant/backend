91c10bd3c0e2edf151c975761c63dfed5d07f46a
Jun Hee <umpa9327@gmail.com>
2025-07-11 22:50:21 +0900

    MP-221 :sparkles: Feat: BusinessException의 생성자에서 메시지를 나타내는 파라미터의 이름 변경
    - 구조적으로 생성자의 메서드가 클라이언트에게 전달되지 않고 내부에서만 쓰이므로 internal이라고 밝힐 필요성이 없다 판단함

--- src/main/java/kr/modusplant/global/error/BusinessException.java
+++ src/main/java/kr/modusplant/global/error/BusinessException.java
@@ -13,8 +13,8 @@ public class BusinessException extends RuntimeException {
         this.errorCode = errorCode;
     }
 
-    public BusinessException(ErrorCode errorCode, String internalMessage) {
-        super(internalMessage);
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
         this.errorCode = errorCode;
     }
 }
