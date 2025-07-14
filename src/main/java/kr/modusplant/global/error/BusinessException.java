1815bf4360f028bd027835eccaf11dd609f1ef7d
Jun Hee <umpa9327@gmail.com>
2025-07-14 18:15:04 +0900

    MP-221 :speech_balloon: Comment: BusinessException에 주석 추가
    - 해당 클래스의 역할과 서브 클래스의 특징을 설명할 목적으로 추가함

--- src/main/java/kr/modusplant/global/error/BusinessException.java
+++ src/main/java/kr/modusplant/global/error/BusinessException.java
@@ -3,6 +3,18 @@ package kr.modusplant.global.error;
 import kr.modusplant.global.enums.ErrorCode;
 import lombok.Getter;
 
+/**
+ * {@code BusinessException}은 모든 커스텀 예외의 상위 클래스입니다.
+ *
+ * <p>커스텀 예외를 {@link kr.modusplant.global.enums.ErrorCode}와 대응시키는 역할을 합니다.
+ * 모든 커스텀 예외는 반드시 {@code BusinessException}을 상속받거나
+ * 해당 클래스의 서브 클래스를 상속받아야 합니다.</p>
+ *
+ * <p>해당 클래스의 서브 클래스들은 {@link kr.modusplant.global.advice.GlobalExceptionHandler}에
+ * 예외를 처리하는 메서드를 구현할 필요가 없습니다.</p>
+ *
+ * @author Jun Hee
+ */
 @Getter
 public class BusinessException extends RuntimeException {
 
