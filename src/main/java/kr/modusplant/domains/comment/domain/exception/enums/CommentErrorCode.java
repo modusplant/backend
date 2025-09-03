64fd7d7b31eca303ba1ed8f597e7e86991145e58
Jun Hee <umpa9327@gmail.com>
2025-09-03 21:38:03 +0900

    MP-233 :sparkles: Feat: 댓글 작성자의 닉네임 값이 빈 경우에 사용할 에러코드 추가

--- src/main/java/kr/modusplant/domains/comment/domain/exception/enums/CommentErrorCode.java
+++ src/main/java/kr/modusplant/domains/comment/domain/exception/enums/CommentErrorCode.java
@@ -12,6 +12,7 @@ public enum CommentErrorCode implements ResponseCode {
     EMPTY_POST_ID(HttpStatus.BAD_REQUEST, "empty_post_id", "게시글의 식별자 값이 비었습니다"),
     EMPTY_COMMENT_PATH(HttpStatus.BAD_REQUEST, "empty_comment_path", "댓글의 경로 값이 비었습니다"),
     EMPTY_AUTHOR(HttpStatus.BAD_REQUEST, "empty_author", "작성자의 값이 비어 있습니다"),
+    EMPTY_MEMBER_NICKNAME(HttpStatus.BAD_REQUEST, "empty_member_nickname", "작성자의 닉네임 값이 비어 있습니다"),
 
     INVALID_COMMENT_PATH(HttpStatus.BAD_REQUEST, "invalid_comment_path", "댓글 경로의 형식이 올바르지 않습니다"),
     INVALID_COMMENT_STATUS(HttpStatus.BAD_REQUEST, "invalid_comment_status", "댓글의 상태가 올바르지 않습니다");
