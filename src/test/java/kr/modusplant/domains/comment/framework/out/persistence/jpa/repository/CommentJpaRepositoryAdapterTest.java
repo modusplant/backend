4860775df2679cb2a1ad28a62abe1cc891b39c9f
Jun Hee <umpa9327@gmail.com>
2025-09-10 22:11:01 +0900

    MP-233 :white_check_mark: Test: CommentJpaRepositoryAdapterTest 클래스 추가

---
+++ src/test/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/CommentJpaRepositoryAdapterTest.java
@@ -0,0 +1,87 @@
+package kr.modusplant.domains.comment.framework.out.persistence.jpa.repository;
+
+import kr.modusplant.domains.comment.adapter.response.CommentResponse;
+import kr.modusplant.domains.comment.framework.out.persistence.jpa.entity.CommentEntity;
+import kr.modusplant.domains.comment.framework.out.persistence.jpa.mapper.CommentJpaMapper;
+import kr.modusplant.domains.comment.framework.out.persistence.jpa.repository.supers.CommentJpaRepository;
+import kr.modusplant.domains.comment.support.utils.adapter.CommentResponseTestUtils;
+import kr.modusplant.domains.comment.support.utils.domain.AuthorTestUtils;
+import kr.modusplant.domains.comment.support.utils.domain.CommentTestUtils;
+import kr.modusplant.domains.comment.support.utils.domain.PostIdTestUtils;
+import kr.modusplant.domains.comment.support.utils.framework.CommentCompositeKeyTestUtils;
+import kr.modusplant.domains.comment.support.utils.framework.CommentEntityTestUtils;
+import kr.modusplant.domains.member.test.utils.domain.MemberIdTestUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+
+public class CommentJpaRepositoryAdapterTest implements PostIdTestUtils,
+        CommentResponseTestUtils, MemberIdTestUtils,
+        AuthorTestUtils, CommentTestUtils,
+        CommentEntityTestUtils, CommentCompositeKeyTestUtils {
+    private final CommentJpaRepository repository = Mockito.mock(CommentJpaRepository.class);
+    private final CommentJpaMapper mapper = Mockito.mock(CommentJpaMapper.class);
+    private final CommentRepositoryJpaAdapter repositoryJpaAdapter = new CommentRepositoryJpaAdapter(repository, mapper);
+
+    @Test
+    @DisplayName("")
+    public void testFindByPost_givenValidPostId_willReturnCommentResponseList() {
+        // given
+        given(repository.findByPostUlid(testPostId.getId())).willReturn(List.of(testCommentResponse));
+
+        // when
+        List<CommentResponse> result = repositoryJpaAdapter.findByPost(testPostId);
+
+        // then
+        assertThat(result).isEqualTo(List.of(testCommentResponse));
+    }
+
+    @Test
+    @DisplayName("")
+    public void testFindByAuthor_givenValidAuthor_willReturnCommentResponseList() {
+        // given
+        given(repository.findByAuthMemberUuid(testMemberId.getValue())).willReturn(List.of(testCommentResponse));
+
+        // when
+        List<CommentResponse> result = repositoryJpaAdapter.findByAuthor(testAuthorWithUuid);
+
+        // then
+        assertThat(result).isEqualTo(List.of(testCommentResponse));
+    }
+
+    @Test
+    @DisplayName("")
+    public void testSave_givenValidCommentId_willSaveComment() {
+        // given
+        CommentEntity testCommentEntity = createCommentEntity();
+
+        given(repository.save(testCommentEntity)).willReturn(testCommentEntity);
+        given(mapper.toCommentEntity(testValidComment)).willReturn(testCommentEntity);
+
+        // when
+        repositoryJpaAdapter.save(testValidComment);
+
+        // then
+        Mockito.verify(repository, times(1)).save(testCommentEntity);
+    }
+
+    @Test
+    @DisplayName("")
+    public void testDeleteById_givenValidCommentId_willDeleteComment() {
+        // given
+        doNothing().when(repository).deleteById(testCommentCompositeKey);
+
+        // when
+        repositoryJpaAdapter.deleteById(testCommentCompositeKey);
+
+        // then
+        Mockito.verify(repository, times(1)).deleteById(testCommentCompositeKey);
+    }
+}
