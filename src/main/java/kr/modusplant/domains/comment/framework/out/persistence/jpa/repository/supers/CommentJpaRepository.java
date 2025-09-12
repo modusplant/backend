31ab81dc1ce437007f341839d064e8eb9856e20e
Jun Hee <umpa9327@gmail.com>
2025-09-12 23:28:02 +0900

    MP-233 :bug: Fix: CommentJpaRepository의 @Query에 위치한 쿼리 구문이 오류를 일으키는 문제 해결

--- src/main/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/supers/CommentJpaRepository.java
+++ src/main/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/supers/CommentJpaRepository.java
@@ -12,16 +12,18 @@ import java.util.UUID;
 
 public interface CommentJpaRepository extends JpaRepository<CommentEntity, CommentCompositeKey> {
 
-    @Query("SELECT c.post_ulid, c.path, m.nickname, c.content, c.is_deleted, c.created_at " +
-            "FROM comm_comment c " +
-            "WHERE c.post_ulid = :postUlid " +
-            "INNER JOIN site_member m ON c.auth_memb_uuid = m.uuid")
+    @Query("SELECT c.postEntity.ulid, c.id.path, m.nickname, c.content, c.isDeleted, c.createdAt " +
+        "FROM CommentEntity c " +
+        "INNER JOIN c.authMember m " +
+        "WHERE c.postEntity.ulid = :postUlid " +
+        "ORDER BY c.createdAt ASC")
     List<CommentResponse> findByPostUlid(@Param("postUlid") String postUlid);
 
-    @Query("SELECT c.post_ulid, c.path, m.nickname, c.content, c.is_deleted, c.created_at " +
-            "FROM comm_comment c " +
-            "WHERE c.auth_memb_uuid = :memberUuid " +
-            "INNER JOIN site_member m ON c.auth_memb_uuid = m.uuid")
+    @Query("SELECT c.postEntity.ulid, c.id.path, m.nickname, c.content, c.isDeleted, c.createdAt " +
+            "FROM CommentEntity c " +
+            "INNER JOIN c.authMember m " +
+            "WHERE c.authMember.uuid = :memberUuid " +
+            "ORDER BY c.createdAt ASC")
     List<CommentResponse> findByAuthMemberUuid(@Param("memberUuid") UUID memberUuid);
 
 }
