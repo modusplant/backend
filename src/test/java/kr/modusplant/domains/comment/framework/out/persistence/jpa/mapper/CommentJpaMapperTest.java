69941cd8f0cda69af4d574f12a85e1930acd52ab
Jun Hee <umpa9327@gmail.com>
2025-09-12 23:30:48 +0900

    MP-233 :white_check_mark: Test: CommentJpaMapperTest를 CommentEntity의 변경 사항에 맞춰 수정
    - CommentEntity가 SiteMember와 CommPostEntity를 사용함에 따라 발생한 변경 사항 반영

--- src/test/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/mapper/CommentJpaMapperTest.java
+++ src/test/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/mapper/CommentJpaMapperTest.java
@@ -3,6 +3,7 @@ package kr.modusplant.domains.comment.framework.out.persistence.jpa.mapper;
 import kr.modusplant.domains.comment.framework.out.persistence.jpa.entity.CommentEntity;
 import kr.modusplant.domains.comment.support.utils.domain.CommentTestUtils;
 import kr.modusplant.domains.comment.support.utils.framework.CommentEntityTestUtils;
+import kr.modusplant.framework.out.persistence.jpa.entity.SiteMemberEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,11 +15,24 @@ public class CommentJpaMapperTest implements CommentTestUtils, CommentEntityTest
     @Test
     @DisplayName("유효한 댓글을 댓글 엔티티로 전환")
     public void testToCommentEntity_givenValidComment_willReturnCommentEntity() {
-        // given & when
+        // given
+        SiteMemberEntity testSiteMemberEntity = SiteMemberEntity.builder()
+                .uuid(testAuthor.getMemberUuid())
+                .nickname(memberBasicUser.getNickname())
+                .birthDate(memberBasicUser.getBirthDate())
+                .loggedInAt(memberBasicUser.getLoggedInAt())
+                .build();
+
+        CommentEntity compare = createCommentEntityBuilder()
+                .postEntity(createCommPostEntityBuilder().ulid(TEST_COMM_POST_WITH_ULID.getUlid()).build())
+                .authMember(testSiteMemberEntity)
+                .createMember(testSiteMemberEntity).build();
+
+        // when
         CommentEntity result = mapper.toCommentEntity(testValidComment);
 
         // then
-        assertThat(result).isEqualTo(createCommentEntity());
+        assertThat(result).isEqualTo(compare);
     }
 
 }
