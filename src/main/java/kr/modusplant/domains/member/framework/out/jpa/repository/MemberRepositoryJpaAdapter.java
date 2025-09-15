f3ffa56298bdb244e3b50ffc7cbb0069a9f9d7ca
JunHyeok <akdnjs0308@gmail.com>
2025-09-15 16:09:18 +0900

    :sparkles: Feat: 전체 회의 간 도출된 사항 반영
    - 사이트 회원의 닉네임을 Unique로 설정

--- src/main/java/kr/modusplant/domains/member/framework/out/jpa/repository/MemberRepositoryJpaAdapter.java
+++ src/main/java/kr/modusplant/domains/member/framework/out/jpa/repository/MemberRepositoryJpaAdapter.java
@@ -1,17 +1,27 @@
 package kr.modusplant.domains.member.framework.out.jpa.repository;
 
 import kr.modusplant.domains.member.domain.aggregate.Member;
+import kr.modusplant.domains.member.domain.vo.MemberNickname;
+import kr.modusplant.domains.member.framework.out.jpa.entity.MemberEntity;
 import kr.modusplant.domains.member.framework.out.jpa.mapper.MemberJpaMapperImpl;
 import kr.modusplant.domains.member.framework.out.jpa.repository.supers.MemberJpaRepository;
 import kr.modusplant.domains.member.usecase.port.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 @RequiredArgsConstructor
 public class MemberRepositoryJpaAdapter implements MemberRepository {
     private final MemberJpaMapperImpl memberJpaMapper;
     private final MemberJpaRepository memberJpaRepository;
+
+    @Override
+    public Optional<Member> getByNickname(MemberNickname nickname) {
+        Optional<MemberEntity> emptyOrMemberEntity = memberJpaRepository.findByNickname(nickname.getValue());
+        return emptyOrMemberEntity.isEmpty() ? Optional.empty() : Optional.of(memberJpaMapper.toMember(emptyOrMemberEntity.orElseThrow()));
+    }
 
     @Override
     public Member updateNickname(Member member) {
@@ -21,5 +31,10 @@ public class MemberRepositoryJpaAdapter implements MemberRepository {
     @Override
     public Member save(Member member) {
         return memberJpaMapper.toMember(memberJpaRepository.save(memberJpaMapper.toMemberEntity(member)));
+    }
+
+    @Override
+    public boolean isNicknameExist(MemberNickname nickname) {
+        return memberJpaRepository.existsByNickname(nickname.getValue());
     }
 }
--- src/main/java/kr/modusplant/domains/member/usecase/port/repository/MemberRepository.java
+++ src/main/java/kr/modusplant/domains/member/usecase/port/repository/MemberRepository.java
@@ -1,9 +1,16 @@
 package kr.modusplant.domains.member.usecase.port.repository;
 
 import kr.modusplant.domains.member.domain.aggregate.Member;
+import kr.modusplant.domains.member.domain.vo.MemberNickname;
+
+import java.util.Optional;
 
 public interface MemberRepository {
+    Optional<Member> getByNickname(MemberNickname nickname);
+
     Member updateNickname(Member member);
 
     Member save(Member member);
+
+    boolean isNicknameExist(MemberNickname nickname);
 }
