38ca788ee459d0909b18bb65919d23f0c460dc29
songu1 <syj0002012@naver.com>
2025-09-17 12:05:03 +0900

    MP-232 :white_check_mark: Test: 게시글 Domain 계층 테스트 구현
    - 테스트를 위한 TestUtils 및 Constant 생성
    - 게시글 Domain 계층 테스트 코드 작성

---
+++ src/test/java/kr/modusplant/domains/post/common/utils/domain/vo/PostContentTestUtils.java
@@ -0,0 +1,10 @@
+package kr.modusplant.domains.post.common.utils.domain.vo;
+
+import kr.modusplant.domains.post.domain.vo.PostContent;
+
+import static kr.modusplant.domains.post.common.constant.PostJsonNodeConstant.TEST_POST_CONTENT;
+import static kr.modusplant.domains.post.common.constant.PostStringConstant.TEST_POST_TITLE;
+
+public interface PostContentTestUtils {
+    PostContent testPostContent = PostContent.create(TEST_POST_TITLE, TEST_POST_CONTENT);
+}
