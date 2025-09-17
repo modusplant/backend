de5550c21f6d5fd485a525029a4df9fa74d5cb84
songu1 <syj0002012@naver.com>
2025-09-17 12:02:28 +0900

    MP-232 :sparkles: Feat: 게시글 Domain 계층 구현
    - 게시글 Bounded Context 클린 아키텍처 적용을 위한 Post Domain계층(aggregate, vo, exception) 구현

---
+++ src/main/java/kr/modusplant/domains/post/domain/exception/enums/PostErrorCode.java
@@ -0,0 +1,32 @@
+package kr.modusplant.domains.post.domain.exception.enums;
+
+import kr.modusplant.shared.exception.enums.supers.ResponseCode;
+import kr.modusplant.shared.http.enums.HttpStatus;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum PostErrorCode implements ResponseCode {
+    EMPTY_POST_ID(HttpStatus.BAD_REQUEST, "empty_post_id", "게시글 id가 비어 있습니다. "),
+    INVALID_POST_ID(HttpStatus.BAD_REQUEST, "invalid_post_id", "게시글 id가 유효하지 않습니다. "),
+    EMPTY_AUTHOR_ID(HttpStatus.BAD_REQUEST, "empty_author_id", "작성자 id가 비어 있습니다. "),
+    INVALID_AUTHOR_ID(HttpStatus.BAD_REQUEST, "invalid_author_id", "작성자 id가 유효하지 않습니다. "),
+    EMPTY_POST_CONTENT(HttpStatus.BAD_REQUEST, "empty_post_content", "게시글 컨텐츠가 비어 있습니다. "),
+    INVALID_POST_CONTENT(HttpStatus.BAD_REQUEST, "invalid_post_content", "게시글 컨텐츠가 유효하지 않습니다. "),
+    EMPTY_LIKE_COUNT(HttpStatus.BAD_REQUEST,"empty_like_count", "좋아요 수가 비어 있습니다. "),
+    INVALID_LIKE_COUNT(HttpStatus.BAD_REQUEST, "invalid_like_count", "좋아요 수가 유효하지 않습니다. "),
+    EMPTY_POST_STATUS(HttpStatus.BAD_REQUEST,"empty_post_status", "게시글 상태가 비어 있습니다. "),
+    INVALID_POST_STATUS(HttpStatus.BAD_REQUEST, "invalid_post_status", "게시글 상태가 유효하지 않습니다. "),
+    EMPTY_CATEGORY_ID(HttpStatus.BAD_REQUEST, "empty_category_id", "카테고리 id가 비어 있습니다. "),
+    INVALID_CATEGORY_ID(HttpStatus.BAD_REQUEST, "invalid_category_id", "카테고리 id가 유효하지 않습니다. ");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public boolean isSuccess() {
+        return false;
+    }
+}
