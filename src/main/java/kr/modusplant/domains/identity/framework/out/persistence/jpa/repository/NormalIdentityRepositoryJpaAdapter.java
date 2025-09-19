f88b2b79676735b8297387568cb416bd7ff8e6c9
Jun Hee <umpa9327@gmail.com>
2025-09-19 17:59:58 +0900

    MP-259 :sparkles: Feat: 사용자 관련 JPA 리포지토리 추가
    - MemberAuthJpaRepository, MemberIdentityJpaRepository, MemberRoleJpaRepository, MemberTermJpaRepository, NormalIdentityRepositoryJpaAdapter 추가

---
+++ src/main/java/kr/modusplant/domains/identity/framework/out/persistence/jpa/repository/NormalIdentityRepositoryJpaAdapter.java
@@ -0,0 +1,48 @@
+package kr.modusplant.domains.identity.framework.out.persistence.jpa.repository;
+
+import kr.modusplant.domains.identity.adapter.repository.NormalIdentityRepository;
+import kr.modusplant.domains.identity.domain.vo.SignUpData;
+import kr.modusplant.domains.identity.domain.vo.enums.UserRole;
+import kr.modusplant.domains.identity.framework.out.persistence.jpa.entity.MemberAuthEntity;
+import kr.modusplant.domains.identity.framework.out.persistence.jpa.entity.MemberRoleEntity;
+import kr.modusplant.domains.identity.framework.out.persistence.jpa.entity.MemberTermEntity;
+import kr.modusplant.domains.identity.framework.out.persistence.jpa.repository.supers.MemberAuthJpaRepository;
+import kr.modusplant.domains.identity.framework.out.persistence.jpa.repository.supers.MemberIdentityJpaRepository;
+import kr.modusplant.domains.identity.framework.out.persistence.jpa.repository.supers.MemberRoleJpaRepository;
+import kr.modusplant.domains.identity.framework.out.persistence.jpa.repository.supers.MemberTermJpaRepository;
+import kr.modusplant.legacy.domains.member.enums.AuthProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class NormalIdentityRepositoryJpaAdapter implements NormalIdentityRepository {
+
+    private final MemberIdentityJpaRepository identityJpaRepository;
+    private final MemberAuthJpaRepository authJpaRepository;
+    private final MemberRoleJpaRepository roleJpaRepository;
+    private final MemberTermJpaRepository termJpaRepository;
+
+    @Override
+    public void save(SignUpData signUpData) {
+        UUID uuidOfSavedMember = identityJpaRepository.saveIdentity(signUpData).getUuid();
+        authJpaRepository.saveAuth(MemberAuthEntity.builder()
+                .originalMemberUuid(uuidOfSavedMember)
+                .activeMemberUuid(uuidOfSavedMember)
+                .email(signUpData.getCredentials().getEmail())
+                .password(signUpData.getCredentials().getPassword())
+                .provider(AuthProvider.BASIC)
+                .build());
+        roleJpaRepository.saveRole(MemberRoleEntity.builder()
+                .uuid(uuidOfSavedMember)
+                .role(UserRole.USER).build());
+        termJpaRepository.saveTerm(MemberTermEntity.builder()
+                .uuid(uuidOfSavedMember)
+                .agreedPrivacyPolicyVersion(signUpData.getAgreedPrivacyPolicyVersion().getVersion())
+                .agreedAdInfoReceivingVersion(signUpData.getAgreedAdInfoReceivingVersion().getVersion())
+                .agreedTermsOfUseVersion(signUpData.getAgreedTermsOfUseVersion().getVersion())
+                .build());
+    }
+}
