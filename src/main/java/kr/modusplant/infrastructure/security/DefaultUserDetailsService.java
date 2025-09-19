ddc68e95206a2f754f4adb81015197b5cceadbc2
Jun Hee <umpa9327@gmail.com>
2025-09-19 18:40:40 +0900

    MP-259 :sparkles: Feat: DefaultUserDetailsService에서 사용자의 정보를 가져오는 로직에 framework의 기능 추가
    - 리팩토링 과정에서 기존에 사용하던 서비스를 사용할 수 없게 되었으므로 framework에 있는 전역 리포지토리를 사용함

--- src/main/java/kr/modusplant/infrastructure/security/DefaultUserDetailsService.java
+++ src/main/java/kr/modusplant/infrastructure/security/DefaultUserDetailsService.java
@@ -1,20 +1,14 @@
 package kr.modusplant.infrastructure.security;
 
 import jakarta.transaction.Transactional;
+import kr.modusplant.framework.out.jpa.entity.SiteMemberAuthEntity;
+import kr.modusplant.framework.out.jpa.entity.SiteMemberEntity;
+import kr.modusplant.framework.out.jpa.entity.SiteMemberRoleEntity;
 import kr.modusplant.framework.out.jpa.repository.SiteMemberAuthRepository;
 import kr.modusplant.framework.out.jpa.repository.SiteMemberRepository;
 import kr.modusplant.framework.out.jpa.repository.SiteMemberRoleRepository;
-import kr.modusplant.legacy.domains.member.domain.model.SiteMember;
-import kr.modusplant.legacy.domains.member.domain.model.SiteMemberAuth;
-import kr.modusplant.legacy.domains.member.domain.model.SiteMemberRole;
-import kr.modusplant.legacy.domains.member.domain.service.SiteMemberAuthValidationService;
-import kr.modusplant.legacy.domains.member.domain.service.SiteMemberRoleValidationService;
-import kr.modusplant.legacy.domains.member.domain.service.SiteMemberValidationService;
-import kr.modusplant.legacy.domains.member.enums.AuthProvider;
-import kr.modusplant.legacy.domains.member.mapper.SiteMemberAuthDomainInfraMapper;
-import kr.modusplant.legacy.domains.member.mapper.SiteMemberDomainInfraMapper;
-import kr.modusplant.legacy.domains.member.mapper.SiteMemberRoleDomainInfraMapper;
 import kr.modusplant.infrastructure.security.models.DefaultUserDetails;
+import kr.modusplant.legacy.domains.member.enums.AuthProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -28,14 +22,6 @@ import java.util.List;
 @Transactional
 public class DefaultUserDetailsService implements UserDetailsService {
 
-    private final SiteMemberValidationService memberValidationService;
-    private final SiteMemberAuthValidationService memberAuthValidationService;
-    private final SiteMemberRoleValidationService memberRoleValidationService;
-
-    private final SiteMemberDomainInfraMapper memberDomainInfraMapper;
-    private final SiteMemberAuthDomainInfraMapper memberAuthDomainInfraMapper;
-    private final SiteMemberRoleDomainInfraMapper memberRoleDomainInfraMapper;
-
     private final SiteMemberRepository memberRepository;
     private final SiteMemberAuthRepository memberAuthRepository;
     private final SiteMemberRoleRepository memberRoleRepository;
@@ -43,29 +29,24 @@ public class DefaultUserDetailsService implements UserDetailsService {
     @Override
     public DefaultUserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        memberAuthValidationService.validateNotFoundEmailAndAuthProvider(email, AuthProvider.BASIC);
-        SiteMemberAuth memberAuth = memberAuthDomainInfraMapper.toSiteMemberAuth(
-                memberAuthRepository.findByEmailAndProvider(email, AuthProvider.BASIC).orElseThrow());
-
-        memberValidationService.validateNotFoundUuid(memberAuth.getActiveMemberUuid());
-        SiteMember member = memberDomainInfraMapper.toSiteMember(
-                memberRepository.findByUuid(memberAuth.getActiveMemberUuid()).orElseThrow());
-
-        memberRoleValidationService.validateNotFoundUuid(memberAuth.getActiveMemberUuid());
-        SiteMemberRole memberRole = memberRoleDomainInfraMapper.toSiteMemberRole(
-                memberRoleRepository.findByUuid(memberAuth.getActiveMemberUuid()).orElseThrow());
+        SiteMemberAuthEntity auth = memberAuthRepository
+                .findByEmailAndProvider(email, AuthProvider.BASIC).orElseThrow();
+        SiteMemberEntity member = memberRepository
+                .findByUuid(auth.getActiveMember().getUuid()).orElseThrow();
+        SiteMemberRoleEntity role = memberRoleRepository
+                .findByUuid(auth.getActiveMember().getUuid()).orElseThrow();
 
         return DefaultUserDetails.builder()
-                .email(memberAuth.getEmail())
-                .password(memberAuth.getPw())
-                .activeUuid(memberAuth.getActiveMemberUuid())
+                .email(auth.getEmail())
+                .password(auth.getPw())
+                .activeUuid(auth.getActiveMember().getUuid())
                 .nickname(member.getNickname())
-                .provider(memberAuth.getProvider())
+                .provider(auth.getProvider())
                 .isActive(member.getIsActive())
                 .isDisabledByLinking(member.getIsDisabledByLinking())
                 .isBanned(member.getIsBanned())
                 .isDeleted(member.getIsDeleted())
-                .authorities(List.of(new SimpleGrantedAuthority(memberRole.getRole().getValue())))
+                .authorities(List.of(new SimpleGrantedAuthority(role.getRole().getValue())))
                 .build();
     }
 }
