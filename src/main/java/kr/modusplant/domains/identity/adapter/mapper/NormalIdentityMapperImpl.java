86ff667a28fa51b8cd99ca5b3ae9e57fe0e193e2
Jun Hee <umpa9327@gmail.com>
2025-09-22 18:15:40 +0900

    MP-259 :sparkles: Feat: NormalIdentityMapperImpl에서 사용자의 비밀번호를 암호화하는 기능 추가
    - 사용자의 비밀번호에 대한 보안을 강화하기 위해 추가함

--- src/main/java/kr/modusplant/domains/identity/adapter/mapper/NormalIdentityMapperImpl.java
+++ src/main/java/kr/modusplant/domains/identity/adapter/mapper/NormalIdentityMapperImpl.java
@@ -2,11 +2,14 @@ package kr.modusplant.domains.identity.adapter.mapper;
 
 import kr.modusplant.domains.identity.domain.vo.SignUpData;
 import kr.modusplant.domains.identity.usecase.request.NormalSignUpRequest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class NormalIdentityMapperImpl {
 
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     public SignUpData toSignUpData(NormalSignUpRequest request) {
-        return SignUpData.create(request.email(), request.password(), request.nickname(),
+        return SignUpData.create(request.email(), encoder.encode(request.password()), request.nickname(),
                 request.agreedTermsOfUseVersion(), request.agreedPrivacyPolicyVersion(),
                 request.agreedAdInfoReceivingVersion());
     }
