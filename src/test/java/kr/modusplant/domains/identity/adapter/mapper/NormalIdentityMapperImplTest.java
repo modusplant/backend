7a196e442425e67a90ea04b41c03cda373af8057
Jun Hee <umpa9327@gmail.com>
2025-09-22 19:50:50 +0900

    MP-259 :white_check_mark: Test: NormalIdentityMapperImplTest 추가

---
+++ src/test/java/kr/modusplant/domains/identity/adapter/mapper/NormalIdentityMapperImplTest.java
@@ -0,0 +1,36 @@
+package kr.modusplant.domains.identity.adapter.mapper;
+
+import kr.modusplant.domains.identity.common.utils.domain.vo.SignUpDataTestUtils;
+import kr.modusplant.domains.identity.common.utils.usecase.request.NormalSignUpRequestTestUtils;
+import kr.modusplant.domains.identity.domain.vo.SignUpData;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NormalIdentityMapperImplTest implements
+        NormalSignUpRequestTestUtils, SignUpDataTestUtils {
+
+    private final NormalIdentityMapperImpl mapper = new NormalIdentityMapperImpl();
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    @Test
+    public void tesToSignUpData_givenValidRequest_willReturnSignUpData() {
+        // given & when
+        SignUpData result = mapper.toSignUpData(testNormalSignUpRequest);
+
+        // then
+        assertThat(result.getCredentials().getEmail())
+                .isEqualTo(testSignUpData.getCredentials().getEmail());
+        assertThat(encoder.matches(testCredentials.getPassword(), result.getCredentials().getPassword()))
+                .isTrue();
+        assertThat(result.getNickname().getNickname())
+                .isEqualTo(testSignUpData.getNickname().getNickname());
+        assertThat(result.getAgreedTermsOfUseVersion().getVersion())
+                .isEqualTo(testSignUpData.getAgreedTermsOfUseVersion().getVersion());
+        assertThat(result.getAgreedPrivacyPolicyVersion().getVersion())
+                .isEqualTo(testSignUpData.getAgreedPrivacyPolicyVersion().getVersion());
+        assertThat(result.getAgreedAdInfoReceivingVersion().getVersion())
+                .isEqualTo(testSignUpData.getAgreedAdInfoReceivingVersion().getVersion());
+    }
+}
