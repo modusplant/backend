18066f54b53be7ee3017e224340261f48491e65f
Jun Hee <umpa9327@gmail.com>
2025-09-22 16:59:04 +0900

    MP-259 :white_check_mark: Test: NormalIdentityRestControllerUnitTest를 Spring의 의존성을 뺀 단위 테스트로 변경함
    - 테스트에서 Spring의 의존성을 없애고 순수한 단위 테스트로 만들기 위해 변경함

--- src/test/java/kr/modusplant/domains/identity/framework/in/web/rest/NormalIdentityRestControllerUnitTest.java
+++ src/test/java/kr/modusplant/domains/identity/framework/in/web/rest/NormalIdentityRestControllerUnitTest.java
@@ -1,58 +1,54 @@
 package kr.modusplant.domains.identity.framework.in.web.rest;
 
+import kr.modusplant.domains.identity.adapter.controller.NormalIdentityController;
+import kr.modusplant.framework.out.jackson.http.response.DataResponse;
+import kr.modusplant.legacy.modules.jwt.common.util.domain.RefreshTokenTestUtils;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
+import org.mockito.Mockito;
+import org.springframework.http.ResponseEntity;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
-@WebMvcTest(controllers = NormalIdentityRestController.class,
-        excludeAutoConfiguration = SecurityAutoConfiguration.class)
-public class NormalIdentityRestControllerUnitTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    @Value("${jwt.refresh_duration}")
-    private long refreshDuration;
+public class NormalIdentityRestControllerUnitTest implements RefreshTokenTestUtils {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    public static ResultMatcher matchCookie(String name, String value,
-                                            String path, int maxAge,
-                                            boolean secure, boolean httpOnly) {
-        return result -> {
-            cookie().exists(name).match(result);
-            cookie().value(name, value).match(result);
-            cookie().path(name, path).match(result);
-            cookie().maxAge(name, maxAge).match(result);
-            cookie().secure(name, secure).match(result);
-            cookie().httpOnly(name, httpOnly).match(result);
-        };
-    }
+    private final NormalIdentityController controller = Mockito.mock(NormalIdentityController.class);
+    private final NormalIdentityRestController restController = new NormalIdentityRestController(controller);
 
     @Test
     public void testRespondToNormalLoginSuccess_givenValidToken_willReturnSuccess() throws Exception {
         // given
         String testAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwianRpIjoiYWJjMTIzeHl6NDU2IiwiZXhwIjoxNjM4NzY4MDIyLCJpYXQiOjE2MzYxNzYwMjJ9.7Qm6ZxQz3XW6J8KvY1lTn4RfG2HsPpLq1DwYb5Nv0eE";
-        String testRefreshToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+        String testRefreshToken = refreshTokenBasicUser.getRefreshToken();
 
         // when
-        mockMvc.perform(post("/api/auth/login-success")
-                        .requestAttr("accessToken", testAccessToken)
-                        .requestAttr("refreshToken", testRefreshToken))
+        ResponseEntity<DataResponse<Map<String, Object>>> response = restController.respondToNormalLoginSuccess(testAccessToken, testRefreshToken);
 
-                // then
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status").value(200))
-                .andExpect(jsonPath("$.data.accessToken").value(testAccessToken))
-                .andExpect(matchCookie(
-                        "refreshToken", testRefreshToken, "/",
-                        (int) refreshDuration, true, true
-                ));
+        String refreshTokenCookie = response.getHeaders().get("Set-Cookie").get(0);
+        Map<String, String> cookieResult = new HashMap<>();
+
+        for(String part: refreshTokenCookie.split(";")) {
+            if(part.contains("=")) {
+                String[] parts = part.trim().split("=");
+                cookieResult.put(parts[0], parts[1]);
+            } else{
+                cookieResult.put(part.trim(), part.trim());
+            }
+        }
+
+        // then
+        assertThat(response.getStatusCode().value()).isEqualTo(200);
+        assertThat(Objects.requireNonNull(response.getBody()).getData().get("accessToken"))
+                .isEqualTo(testAccessToken);
+        assertThat(refreshTokenCookie).isNotNull();
+        assertThat(cookieResult.get("refreshToken")).isEqualTo(testRefreshToken);
+        assertThat(cookieResult.get("Path")).isEqualTo("/");
+        assertThat(cookieResult.get("Secure")).isEqualTo("Secure");
+        assertThat(cookieResult.get("HttpOnly")).isEqualTo("HttpOnly");
+        assertThat(cookieResult.get("SameSite")).isEqualTo("Lax");
+
     }
 }
