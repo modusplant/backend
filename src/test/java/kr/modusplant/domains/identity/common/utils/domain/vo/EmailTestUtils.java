d3a23a692b699e7d6fab2dec60ff31b4b8b7c9bb
Jun Hee <umpa9327@gmail.com>
2025-09-29 17:57:45 +0900

    MP-259 :white_check_mark: Test: EmailTestUtils, PasswordTestUtils 추가
    - 리터럴 값으로 쓰이던 이메일과 비밀번호가 VO로 생성되어 관련된 테스트 유틸리티 클래스를 추가함

---
+++ src/test/java/kr/modusplant/domains/identity/common/utils/domain/vo/EmailTestUtils.java
@@ -0,0 +1,7 @@
+package kr.modusplant.domains.identity.common.utils.domain.vo;
+
+import kr.modusplant.domains.identity.domain.vo.Email;
+
+public interface EmailTestUtils {
+    Email testEmail = Email.create("test123@example.com");
+}
---
+++ src/test/java/kr/modusplant/domains/identity/common/utils/domain/vo/PasswordTestUtils.java
@@ -0,0 +1,7 @@
+package kr.modusplant.domains.identity.common.utils.domain.vo;
+
+import kr.modusplant.domains.identity.domain.vo.Password;
+
+public interface PasswordTestUtils {
+    Password testPassword = Password.create("password123!");
+}
