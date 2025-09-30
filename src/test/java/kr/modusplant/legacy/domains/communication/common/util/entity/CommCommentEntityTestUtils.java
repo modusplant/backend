b7cb31f9806790c91f56983d31836ffadb92d1eb
JunHyeok <akdnjs0308@gmail.com>
2025-10-01 00:27:48 +0900

    MP-307 :white_check_mark: Test: CommCommentEntityTestUtils 내부 데이터 상수화

--- src/test/java/kr/modusplant/legacy/domains/communication/common/util/entity/CommCommentEntityTestUtils.java
+++ src/test/java/kr/modusplant/legacy/domains/communication/common/util/entity/CommCommentEntityTestUtils.java
@@ -1,12 +1,14 @@
 package kr.modusplant.legacy.domains.communication.common.util.entity;
 
-import kr.modusplant.framework.out.jpa.entity.CommCommentEntity;
+import static kr.modusplant.framework.out.jpa.entity.CommCommentEntity.CommCommentEntityBuilder;
+import static kr.modusplant.framework.out.jpa.entity.CommCommentEntity.builder;
+import static kr.modusplant.framework.out.jpa.entity.constant.CommCommentConstant.TEST_COMM_COMMENT_CONTENT;
+import static kr.modusplant.framework.out.jpa.entity.constant.CommCommentConstant.TEST_COMM_COMMENT_PATH;
 
 public interface CommCommentEntityTestUtils extends CommPostEntityTestUtils {
-
-    default CommCommentEntity.CommCommentEntityBuilder createCommCommentEntityBuilder() {
-        return CommCommentEntity.builder()
-                .path("1.6.2")
-                .content("테스트 댓글 내용");
+    default CommCommentEntityBuilder createCommCommentEntityBuilder() {
+        return builder()
+                .path(TEST_COMM_COMMENT_PATH)
+                .content(TEST_COMM_COMMENT_CONTENT);
     }
 }
