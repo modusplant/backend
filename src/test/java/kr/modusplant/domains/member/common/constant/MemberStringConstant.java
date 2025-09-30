f0da5d9d9aabbe1b002212f50568301f58b2a75c
JunHyeok <akdnjs0308@gmail.com>
2025-10-01 00:57:58 +0900

    MP-307 :white_check_mark: Test: 회원 속박된 맥락 내부의 유틸리티 클래스에 공유되는 패키지에 있는 상수 적용

--- src/test/java/kr/modusplant/domains/member/common/constant/MemberStringConstant.java
+++ src/test/java/kr/modusplant/domains/member/common/constant/MemberStringConstant.java
@@ -1,11 +1,14 @@
 package kr.modusplant.domains.member.common.constant;
 
+import kr.modusplant.domains.member.domain.vo.MemberStatus;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import static kr.modusplant.shared.persistence.common.util.constant.SiteMemberConstant.MEMBER_BASIC_USER_NICKNAME;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class MemberStringConstant {
-    public static final String TEST_MEMBER_ACTIVE_STATUS = "활동 중";
-    public static final String TEST_MEMBER_INACTIVE_STATUS = "활동 정지";
-    public static final String TEST_MEMBER_NICKNAME = "testNickname";
+    public static final String TEST_MEMBER_ACTIVE_STATUS = MemberStatus.active().getValue();
+    public static final String TEST_MEMBER_INACTIVE_STATUS = MemberStatus.inactive().getValue();
+    public static final String TEST_MEMBER_NICKNAME = MEMBER_BASIC_USER_NICKNAME;
 }
