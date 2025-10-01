27c362545b8c006d7b5a19e32818c6e52c7f4437
JunHyeok <akdnjs0308@gmail.com>
2025-10-01 23:32:04 +0900

    MP-307 :sparkles: Feat: TargetCommentPath 및 관련한 도메인 패키지 내 요소 추가

--- src/main/java/kr/modusplant/domains/member/domain/exception/enums/MemberErrorCode.java
+++ src/main/java/kr/modusplant/domains/member/domain/exception/enums/MemberErrorCode.java
@@ -12,6 +12,7 @@ public enum MemberErrorCode implements ResponseCode {
     EMPTY_MEMBER_ID(HttpStatus.BAD_REQUEST, "empty_member_id", "회원 아이디가 비어 있습니다. "),
     EMPTY_MEMBER_NICKNAME(HttpStatus.BAD_REQUEST, "empty_member_nickname", "회원 닉네임이 비어 있습니다. "),
     EMPTY_MEMBER_STATUS(HttpStatus.BAD_REQUEST, "empty_member_status", "회원 상태가 비어 있습니다. "),
+    EMPTY_TARGET_COMMENT_PATH(HttpStatus.BAD_REQUEST, "empty_target_path", "대상 댓글 경로가 비어 있습니다. "),
     EMPTY_TARGET_POST_ID(HttpStatus.BAD_REQUEST, "empty_target_post_id", "대상 게시글 아이디가 비어 있습니다. "),
     ALREADY_EXISTED_NICKNAME(HttpStatus.BAD_REQUEST, "already_existed_nickname", "이미 동일한 닉네임이 존재합니다. "),
     NOT_FOUND_MEMBER_ID(HttpStatus.BAD_REQUEST, "not_found_member_id", "회원 아이디를 찾을 수 없습니다. "),
--- src/test/java/kr/modusplant/domains/member/common/constant/MemberStringConstant.java
+++ src/test/java/kr/modusplant/domains/member/common/constant/MemberStringConstant.java
@@ -4,6 +4,7 @@ import kr.modusplant.domains.member.domain.vo.MemberStatus;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import static kr.modusplant.shared.persistence.common.util.constant.CommCommentConstant.TEST_COMM_COMMENT_PATH;
 import static kr.modusplant.shared.persistence.common.util.constant.CommPostConstant.TEST_COMM_POST_ULID;
 import static kr.modusplant.shared.persistence.common.util.constant.SiteMemberConstant.MEMBER_BASIC_USER_NICKNAME;
 
@@ -12,5 +13,6 @@ public final class MemberStringConstant {
     public static final String TEST_MEMBER_ACTIVE_STATUS_STRING = MemberStatus.active().getValue();
     public static final String TEST_MEMBER_INACTIVE_STATUS_STRING = MemberStatus.inactive().getValue();
     public static final String TEST_MEMBER_NICKNAME_STRING = MEMBER_BASIC_USER_NICKNAME;
+    public static final String TEST_TARGET_COMMENT_PATH_STRING = TEST_COMM_COMMENT_PATH;
     public static final String TEST_TARGET_POST_ID_STRING = TEST_COMM_POST_ULID;
 }
