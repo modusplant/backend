e07cf44b5e03bb1cf63d1db2709b4fd3fa2c4aa8
Jun Hee <umpa9327@gmail.com>
2025-10-03 22:13:40 +0900

    MP-348 :sparkles: Feat: CommentJooqRepository 추가
    - JOOQ의 설정이 구현되지 않은 관계로 read 작업에 사용하는 구문을 선제적으로 구성한 뒤 주석처리하여 컴파일 에러가 발생하지 않도록 조치함

---
+++ src/main/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/supers/CommentJooqRepository.java
@@ -0,0 +1,47 @@
+package kr.modusplant.domains.comment.framework.out.persistence.jpa.repository.supers;
+
+import kr.modusplant.domains.comment.usecase.response.CommentResponse;
+import org.jooq.DSLContext;
+import org.jooq.impl.DSL;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public class CommentJooqRepository {
+//    private final DSLContext dsl = DSL.using("temporary url");
+//
+////    @Query("SELECT c.postEntity.ulid, c.id.path, m.nickname, c.content, c.isDeleted, c.createdAt " +
+////            "FROM CommCommentEntity c " +
+////            "INNER JOIN c.authMember m " +
+////            "WHERE c.postEntity.ulid = :postUlid " +
+////            "ORDER BY c.createdAt ASC")
+//    public List<CommentResponse> findByPostUlid() {
+//        return dsl.select(CommPost.ULID, CommComment.PATH, SiteMember.NICKNAME,
+//                CommComment.CONTENT, CommComment.IS_DELETED, CommComment.CREATED_AT)
+//                .from(CommPost, CommComment, SiteMember)
+//                .join(SiteMember).on(CommComment.AUTH_MEMB_UUID.eq(SiteMember.UUID))
+//                .where(CommComment.POST_ULID.eq(CommPost.ULID))
+//                .orderBy(CommComment.CREATED_AT.asc())
+//                .fetch();
+//    }
+//
+////    @Query("SELECT c.postEntity.ulid, c.id.path, m.nickname, c.content, c.isDeleted, c.createdAt " +
+////            "FROM CommCommentEntity c " +
+////            "INNER JOIN c.authMember m " +
+////            "WHERE c.authMember.uuid = :memberUuid " +
+////            "ORDER BY c.createdAt ASC")
+////    List<CommentResponse> findByAuthMemberUuid(@Param("memberUuid") UUID memberUuid);
+//    public List<CommentResponse> findByAuthMemberUuid() {
+//        return dsl.select(CommPost.ULID, CommComment.PATH, SiteMember.NICKNAME,
+//                        CommComment.CONTENT, CommComment.IS_DELETED, CommComment.CREATED_AT)
+//                .from(CommPost, CommComment, SiteMember)
+//                .join(SiteMember).on(CommComment.AUTH_MEMB_UUID.eq(SiteMember.UUID))
+//                .where(CommComment.AUTH_MEMB_UUID.eq(SiteMember.UUID))
+//                .orderBy(CommComment.CREATED_AT.asc())
+//                .fetch();
+//    }
+}
