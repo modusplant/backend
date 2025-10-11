030242b3466350ad811d12e1de7780f02ab5eba2
Jun Hee <umpa9327@gmail.com>
2025-10-11 21:13:50 +0900

    MP-348 :sparkles: Feat: CommentRepositoryJooqAdapter 추가
    - jOOQ 리포지토리를 레버러지하기 위해 추가함
    - jOOQ의 클래스가 생성되지 않는 관계로 null을 반환함

---
+++ src/main/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/CommentRepositoryJooqAdapter.java
@@ -0,0 +1,43 @@
+package kr.modusplant.domains.comment.framework.out.persistence.jpa.repository;
+
+import kr.modusplant.domains.comment.domain.aggregate.Comment;
+import kr.modusplant.domains.comment.domain.vo.Author;
+import kr.modusplant.domains.comment.domain.vo.PostId;
+import kr.modusplant.domains.comment.framework.out.persistence.jpa.repository.supers.CommentJooqRepository;
+import kr.modusplant.domains.comment.usecase.port.repository.CommentRepository;
+import kr.modusplant.domains.comment.usecase.response.CommentResponse;
+import kr.modusplant.shared.persistence.compositekey.CommCommentId;
+import lombok.RequiredArgsConstructor;
+import org.jooq.Result;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepositoryJooqAdapter implements CommentRepository {
+    private final CommentJooqRepository repository;
+    // private final CommentJooqMapper mapper;
+
+    @Override
+    public List<CommentResponse> findByPost(PostId postId) {
+//        return repository.findByPostUlid(postId.getId());
+        return null;
+    }
+
+    @Override
+    public List<CommentResponse> findByAuthor(Author author) {
+//        return repository.findByAuthMemberUuid(author.getMemberUuid());
+        return null;
+    }
+
+    @Override
+    public void save(Comment comment) {
+
+    }
+
+    @Override
+    public void deleteById(CommCommentId id) {
+
+    }
+}
