5bc26d094309b70097af2ea1124de0a01f59e848
Jun Hee <umpa9327@gmail.com>
2025-10-13 18:46:50 +0900

    MP-348 :sparkles: Feat: CommentJooqRepository에 jOOQ가 생성한 테이블 클래스 반영

--- src/main/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/supers/CommentJooqRepository.java
+++ src/main/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/supers/CommentJooqRepository.java
@@ -1,38 +1,48 @@
 package kr.modusplant.domains.comment.framework.out.persistence.jpa.repository.supers;
 
+import kr.modusplant.domains.comment.usecase.response.CommentResponse;
+import kr.modusplant.jooq.tables.CommComment;
+import kr.modusplant.jooq.tables.CommPost;
+import kr.modusplant.jooq.tables.SiteMember;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
 public class CommentJooqRepository {
 
     private final DSLContext dsl;
+    private final CommPost commPost;
+    private final CommComment commComment;
+    private final SiteMember siteMember;
 
-//    public List<CommentResponse> findByPostUlid(String postId) {
-//        return dsl.select(CommPost.ULID, CommComment.PATH, SiteMember.NICKNAME,
-//                CommComment.CONTENT, CommComment.IS_DELETED, CommComment.CREATED_AT)
-//                .from(CommPost, CommComment, SiteMember)
-//                .join(SiteMember).on(CommComment.AUTH_MEMB_UUID.eq(SiteMember.UUID))
-//                .where(CommComment.POST_ULID.eq(postId))
-//                .orderBy(CommComment.CREATED_AT.asc())
-//                .fetch(record -> new CommentResponse(
-//                    CommComment.ULID, CommComment.PATH, SiteMember.NICKNAME,
-//                    CommComment.CONTENT, CommComment.IS_DELETED, CommComment.CREATED_AT
-//                ));
-//    }
-//
-//    public List<CommentResponse> findByAuthMemberUuid(UUID authMemberUuid) {
-//        return dsl.select(CommPost.ULID, CommComment.PATH, SiteMember.NICKNAME,
-//                        CommComment.CONTENT, CommComment.IS_DELETED, CommComment.CREATED_AT)
-//                .from(CommPost, CommComment, SiteMember)
-//                .join(SiteMember).on(CommComment.AUTH_MEMB_UUID.eq(SiteMember.UUID))
-//                .where(CommComment.AUTH_MEMB_UUID.eq(authMemberUuid))
-//                .orderBy(CommComment.CREATED_AT.asc())
-//                .fetch(record -> new CommentResponse(
-//                        CommComment.ULID, CommComment.PATH, SiteMember.NICKNAME,
-//                        CommComment.CONTENT, CommComment.IS_DELETED, CommComment.CREATED_AT
-//                ));
-//    }
+    public List<CommentResponse> findByPostUlid(String postId) {
+        return dsl.select(commPost.ULID, commComment.PATH, siteMember.NICKNAME,
+                commComment.CONTENT, commComment.IS_DELETED, commComment.CREATED_AT)
+                .from(commPost, commComment, siteMember)
+                .join(siteMember).on(commComment.AUTH_MEMB_UUID.eq(siteMember.UUID))
+                .where(commComment.POST_ULID.eq(postId))
+                .orderBy(commComment.CREATED_AT.asc())
+                .fetch(record -> new CommentResponse(
+                    record.get(commComment.POST_ULID), record.get(commComment.PATH), record.get(siteMember.NICKNAME),
+                    record.get(commComment.CONTENT), record.get(commComment.IS_DELETED), record.get(commComment.CREATED_AT).toString()
+                ));
+    }
+
+    public List<CommentResponse> findByAuthMemberUuid(UUID authMemberUuid) {
+        return dsl.select(commPost.ULID, commComment.PATH, siteMember.NICKNAME,
+                        commComment.CONTENT, commComment.IS_DELETED, commComment.CREATED_AT)
+                .from(commPost, commComment, siteMember)
+                .join(siteMember).on(commComment.AUTH_MEMB_UUID.eq(siteMember.UUID))
+                .where(commComment.AUTH_MEMB_UUID.eq(authMemberUuid))
+                .orderBy(commComment.CREATED_AT.asc())
+                .fetch(record -> new CommentResponse(
+                        record.get(commComment.POST_ULID), record.get(commComment.PATH), record.get(siteMember.NICKNAME),
+                        record.get(commComment.CONTENT), record.get(commComment.IS_DELETED), record.get(commComment.CREATED_AT).toString()
+                ));
+    }
 }
