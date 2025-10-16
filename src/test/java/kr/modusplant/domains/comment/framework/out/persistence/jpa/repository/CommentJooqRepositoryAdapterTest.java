cab3e4f1e906d80ace4d3b9ba3fd3bcc14139c6d
Jun Hee <umpa9327@gmail.com>
2025-10-16 18:16:07 +0900

    MP-348 :white_check_mark: Test: CommentJooqRepositoryAdapterTest에 테스트 메서드 추가

--- src/test/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/CommentJooqRepositoryAdapterTest.java
+++ src/test/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/CommentJooqRepositoryAdapterTest.java
@@ -8,7 +8,6 @@ import kr.modusplant.domains.comment.framework.out.persistence.jpa.repository.su
 import kr.modusplant.domains.member.common.util.domain.vo.MemberIdTestUtils;
 import kr.modusplant.framework.out.jpa.entity.common.util.CommCommentEntityTestUtils;
 import kr.modusplant.framework.out.jpa.entity.common.util.CommCommentIdTestUtils;
-import kr.modusplant.jooq.tables.CommComment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -25,29 +24,29 @@ public class CommentJooqRepositoryAdapterTest implements PostIdTestUtils,
     private final CommentJooqRepository repository = Mockito.mock(CommentJooqRepository.class);
     private final CommentRepositoryJooqAdapter adapter = new CommentRepositoryJooqAdapter(repository);
 
-//    @Test
-//    @DisplayName("게시글 식별자로 댓글 가져오기")
-//    public void testFindByPost_givenValidPostUlid_willGetComment() {
-//        // given
-//        given(repository.findByPostUlid(testPostId.getId())).willReturn(List.of(testCommentResponse));
-//
-//        // when
-//        adapter.findByPost(testPostId);
-//
-//        // then
-//        Mockito.verify(repository, times(1)).findByPostUlid(testPostId.getId());
-//    }
-//
-//    @Test
-//    @DisplayName("사용자의 식별자로 댓글 가져오기")
-//    public void testFindByAuthor_givenValidAuthor_willGetComment() {
-//        // given
-//        given(repository.findByAuthMemberUuid(testAuthor.getMemberUuid())).willReturn(List.of(testCommentResponse));
-//
-//        // when
-//        adapter.findByAuthor(testAuthor);
-//
-//        // then
-//        Mockito.verify(repository, times(1)).findByAuthMemberUuid(testAuthor.getMemberUuid());
-//    }
+    @Test
+    @DisplayName("게시글 식별자로 댓글 가져오기")
+    public void testFindByPost_givenValidPostUlid_willGetComment() {
+        // given
+        given(repository.findByPostUlid(testPostId.getId())).willReturn(List.of(testCommentResponse));
+
+        // when
+        adapter.findByPost(testPostId);
+
+        // then
+        Mockito.verify(repository, times(1)).findByPostUlid(testPostId.getId());
+    }
+
+    @Test
+    @DisplayName("사용자의 식별자로 댓글 가져오기")
+    public void testFindByAuthor_givenValidAuthor_willGetComment() {
+        // given
+        given(repository.findByAuthMemberUuid(testAuthor.getMemberUuid())).willReturn(List.of(testCommentResponse));
+
+        // when
+        adapter.findByAuthor(testAuthor);
+
+        // then
+        Mockito.verify(repository, times(1)).findByAuthMemberUuid(testAuthor.getMemberUuid());
+    }
 }
