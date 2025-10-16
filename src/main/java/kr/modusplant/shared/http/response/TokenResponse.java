96489e6a92ab800d37d4357c54b43fd11787e0d6
songu1 <syj0002012@naver.com>
2025-10-16 20:17:50 +0900

    MP-274 :truck: Rename: TokenResponse 이동
    - TokenResponse를 infrastructure에서 shared로 이동

--- src/main/java/kr/modusplant/shared/http/response/TokenResponse.java
+++ src/main/java/kr/modusplant/shared/http/response/TokenResponse.java
@@ -1,10 +1,8 @@
-package kr.modusplant.infrastructure.jwt.framework.in.response;
+package kr.modusplant.shared.http.response;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record TokenResponse(
-        @Schema(description = "접근 토큰")
         @NotBlank(message = "토큰이 비어 있습니다.")
         String accessToken) {
 }
