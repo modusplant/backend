0e7ee313405466de1e8c8bbabb2de14b133554aa
Jun Hee <umpa9327@gmail.com>
2025-10-17 20:49:15 +0900

    MP-348 :truck: Rename: CommentRepositoryJpaAdapterTest
    - 테스트 클래스의 이름이 테스트 대상 클래스의 이름과 다르므로 수정함

--- src/test/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/CommentRepositoryJpaAdapterTest.java
+++ src/test/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/CommentRepositoryJpaAdapterTest.java
@@ -18,7 +18,7 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 
-public class CommentJpaRepositoryAdapterTest implements PostIdTestUtils,
+public class CommentRepositoryJpaAdapterTest implements PostIdTestUtils,
         CommentResponseTestUtils, MemberIdTestUtils,
         AuthorTestUtils, CommentTestUtils,
         CommCommentEntityTestUtils, CommCommentIdTestUtils {
