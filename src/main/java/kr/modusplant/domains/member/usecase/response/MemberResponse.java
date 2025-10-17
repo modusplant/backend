f2100518eb6bbb90fc113f8f64ff4d4ae63ed7c3
JunHyeok <akdnjs0308@gmail.com>
2025-10-17 12:17:16 +0900

    MP-403 :sparkles: Feat: 회원 속박된 맥락 내에서 요청 객체 빌드 및 응답 객체 Swagger 재설정

--- src/main/java/kr/modusplant/domains/member/usecase/response/MemberResponse.java
+++ src/main/java/kr/modusplant/domains/member/usecase/response/MemberResponse.java
@@ -1,7 +1,24 @@
 package kr.modusplant.domains.member.usecase.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
-public record MemberResponse(UUID uuid, String status, String nickname, LocalDate birthDate) {
+public record MemberResponse(
+        @Schema(description = "회원 아이디",
+                type = "UUID")
+        UUID uuid,
+
+        @Schema(description = "회원 상태",
+                example = "활동 중")
+        String status,
+
+        @Schema(description = "회원 닉네임",
+                example = "ModusPlantPlayer")
+        String nickname,
+
+        @Schema(description = "회원 생일",
+                type = "LocalDate")
+        LocalDate birthDate) {
 }
