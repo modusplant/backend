a16058124abd944aff1acb1b94b0f3bb4e0b15f3
Jun Hee <umpa9327@gmail.com>
2025-10-17 18:55:03 +0900

    MP-348 :white_check_mark: Test: CommentJooqRepositoryTest의 테스트 메서드 추가
    - CommentJooqRepository의 메서드들을 테스트하는 메서드 추가

--- src/test/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/CommentJooqRepositoryTest.java
+++ src/test/java/kr/modusplant/domains/comment/framework/out/persistence/jpa/repository/CommentJooqRepositoryTest.java
@@ -1,18 +1,105 @@
 package kr.modusplant.domains.comment.framework.out.persistence.jpa.repository;
 
+import kr.modusplant.domains.comment.common.util.domain.AuthorTestUtils;
+import kr.modusplant.domains.comment.common.util.domain.CommentContentTestUtils;
+import kr.modusplant.domains.comment.common.util.domain.CommentPathTestUtils;
+import kr.modusplant.domains.comment.common.util.domain.PostIdTestUtils;
 import kr.modusplant.domains.comment.framework.out.persistence.jpa.repository.supers.CommentJooqRepository;
-import kr.modusplant.jooq.tables.CommComment;
-import kr.modusplant.jooq.tables.CommPost;
-import kr.modusplant.jooq.tables.SiteMember;
+import kr.modusplant.domains.comment.usecase.response.CommentResponse;
+import kr.modusplant.domains.identity.normal.common.util.domain.vo.NicknameTestUtils;
 import org.jooq.DSLContext;
-import org.mockito.Mockito;
+import org.jooq.Record6;
+import org.jooq.Result;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+import org.jooq.tools.jdbc.MockConnection;
+import org.jooq.tools.jdbc.MockDataProvider;
+import org.jooq.tools.jdbc.MockExecuteContext;
+import org.jooq.tools.jdbc.MockResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-public class CommentJooqRepositoryTest {
-    private final DSLContext context = Mockito.mock(DSLContext.class);
-    private final CommPost commPost = CommPost.COMM_POST;
-    private final CommComment commComment = CommComment.COMM_COMMENT;
-    private final SiteMember siteMember = SiteMember.SITE_MEMBER;
-//    private final CommentJooqRepository repository = new CommentJooqRepository(context, commPost, commComment, siteMember);
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
+public class CommentJooqRepositoryTest implements
+        PostIdTestUtils, AuthorTestUtils, CommentPathTestUtils,
+        NicknameTestUtils, CommentContentTestUtils {
+
+    private Result<Record6<String, String, String, String, Boolean, LocalDateTime>> testResult;
+
+    private final MockDataProvider provider = new MockDataProvider() {
+        @Override
+        public MockResult[] execute(MockExecuteContext mockExecuteContext) {
+            return new MockResult[]{
+                    new MockResult(1, testResult)
+            };
+        }
+    };
+
+    private final MockConnection connection = new MockConnection(provider);
+    private final DSLContext dsl = DSL.using(connection, SQLDialect.POSTGRES);
+    private final CommentJooqRepository repository = new CommentJooqRepository(dsl);
+    private final LocalDateTime testDateTime = LocalDateTime.parse("2025-10-16 14:30:45", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+    @BeforeEach
+    void setUp() {
+        Record6<String, String, String, String, Boolean, LocalDateTime> testRecord = dsl.newRecord(
+                DSL.field("ulid", String.class),
+                DSL.field("path", String.class),
+                DSL.field("nickname", String.class),
+                DSL.field("content", String.class),
+                DSL.field("is_deleted", Boolean.class),
+                DSL.field("created_at", LocalDateTime.class)
+        );
+
+        testRecord.value1(testPostId.getId());
+        testRecord.value2(testCommentPath.getPath());
+        testRecord.value3(testNickname.getNickname());
+        testRecord.value4(testCommentContent.getContent());
+        testRecord.value5(false);
+        testRecord.value6(testDateTime);
+
+        testResult = dsl.newResult(testRecord.field1(), testRecord.field2(), testRecord.field3(),
+                        testRecord.field4(), testRecord.field5(), testRecord.field6());
+        testResult.add(testRecord);
+    }
+
+    @Test
+    @DisplayName("게시글의 식별자로 댓글 응답 가져오기")
+    void testFindByPostUlid_givenValidPostUlid_willReturnCommentResponse() {
+
+        // given & when
+        List<CommentResponse> result = repository.findByPostUlid(testPostId.getId());
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(result.getFirst().postId()).isEqualTo(testPostId.getId());
+        assertThat(result.getFirst().path()).isEqualTo(testCommentPath.getPath());
+        assertThat(result.getFirst().nickname()).isEqualTo(testNickname.getNickname());
+        assertThat(result.getFirst().content()).isEqualTo(testCommentContent.getContent());
+        assertThat(result.getFirst().isDeleted()).isFalse();
+        assertThat(result.getFirst().createdAt()).isEqualTo(testDateTime.toString());
+    }
+
+    @Test
+    @DisplayName("사용자의 식별자로 댓글 응답 가져오기")
+    void testFindByAuthMemberUuid_givenValidAuthMemberUuid_willReturnCommentResponse() {
+
+        // given & when
+        List<CommentResponse> result = repository.findByAuthMemberUuid(testAuthorWithUuid.getMemberUuid());
+
+        // then
+        assertThat(result).isNotNull();
+        assertThat(result.getFirst().postId()).isEqualTo(testPostId.getId());
+        assertThat(result.getFirst().path()).isEqualTo(testCommentPath.getPath());
+        assertThat(result.getFirst().nickname()).isEqualTo(testNickname.getNickname());
+        assertThat(result.getFirst().content()).isEqualTo(testCommentContent.getContent());
+        assertThat(result.getFirst().isDeleted()).isFalse();
+        assertThat(result.getFirst().createdAt()).isEqualTo(testDateTime.toString());
+    }
 }
