5574da5239b2c62bcdc48e8103d513b81bdbda31
Jun Hee <umpa9327@gmail.com>
2025-10-29 16:32:00 +0900

    MP-410 :sparkles: Feat: NormalIdentityRestController의 respondToNomalLoginSuccess에 Swagger 어노테이션 삭제
    - 해당 메서드는 보안 체인의 마지막 요소이고, 일반적인 컨트롤러 메서드처럼 요청의 도입부 역할을 하지 않으므로 Swagger의 어노테이션을 삭제함
    - 다른 팀원들의 이해를 돕기 위해 JavaDoc의 주석을 추가함

--- src/main/java/kr/modusplant/domains/identity/normal/framework/in/web/rest/NormalIdentityRestController.java
+++ src/main/java/kr/modusplant/domains/identity/normal/framework/in/web/rest/NormalIdentityRestController.java
@@ -50,22 +50,22 @@ public class NormalIdentityRestController {
 
     }
 
-    @Operation(
-            summary = "일반 로그인 API",
-            description = "토큰을 통해 일반 로그인 절차를 수행합니다."
-    )
+    /**
+     * Spring Security 의 일반 로그인 필터 체인의
+     * 성공 핸들러인 {@link kr.modusplant.infrastructure.security.handler.ForwardRequestLoginSuccessHandler} 가
+     * 인증 완료 후 forward 하는 메서드입니다.
+     * <p>클라이언트의 요청을 받는 도입부 역할을 하지 않으며, Swagger 어노테이션을 사용하지 않습니다. <p/>
+     *
+     * @param accessToken 클라이언트에게 보내는 접근 토큰입니다.
+     * @param refreshToken 클라이언트에게 보내는 갱신 토큰입니다.
+     * @return 클라이언트에게 로그인에 대한 성공 응답을 반환합니다.
+     */
     @PostMapping("/api/auth/login-success")
     public ResponseEntity<DataResponse<Map<String, Object>>> respondToNormalLoginSuccess(
-            @Parameter(schema = @Schema(
-                    description = "접근 토큰")
-            )
             @RequestAttribute("accessToken")
             @NotBlank(message = "접근 토큰이 비어 있습니다.")
             String accessToken,
 
-            @Parameter(schema = @Schema(
-                    description = "리프레시 토큰")
-            )
             @RequestAttribute("refreshToken")
             @NotBlank(message = "리프레시 토큰이 비어 있습니다.")
             String refreshToken) {
@@ -90,7 +90,7 @@ public class NormalIdentityRestController {
     /**
      * Spring Security 필터인 EmailPasswordAuthenticationFilter 에 등록된
      * 일반 로그인 API 의 경로를 Swagger UI에 등록하기 위해 만들어진 더미 메서드입니다.
-     * "절대로" 호출되거나, 사용될 일이 없습니다.
+     * <p>"절대로" 호출되거나, 사용될 일이 없습니다.<p/>
      * @param loginRequest 일반 회원가입에서 사용되는 이메일, 비밀번호로 구성되었습니다.
      */
     @Operation(
