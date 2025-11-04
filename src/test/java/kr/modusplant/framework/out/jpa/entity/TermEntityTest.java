281c4541050713eff5536a9d3b569050f5ce1f55
JunHyeok <akdnjs0308@gmail.com>
2025-11-04 18:54:07 +0900

    MP-406 :recycle: Refactor: 레거시 데이터 완전 삭제 및 그로 인한 리팩토링

--- src/test/java/kr/modusplant/framework/out/jpa/entity/TermEntityTest.java
+++ src/test/java/kr/modusplant/framework/out/jpa/entity/TermEntityTest.java
@@ -1,7 +1,7 @@
 package kr.modusplant.framework.out.jpa.entity;
 
 import kr.modusplant.infrastructure.context.RepositoryOnlyContext;
-import kr.modusplant.legacy.domains.term.common.util.entity.TermEntityTestUtils;
+import kr.modusplant.framework.out.jpa.entity.common.util.TermEntityTestUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
---
+++ src/test/java/kr/modusplant/shared/persistence/common/constant/TermConstant.java
@@ -0,0 +1,16 @@
+package kr.modusplant.shared.persistence.common.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+import static kr.modusplant.shared.util.VersionUtils.createVersion;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class TermConstant {
+    public static final UUID TERMS_OF_USE_UUID = UUID.fromString("848f747c-72c2-4a67-9266-71afc893b070");
+    public static final String TERMS_OF_USE_NAME = "이용약관";
+    public static final String TERMS_OF_USE_CONTENT = "이용약관 내용";
+    public static final String TERMS_OF_USE_VERSION = createVersion(1, 0, 0);
+}
