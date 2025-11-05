9da3b0b821b603af566eb904fb7a7ec5a41839aa
JunHyeok <akdnjs0308@gmail.com>
2025-11-05 22:42:38 +0900

    MP-443 :white_check_mark: Test: SiteMemberProfileEntity 관련 테스트 객체 추가

---
+++ src/test/java/kr/modusplant/framework/out/jpa/entity/common/util/SiteMemberProfileEntityTestUtils.java
@@ -0,0 +1,19 @@
+package kr.modusplant.framework.out.jpa.entity.common.util;
+
+import kr.modusplant.framework.out.jpa.entity.SiteMemberProfileEntity;
+
+import static kr.modusplant.shared.persistence.common.util.constant.SiteMemberProfileConstant.*;
+
+public interface SiteMemberProfileEntityTestUtils {
+    default SiteMemberProfileEntity.SiteMemberProfileEntityBuilder createMemberProfileBasicAdminEntityBuilder() {
+        return SiteMemberProfileEntity.builder()
+                .imageUrl(MEMBER_PROFILE_BASIC_ADMIN_IMAGE_URL)
+                .introduction(MEMBER_PROFILE_BASIC_ADMIN_INTRODUCTION);
+    }
+
+    default SiteMemberProfileEntity.SiteMemberProfileEntityBuilder createMemberProfileBasicUserEntityBuilder() {
+        return SiteMemberProfileEntity.builder()
+                .imageUrl(MEMBER_PROFILE_BASIC_USER_IMAGE_URL)
+                .introduction(MEMBER_PROFILE_BASIC_USER_INTRODUCTION);
+    }
+}
