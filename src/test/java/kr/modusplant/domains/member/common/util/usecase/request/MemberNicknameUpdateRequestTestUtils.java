89edb7879342d0a1b0be396d16b9458bb3a1002e
JunHyeok <akdnjs0308@gmail.com>
2025-11-08 00:35:27 +0900

    MP-443 :wrench: Chore: IMAGE_URL을 IMAGE_PATH로 변경

--- src/test/java/kr/modusplant/domains/member/common/util/usecase/request/MemberNicknameUpdateRequestTestUtils.java
+++ src/test/java/kr/modusplant/domains/member/common/util/usecase/request/MemberNicknameUpdateRequestTestUtils.java
@@ -3,9 +3,9 @@ package kr.modusplant.domains.member.common.util.usecase.request;
 import kr.modusplant.domains.member.usecase.request.MemberProfileUpdateRequest;
 
 import static kr.modusplant.shared.persistence.common.util.constant.SiteMemberConstant.MEMBER_BASIC_USER_NICKNAME;
-import static kr.modusplant.shared.persistence.common.util.constant.SiteMemberProfileConstant.MEMBER_PROFILE_BASIC_USER_IMAGE_URL;
+import static kr.modusplant.shared.persistence.common.util.constant.SiteMemberProfileConstant.MEMBER_PROFILE_BASIC_USER_IMAGE_PATH;
 import static kr.modusplant.shared.persistence.common.util.constant.SiteMemberProfileConstant.MEMBER_PROFILE_BASIC_USER_INTRODUCTION;
 
 public interface MemberNicknameUpdateRequestTestUtils {
-    MemberProfileUpdateRequest TEST_MEMBER_NICKNAME_UPDATE_REQUEST = new MemberProfileUpdateRequest(MEMBER_PROFILE_BASIC_USER_INTRODUCTION, MEMBER_PROFILE_BASIC_USER_IMAGE_URL, MEMBER_BASIC_USER_NICKNAME);
+    MemberProfileUpdateRequest TEST_MEMBER_NICKNAME_UPDATE_REQUEST = new MemberProfileUpdateRequest(MEMBER_PROFILE_BASIC_USER_INTRODUCTION, MEMBER_PROFILE_BASIC_USER_IMAGE_PATH, MEMBER_BASIC_USER_NICKNAME);
 }
