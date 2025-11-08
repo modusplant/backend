70b613298653557d14025ccfe68c35a6d8a6b41e
JunHyeok <akdnjs0308@gmail.com>
2025-11-08 12:16:11 +0900

    MP-443 :sparkles: Feat: MemberEmptyProfileIntroduction 추가 및 관련 테스트 추가

---
+++ src/test/java/kr/modusplant/domains/member/common/util/domain/vo/nullobject/MemberEmptyProfileIntroductionTestUtils.java
@@ -0,0 +1,7 @@
+package kr.modusplant.domains.member.common.util.domain.vo.nullobject;
+
+import kr.modusplant.domains.member.domain.vo.nullobject.MemberEmptyProfileIntroduction;
+
+public interface MemberEmptyProfileIntroductionTestUtils {
+    MemberEmptyProfileIntroduction testMemberEmptyProfileIntroduction = MemberEmptyProfileIntroduction.create();
+}
