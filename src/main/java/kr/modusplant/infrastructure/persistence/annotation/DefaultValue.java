bbfb0613f3939afdafad82381853891370530d6f
JunHyeok <akdnjs0308@gmail.com>
2025-11-18 18:12:02 +0900

    :memo: Docs: DefaultValue 의미 현 상황에 맞게 수정

--- src/main/java/kr/modusplant/infrastructure/persistence/annotation/DefaultValue.java
+++ src/main/java/kr/modusplant/infrastructure/persistence/annotation/DefaultValue.java
@@ -3,7 +3,7 @@ package kr.modusplant.infrastructure.persistence.annotation;
 import java.lang.annotation.*;
 
 /**
- * 수식된 필드는 테이블의 행 수준에서 설정된 디폴트 값을 가진다는 것을 의미하는 마커 어노테이션입니다.
+ * 수식된 필드는 Persist 시에 App 수준에서 디폴트 값을 줄 수 있어야 한다는 것을 의미하는 마커 어노테이션입니다.
  */
 @Target(ElementType.FIELD)
 @Documented
