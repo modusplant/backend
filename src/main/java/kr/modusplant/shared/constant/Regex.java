eba1f69ce8ec60a6f4d94cb2e9c392893e0ad769
JunHyeok <akdnjs0308@gmail.com>
2025-11-19 14:02:19 +0900

    MP-486 :recycle: Refactor: Pattern 상수가 Regex 상수 값을 사용하도록 수정

--- src/main/java/kr/modusplant/shared/constant/Regex.java
+++ src/main/java/kr/modusplant/shared/constant/Regex.java
@@ -7,19 +7,21 @@ import java.util.regex.Pattern;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Regex {
-    public static final Pattern PATTERN_EMAIL = Pattern.compile("^[A-Za-z0-9]+@[A-Za-z0-9]+\\.[a-z]{2,4}$");
-    public static final Pattern PATTERN_MATERIALIZED_PATH = Pattern.compile("^\\d+(?:\\.\\d+)*$");
-    public static final Pattern PATTERN_NICKNAME = Pattern.compile("^[가-힣A-Za-z0-9]{2,16}$");
-    public static final Pattern PATTERN_PASSWORD = Pattern.compile("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&()_\\-+=\\[\\]{}|\\\\;:'\",.<>/?]).{8,64}$");
-    public static final Pattern PATTERN_ULID = Pattern.compile("^[0-9A-HJKMNP-TV-Z]{26}$");
-    public static final Pattern PATTERN_UUID = Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
-    public static final Pattern PATTERN_VERSION = Pattern.compile("^v\\d+.\\d+.\\d+$");
-
     public static final String REGEX_EMAIL = "^[A-Za-z0-9]+@[A-Za-z0-9]+\\.[a-z]{2,4}$";
+    public static final String REGEX_JWT = "^[A-Za-z0-9-_=]+\\.[A-Za-z0-9-_=]+\\.?\\[A-Za-z0-9-_.+/=]*$";
     public static final String REGEX_MATERIALIZED_PATH = "^\\d+(?:\\.\\d+)*$";
     public static final String REGEX_NICKNAME = "^[가-힣A-Za-z0-9]{2,16}$";
     public static final String REGEX_PASSWORD = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&()_\\-+=\\[\\]{}|\\\\;:'\",.<>/?]).{8,64}$";
     public static final String REGEX_ULID = "^[0-9A-HJKMNP-TV-Z]{26}$";
     public static final String REGEX_UUID = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
     public static final String REGEX_VERSION = "^v\\d+.\\d+.\\d+$";
+
+    public static final Pattern PATTERN_EMAIL = Pattern.compile(REGEX_EMAIL);
+    public static final Pattern PATTERN_JWT = Pattern.compile(REGEX_JWT);
+    public static final Pattern PATTERN_MATERIALIZED_PATH = Pattern.compile(REGEX_MATERIALIZED_PATH);
+    public static final Pattern PATTERN_NICKNAME = Pattern.compile(REGEX_NICKNAME);
+    public static final Pattern PATTERN_PASSWORD = Pattern.compile(REGEX_PASSWORD);
+    public static final Pattern PATTERN_ULID = Pattern.compile(REGEX_ULID);
+    public static final Pattern PATTERN_UUID = Pattern.compile(REGEX_UUID);
+    public static final Pattern PATTERN_VERSION = Pattern.compile(REGEX_VERSION);
 }
