32840d5b76c47964d0723ef3161099d51701915a
JunHyeok <akdnjs0308@gmail.com>
2025-11-19 16:08:38 +0900

    MP-486 :sparkles: Feat: 회원 속박된 맥락 내에서 헤더의 ID와 매개변수로서의 ID를 비교하는 로직 추가

--- src/main/java/kr/modusplant/domains/member/framework/in/web/rest/MemberRestController.java
+++ src/main/java/kr/modusplant/domains/member/framework/in/web/rest/MemberRestController.java
@@ -9,11 +9,15 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import kr.modusplant.domains.member.adapter.controller.MemberController;
+import kr.modusplant.domains.member.domain.exception.IncorrectMemberIdException;
 import kr.modusplant.domains.member.usecase.record.*;
 import kr.modusplant.domains.member.usecase.request.MemberRegisterRequest;
 import kr.modusplant.domains.member.usecase.response.MemberProfileResponse;
 import kr.modusplant.domains.member.usecase.response.MemberResponse;
 import kr.modusplant.framework.jackson.http.response.DataResponse;
+import kr.modusplant.infrastructure.jwt.exception.InvalidTokenException;
+import kr.modusplant.infrastructure.jwt.exception.TokenExpiredException;
+import kr.modusplant.infrastructure.jwt.provider.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -35,6 +39,7 @@ import static kr.modusplant.shared.constant.Regex.*;
 @Validated
 public class MemberRestController {
     private final MemberController memberController;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Operation(summary = "회원 등록 API", description = "닉네임을 통해 회원을 등록합니다.")
     @PostMapping
@@ -64,7 +69,13 @@ public class MemberRestController {
             @Parameter(description = "기존에 저장된 회원의 아이디", schema = @Schema(type = "string", format = "uuid", pattern = REGEX_UUID))
             @PathVariable(required = false)
             @NotNull(message = "회원 아이디가 비어 있습니다. ")
-            UUID id) throws IOException {
+            UUID id,
+
+            @Parameter(description = "헤더로 전송되는 접근 토큰")
+            @RequestHeader(name = "Authorization")
+            @NotNull(message = "접근 토큰이 비어 있습니다. ")
+            String auth) throws IOException {
+        validateTokenAndAccessToId(id, auth);
         return ResponseEntity.status(HttpStatus.OK).body(
                 DataResponse.ok(memberController.getProfile(new MemberProfileGetRecord(id))));
     }
@@ -89,7 +100,13 @@ public class MemberRestController {
             @RequestPart(name = "nickname")
             @NotBlank(message = "회원 닉네임이 비어 있습니다. ")
             @Pattern(regexp = REGEX_NICKNAME, message = "회원 닉네임 서식이 올바르지 않습니다. ")
-            String nickname) throws IOException {
+            String nickname,
+
+            @Parameter(description = "헤더로 전송되는 접근 토큰")
+            @RequestHeader(name = "Authorization")
+            @NotNull(message = "접근 토큰이 비어 있습니다. ")
+            String auth) throws IOException {
+        validateTokenAndAccessToId(id, auth);
         return ResponseEntity.status(HttpStatus.OK).body(
                 DataResponse.ok(memberController.overrideProfile(new MemberProfileOverrideRecord(id, introduction, image, nickname))));
     }
@@ -105,7 +122,13 @@ public class MemberRestController {
             @Parameter(description = "좋아요를 누를 게시글의 식별자", schema = @Schema(type = "string", format = "ulid", pattern = REGEX_ULID))
             @PathVariable(required = false)
             @NotBlank(message = "게시글 식별자가 비어 있습니다.")
-            String postUlid) {
+            String postUlid,
+
+            @Parameter(description = "헤더로 전송되는 접근 토큰")
+            @RequestHeader(name = "Authorization")
+            @NotNull(message = "접근 토큰이 비어 있습니다. ")
+            String auth) {
+        validateTokenAndAccessToId(id, auth);
         memberController.likePost(new MemberPostLikeRecord(id, postUlid));
         return ResponseEntity.ok().body(DataResponse.ok());
     }
@@ -121,7 +144,13 @@ public class MemberRestController {
             @Parameter(description = "좋아요를 취소할 게시글의 식별자", schema = @Schema(type = "string", format = "ulid", pattern = REGEX_ULID))
             @PathVariable(required = false)
             @NotBlank(message = "게시글 식별자가 비어 있습니다.")
-            String postUlid) {
+            String postUlid,
+
+            @Parameter(description = "헤더로 전송되는 접근 토큰")
+            @RequestHeader(name = "Authorization")
+            @NotNull(message = "접근 토큰이 비어 있습니다. ")
+            String auth) {
+        validateTokenAndAccessToId(id, auth);
         memberController.unlikePost(new MemberPostUnlikeRecord(id, postUlid));
         return ResponseEntity.ok().body(DataResponse.ok());
     }
@@ -137,7 +166,13 @@ public class MemberRestController {
             @Parameter(description = "북마크를 누를 게시글의 식별자", schema = @Schema(type = "string", format = "ulid", pattern = REGEX_ULID))
             @PathVariable(required = false)
             @NotBlank(message = "게시글 식별자가 비어 있습니다.")
-            String postUlid) {
+            String postUlid,
+
+            @Parameter(description = "헤더로 전송되는 접근 토큰")
+            @RequestHeader(name = "Authorization")
+            @NotNull(message = "접근 토큰이 비어 있습니다. ")
+            String auth) {
+        validateTokenAndAccessToId(id, auth);
         memberController.bookmarkPost(new MemberPostBookmarkRecord(id, postUlid));
         return ResponseEntity.ok().body(DataResponse.ok());
     }
@@ -153,7 +188,13 @@ public class MemberRestController {
             @Parameter(description = "북마크를 취소할 게시글의 식별자", schema = @Schema(type = "string", format = "ulid", pattern = REGEX_ULID))
             @PathVariable(required = false)
             @NotBlank(message = "게시글 식별자가 비어 있습니다.")
-            String postUlid) {
+            String postUlid,
+
+            @Parameter(description = "헤더로 전송되는 접근 토큰")
+            @RequestHeader(name = "Authorization")
+            @NotNull(message = "접근 토큰이 비어 있습니다. ")
+            String auth) {
+        validateTokenAndAccessToId(id, auth);
         memberController.cancelPostBookmark(new MemberPostBookmarkCancelRecord(id, postUlid));
         return ResponseEntity.ok().body(DataResponse.ok());
     }
@@ -174,7 +215,13 @@ public class MemberRestController {
             @Parameter(description = "좋아요를 누를 댓글의 경로", example = "1.0.4", schema = @Schema(type = "string", pattern = REGEX_MATERIALIZED_PATH))
             @PathVariable(required = false)
             @NotBlank(message = "댓글 경로가 비어 있습니다.")
-            String path) {
+            String path,
+
+            @Parameter(description = "헤더로 전송되는 접근 토큰")
+            @RequestHeader(name = "Authorization")
+            @NotNull(message = "접근 토큰이 비어 있습니다. ")
+            String auth) {
+        validateTokenAndAccessToId(id, auth);
         memberController.likeComment(new MemberCommentLikeRecord(id, postUlid, path));
         return ResponseEntity.ok().body(DataResponse.ok());
     }
@@ -195,8 +242,29 @@ public class MemberRestController {
             @Parameter(description = "좋아요를 취소할 댓글의 경로", example = "1.0.4", schema = @Schema(type = "string", pattern = REGEX_MATERIALIZED_PATH))
             @PathVariable(required = false)
             @NotBlank(message = "댓글 경로가 비어 있습니다.")
-            String path) {
+            String path,
+
+            @Parameter(description = "헤더로 전송되는 접근 토큰")
+            @RequestHeader(name = "Authorization")
+            @NotNull(message = "접근 토큰이 비어 있습니다. ")
+            String auth) {
+        validateTokenAndAccessToId(id, auth);
         memberController.unlikeComment(new MemberCommentUnlikeRecord(id, postUlid, path));
         return ResponseEntity.ok().body(DataResponse.ok());
+    }
+
+    private void validateTokenAndAccessToId(UUID id, String auth) {
+        String accessToken;
+        if (auth.startsWith("Bearer ")) {
+            accessToken = auth.substring(7);
+        } else {
+            throw new InvalidTokenException();
+        }
+        if (!jwtTokenProvider.validateToken(accessToken)) {
+            throw new TokenExpiredException();
+        }
+        if (!jwtTokenProvider.getMemberUuidFromToken(accessToken).equals(id)) {
+            throw new IncorrectMemberIdException();
+        }
     }
 }
