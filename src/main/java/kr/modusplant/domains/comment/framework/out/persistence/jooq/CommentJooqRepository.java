6758f4a6f1850bcee8295b5c11761a379ddd955c
Jun Hee <umpa9327@gmail.com>
2025-11-26 20:47:52 +0900

    MP-424 :sparkles: Feat: 댓글 API 복구

--- src/main/java/kr/modusplant/domains/comment/framework/out/persistence/jooq/CommentJooqRepository.java
+++ src/main/java/kr/modusplant/domains/comment/framework/out/persistence/jooq/CommentJooqRepository.java
@@ -3,15 +3,23 @@ package kr.modusplant.domains.comment.framework.out.persistence.jooq;
 import kr.modusplant.domains.comment.domain.vo.Author;
 import kr.modusplant.domains.comment.domain.vo.PostId;
 import kr.modusplant.domains.comment.usecase.port.repository.CommentReadRepository;
-import kr.modusplant.domains.comment.usecase.response.CommentResponse;
+import kr.modusplant.domains.comment.usecase.model.CommentOfAuthorPageModel;
+import kr.modusplant.domains.comment.usecase.response.CommentOfPostResponse;
 import kr.modusplant.jooq.tables.CommComment;
 import kr.modusplant.jooq.tables.CommPost;
 import kr.modusplant.jooq.tables.SiteMember;
 import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
+import org.jooq.Record1;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
+import static org.jooq.impl.DSL.count;
 
 @Repository
 @RequiredArgsConstructor
@@ -22,29 +30,43 @@ public class CommentJooqRepository implements CommentReadRepository {
     private final CommComment commComment = CommComment.COMM_COMMENT;
     private final SiteMember siteMember = SiteMember.SITE_MEMBER;
 
-    public List<CommentResponse> findByPost(PostId postId) {
-        return dsl.select(commPost.ULID, commComment.PATH, siteMember.NICKNAME,
-                commComment.CONTENT, commComment.IS_DELETED, commComment.CREATED_AT)
-                .from(commPost, commComment, siteMember)
-                .join(siteMember).on(commComment.AUTH_MEMB_UUID.eq(siteMember.UUID))
+    public List<CommentOfPostResponse> findByPost(PostId postId) {
+        return dsl.select(siteMember.NICKNAME, commComment.PATH, commComment.CONTENT,
+                        commComment.LIKE_COUNT, commComment.CREATED_AT, commComment.IS_DELETED)
+                .from(commComment)
+                .join(commPost).on(commComment.POST_ULID.eq(commPost.ULID))
+                .join(siteMember).on(commPost.AUTH_MEMB_UUID.eq(siteMember.UUID))
                 .where(commComment.POST_ULID.eq(postId.getId()))
-                .orderBy(commComment.CREATED_AT.asc())
-                .fetch(record -> new CommentResponse(
-                    record.get(commPost.ULID), record.get(commComment.PATH), record.get(siteMember.NICKNAME),
-                    record.get(commComment.CONTENT), record.get(commComment.IS_DELETED), record.get(commComment.CREATED_AT).toString()
-                ));
+                .orderBy(commComment.CREATED_AT.desc())
+                .fetchInto(CommentOfPostResponse.class);
     }
 
-    public List<CommentResponse> findByAuthor(Author author) {
-        return dsl.select(commPost.ULID, commComment.PATH, siteMember.NICKNAME,
-                        commComment.CONTENT, commComment.IS_DELETED, commComment.CREATED_AT)
-                .from(commPost, commComment, siteMember)
+    public PageImpl<CommentOfAuthorPageModel> findByAuthor(Author author, Pageable pageable) {
+
+        Optional<Record1<Integer>> totalComments = dsl.selectCount()
+                .from(commComment)
                 .join(siteMember).on(commComment.AUTH_MEMB_UUID.eq(siteMember.UUID))
                 .where(commComment.AUTH_MEMB_UUID.eq(author.getMemberUuid()))
-                .orderBy(commComment.CREATED_AT.asc())
-                .fetch(record -> new CommentResponse(
-                        record.get(commPost.ULID), record.get(commComment.PATH), record.get(siteMember.NICKNAME),
-                        record.get(commComment.CONTENT), record.get(commComment.IS_DELETED), record.get(commComment.CREATED_AT).toString()
-                ));
+                .fetchOptional();
+
+        if(totalComments.isPresent()) {
+            int totalComment = totalComments.get().component1();
+
+            List<CommentOfAuthorPageModel> commentList = dsl.select(commComment.CONTENT, commComment.CREATED_AT,
+                            commPost.TITLE, count(commComment.POST_ULID))
+                    .from(commComment)
+                    .join(siteMember).on(commComment.AUTH_MEMB_UUID.eq(siteMember.UUID))
+                    .join(commPost).on(commComment.POST_ULID.eq(commPost.ULID))
+                    .where(commComment.AUTH_MEMB_UUID.eq(author.getMemberUuid()))
+                    .groupBy(commComment.CONTENT, commComment.CREATED_AT, commPost.TITLE)
+                    .orderBy(commComment.CREATED_AT.desc())
+                    .limit(pageable.getPageSize())
+                    .offset(pageable.getOffset())
+                    .fetchInto(CommentOfAuthorPageModel.class);
+
+            return new PageImpl<>(commentList, pageable, totalComment);
+        } else {
+            return new PageImpl<>(new ArrayList<>(), pageable, 0);
+        }
     }
 }
