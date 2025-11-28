667a5854e15b9a7542eb600f19706b651f158cce
songu1 <syj0002012@naver.com>
2025-11-28 12:55:27 +0900

    MP-469 :sparkles: Feat: 소통 항목 어댑터 계층 및 RestController 구현
    - 소통 항목 adapter 계층의 컨트롤러와 매퍼 구현
    - 소통 항목 RestController 와 Response 객체 구현

---
+++ src/main/java/kr/modusplant/domains/post/usecase/response/SecondaryCategoryResponse.java
@@ -0,0 +1,23 @@
+package kr.modusplant.domains.post.usecase.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.UUID;
+
+public record SecondaryCategoryResponse(
+        @Schema(description = "2차 항목의 식별자", example = "12d0d32d-f907-4605-a9d0-00b5669ea18a")
+        @JsonProperty("id")
+        UUID uuid,
+
+        @Schema(description = "2차 항목이 포함된 1차 항목의 식별자", example = "2d9f462d-b50f-4394-928e-5c864f60b09a")
+        @JsonProperty("primaryCategoryId")
+        UUID primaryCategoryId,
+
+        @Schema(description = "2차 항목의 카테고리명", example = "물꽂이 + 잎꽂이")
+        String category,
+
+        @Schema(description = "2차 항목의 순서", example = "1")
+        Integer order
+) {
+}
