ec4d5bcd14ac7c3fdb38cb6479c170ddeee9635f
songu1 <syj0002012@naver.com>
2025-11-28 12:50:18 +0900

    MP-469 :sparkles: Feat: 소통 항목 JPA 리포지토리/어댑터 및 매퍼 구현
    - 소통 항목 JPQ 리포지토리 및 어댑터 생성
    - 소통 항목과 VO 간 변환하는 매퍼 새엇ㅇ

---
+++ src/main/java/kr/modusplant/domains/post/framework/out/jpa/mapper/PrimaryCategoryJpaMapperImpl.java
@@ -0,0 +1,19 @@
+package kr.modusplant.domains.post.framework.out.jpa.mapper;
+
+import kr.modusplant.domains.post.domain.vo.PrimaryCategory;
+import kr.modusplant.domains.post.domain.vo.PrimaryCategoryId;
+import kr.modusplant.domains.post.framework.out.jpa.mapper.supers.PrimaryCategoryJpaMapper;
+import kr.modusplant.framework.jpa.entity.CommPrimaryCategoryEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrimaryCategoryJpaMapperImpl implements PrimaryCategoryJpaMapper {
+    @Override
+    public PrimaryCategory toPrimaryCategory(CommPrimaryCategoryEntity primaryCategoryEntity) {
+        return PrimaryCategory.create(
+                PrimaryCategoryId.fromUuid(primaryCategoryEntity.getUuid()),
+                primaryCategoryEntity.getCategory(),
+                primaryCategoryEntity.getOrder()
+        );
+    }
+}
