60717a0a6618c1d7f98c376468404497200be448
Jun Hee <umpa9327@gmail.com>
2025-12-11 16:41:49 +0900

    MP-531 :sparkles: Feat: 사용자의 인증 정보를 가져오는 API에 createdAt 필드 추가

--- src/main/java/kr/modusplant/domains/identity/account/framework/out/joop/AccountJooqRepository.java
+++ src/main/java/kr/modusplant/domains/identity/account/framework/out/joop/AccountJooqRepository.java
@@ -3,6 +3,7 @@ package kr.modusplant.domains.identity.account.framework.out.joop;
 import kr.modusplant.domains.identity.account.domain.vo.MemberId;
 import kr.modusplant.domains.identity.account.usecase.port.repository.AccountRepository;
 import kr.modusplant.domains.identity.account.usecase.response.AccountAuthResponse;
+import kr.modusplant.jooq.tables.SiteMember;
 import kr.modusplant.jooq.tables.SiteMemberAuth;
 import kr.modusplant.shared.enums.AuthProvider;
 import lombok.RequiredArgsConstructor;
@@ -15,14 +16,18 @@ public class AccountJooqRepository implements AccountRepository {
 
     private final DSLContext dsl;
     private final SiteMemberAuth memberAuth = SiteMemberAuth.SITE_MEMBER_AUTH;
+    private final SiteMember member = SiteMember.SITE_MEMBER;
 
     @Override
     public AccountAuthResponse getAuthInfo(MemberId id) {
-        return dsl.select(memberAuth.EMAIL, memberAuth.PROVIDER)
+        return dsl.select(memberAuth.EMAIL, memberAuth.PROVIDER, member.CREATED_AT)
                 .from(memberAuth)
+                .join(member).on(memberAuth.ACT_MEMB_UUID.eq(member.UUID))
                 .where(memberAuth.ACT_MEMB_UUID.eq(id.getValue()))
                 .fetchOne(record -> new AccountAuthResponse(
-                        record.get(memberAuth.EMAIL), AuthProvider.valueOf(record.get(memberAuth.PROVIDER))
+                        record.get(memberAuth.EMAIL),
+                        AuthProvider.valueOf(record.get(memberAuth.PROVIDER)),
+                        record.get(member.CREATED_AT).toLocalDate()
                 ));
     }
 }
