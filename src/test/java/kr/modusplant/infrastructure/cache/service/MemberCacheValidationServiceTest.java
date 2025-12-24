574469d2615bbc29134f8680e917616089a5dbe5
JunHyeok <akdnjs0308@gmail.com>
2025-12-24 19:40:31 +0900

    :recycle: Refactor: 캐시 관련 인프라 클래스를 각 도메인 안에서 사용하도록 디렉토리 이동 및 리팩토링

--- src/test/java/kr/modusplant/infrastructure/cache/service/MemberCacheValidationServiceTest.java
+++ src/test/java/kr/modusplant/infrastructure/cache/service/MemberCacheValidationServiceTest.java
@@ -1,5 +1,6 @@
 package kr.modusplant.infrastructure.cache.service;
 
+import kr.modusplant.domains.member.framework.in.web.cache.record.MemberCacheValidationResult;
 import kr.modusplant.domains.member.framework.in.web.cache.service.MemberCacheValidationService;
 import kr.modusplant.framework.jpa.entity.SiteMemberProfileEntity;
 import kr.modusplant.framework.jpa.entity.common.util.SiteMemberEntityTestUtils;
@@ -19,7 +20,6 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -36,18 +36,17 @@ class MemberCacheValidationServiceTest implements SiteMemberEntityTestUtils, Sit
     private final MemberCacheValidationService memberCacheValidationService = new MemberCacheValidationService(memberProfileJpaRepository, passwordEncoder);
 
     private final UUID id = UUID.randomUUID();
-    private final String RESULT = "result";
 
     @Test
     @DisplayName("비어 있는 프로필로 인해 예외 발생")
-    void testIsCacheUsableForSiteMemberProfile_givenEmptyMemberProfile_willThrowException() {
+    void testIsCacheable_willThrowException() {
         // given
         given(memberProfileJpaRepository.findByUuid(any())).willReturn(Optional.empty());
 
         // when
         EntityNotFoundException exception = assertThrows(
                 EntityNotFoundException.class,
-                () -> memberCacheValidationService.isCacheUsableForSiteMemberProfile(
+                () -> memberCacheValidationService.isCacheable(
                         String.format("\"%s\"", passwordEncoder.encode(UUID.randomUUID() + "-0")),
                         ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME),
                         id));
@@ -58,7 +57,7 @@ class MemberCacheValidationServiceTest implements SiteMemberEntityTestUtils, Sit
 
     @Test
     @DisplayName("ifNoneMatch가 null일 때 응답 반환")
-    void testIsCacheUsableForSiteMemberProfile_givenNullIfNoneMatch_willReturnResponse() {
+    void testIsCacheable_givenNullIfNoneMatch_willReturnResponse() {
         // given
         Optional<SiteMemberProfileEntity> optionalMemberProfileEntity = Optional.of(
                 createMemberProfileBasicUserEntityBuilder().member(createMemberBasicUserEntityWithUuid()).build());
@@ -68,16 +67,16 @@ class MemberCacheValidationServiceTest implements SiteMemberEntityTestUtils, Sit
         given(memberProfileJpaRepository.findByUuid(any())).willReturn(optionalMemberProfileEntity);
 
         // when
-        Map<String, ?> returnedMap = memberCacheValidationService.isCacheUsableForSiteMemberProfile(
+        MemberCacheValidationResult returnedMap = memberCacheValidationService.isCacheable(
                 null, ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME), id);
 
         // then
-        assertThat(returnedMap.get(RESULT)).isEqualTo(false);
+        assertThat(returnedMap.isCacheable()).isEqualTo(false);
     }
 
     @Test
     @DisplayName("매칭되는 엔터티 태그가 없을 때 응답 반환")
-    void testIsCacheUsableForSiteMemberProfile_givenNotMatchedEntityTag_willReturnResponse() {
+    void testIsCacheable_givenNotMatchedEntityTag_willReturnResponse() {
         // given
         Optional<SiteMemberProfileEntity> optionalMemberProfileEntity = Optional.of(
                 createMemberProfileBasicUserEntityBuilder().member(createMemberBasicUserEntityWithUuid()).build());
@@ -87,18 +86,18 @@ class MemberCacheValidationServiceTest implements SiteMemberEntityTestUtils, Sit
         given(memberProfileJpaRepository.findByUuid(any())).willReturn(optionalMemberProfileEntity);
 
         // when
-        Map<String, ?> returnedMap = memberCacheValidationService.isCacheUsableForSiteMemberProfile(
+        MemberCacheValidationResult returnedMap = memberCacheValidationService.isCacheable(
                 String.format("\"%s\"", passwordEncoder.encode(memberProfileEntity.getUuid() + "-99")),
                 ZonedDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME),
                 id);
 
         // then
-        assertThat(returnedMap.get(RESULT)).isEqualTo(false);
+        assertThat(returnedMap.isCacheable()).isEqualTo(false);
     }
 
     @Test
     @DisplayName("매칭되는 엔터티 태그가 있고 ifModifiedSince가 null일 때 응답 반환")
-    void testIsCacheUsableForSiteMemberProfile_givenMatchedEntityTagAndNullIfModifiedSince_willReturnResponse() {
+    void testIsCacheable_givenMatchedEntityTagAndNullIfModifiedSince_willReturnResponse() {
         // given
         Optional<SiteMemberProfileEntity> optionalMemberProfileEntity = Optional.of(
                 createMemberProfileBasicUserEntityBuilder().member(createMemberBasicUserEntityWithUuid()).build());
@@ -108,18 +107,18 @@ class MemberCacheValidationServiceTest implements SiteMemberEntityTestUtils, Sit
         given(memberProfileJpaRepository.findByUuid(any())).willReturn(optionalMemberProfileEntity);
 
         // when
-        Map<String, ?> returnedMap = memberCacheValidationService.isCacheUsableForSiteMemberProfile(
+        MemberCacheValidationResult returnedMap = memberCacheValidationService.isCacheable(
                 String.format("\"%s\"", passwordEncoder.encode(memberProfileEntity.getETagSource())),
                 null,
                 id);
 
         // then
-        assertThat(returnedMap.get(RESULT)).isEqualTo(true);
+        assertThat(returnedMap.isCacheable()).isEqualTo(true);
     }
 
     @Test
     @DisplayName("매칭되는 엔터티 태그가 있고 ifModifiedSince가 조건을 만족할 때 응답 반환")
-    void testIsCacheUsableForSiteMemberProfile_givenMatchedEntityTagAndRangedIfModifiedSince_willReturnResponse() {
+    void testIsCacheable_givenMatchedEntityTagAndRangedIfModifiedSince_willReturnResponse() {
         // given
         Optional<SiteMemberProfileEntity> optionalMemberProfileEntity = Optional.of(
                 createMemberProfileBasicUserEntityBuilder().member(createMemberBasicUserEntityWithUuid()).build());
@@ -130,30 +129,30 @@ class MemberCacheValidationServiceTest implements SiteMemberEntityTestUtils, Sit
 
         // 엔터티의 lastModifiedAt 값이 ifModifiedSince 값과 같을 때
         // when
-        Map<String, ?> returnedMapEqual = memberCacheValidationService.isCacheUsableForSiteMemberProfile(
+        MemberCacheValidationResult returnedMapEqual = memberCacheValidationService.isCacheable(
                 String.format("\"%s\"", passwordEncoder.encode(memberProfileEntity.getETagSource())),
                 ZonedDateTime.of(memberProfileEntity.getLastModifiedAtAsTruncatedToSeconds(),
                         ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.RFC_1123_DATE_TIME),
                 id);
 
         // then
-        assertThat(returnedMapEqual.get(RESULT)).isEqualTo(true);
+        assertThat(returnedMapEqual.isCacheable()).isEqualTo(true);
 
         // 엔터티의 lastModifiedAt 값이 ifModifiedSince 값보다 과거일 때
         // when
-        Map<String, ?> returnedMapPast = memberCacheValidationService.isCacheUsableForSiteMemberProfile(
+        MemberCacheValidationResult returnedMapPast = memberCacheValidationService.isCacheable(
                 String.format("\"%s\"", passwordEncoder.encode(memberProfileEntity.getETagSource())),
                 ZonedDateTime.of(memberProfileEntity.getLastModifiedAtAsTruncatedToSeconds().plusMinutes(5),
                         ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.RFC_1123_DATE_TIME),
                 id);
 
         // then
-        assertThat(returnedMapPast.get(RESULT)).isEqualTo(true);
+        assertThat(returnedMapPast.isCacheable()).isEqualTo(true);
     }
 
     @Test
     @DisplayName("매칭되는 엔터티 태그가 있고 ifModifiedSince가 조건을 만족하지 않을 때 응답 반환")
-    void testIsCacheUsableForSiteMemberProfile_givenMatchedEntityTagAndNotRangedIfModifiedSince_willReturnResponse() {
+    void testIsCacheable_givenMatchedEntityTagAndNotRangedIfModifiedSince_willReturnResponse() {
         // given
         Optional<SiteMemberProfileEntity> optionalMemberProfileEntity = Optional.of(
                 createMemberProfileBasicUserEntityBuilder().member(createMemberBasicUserEntityWithUuid()).build());
@@ -163,13 +162,13 @@ class MemberCacheValidationServiceTest implements SiteMemberEntityTestUtils, Sit
         given(memberProfileJpaRepository.findByUuid(any())).willReturn(optionalMemberProfileEntity);
 
         // when
-        Map<String, ?> returnedMapEqual = memberCacheValidationService.isCacheUsableForSiteMemberProfile(
+        MemberCacheValidationResult returnedMapEqual = memberCacheValidationService.isCacheable(
                 String.format("\"%s\"", passwordEncoder.encode(memberProfileEntity.getETagSource())),
                 ZonedDateTime.of(memberProfileEntity.getLastModifiedAtAsTruncatedToSeconds().minusMinutes(5),
                         ZoneId.of("Asia/Seoul")).format(DateTimeFormatter.RFC_1123_DATE_TIME),
                 id);
 
         // then
-        assertThat(returnedMapEqual.get(RESULT)).isEqualTo(false);
+        assertThat(returnedMapEqual.isCacheable()).isEqualTo(false);
     }
 }
