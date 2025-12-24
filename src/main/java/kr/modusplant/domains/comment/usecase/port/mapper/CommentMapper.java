1f18201a0f85c51e26df75c048d2114d45dcf497
Jun Hee <umpa9327@gmail.com>
2025-12-24 18:43:21 +0900

    MP-533 :wrench: Chore: 사용하지 않는 import문 제거

--- src/main/java/kr/modusplant/domains/comment/usecase/port/mapper/CommentMapper.java
+++ src/main/java/kr/modusplant/domains/comment/usecase/port/mapper/CommentMapper.java
@@ -6,8 +6,6 @@ import kr.modusplant.domains.comment.domain.vo.CommentContent;
 import kr.modusplant.domains.comment.domain.vo.CommentPath;
 import kr.modusplant.domains.comment.domain.vo.PostId;
 
-import java.util.UUID;
-
 public interface CommentMapper {
 
     Comment toComment(PostId postId, CommentPath path, Author author, CommentContent content);
