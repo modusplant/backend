6edc3913add372015dfa0bf494e464c6e02b3ba2
songu1 <syj0002012@naver.com>
2025-12-30 20:31:02 +0900

    :white_check_mark: Test: 카테고리 관련 게시글 도메인의 테스트 유틸리티 및 헬퍼 수정
    - 게시글/카테고리 테스트 유틸리티 및 헬퍼 수정

--- src/test/java/kr/modusplant/domains/post/common/util/domain/vo/SecondaryCategoryIdTestUtils.java
+++ src/test/java/kr/modusplant/domains/post/common/util/domain/vo/SecondaryCategoryIdTestUtils.java
@@ -2,10 +2,7 @@ package kr.modusplant.domains.post.common.util.domain.vo;
 
 import kr.modusplant.domains.post.domain.vo.SecondaryCategoryId;
 
-import static kr.modusplant.domains.post.common.constant.PostUuidConstant.TEST_POST_UUID;
-import static kr.modusplant.domains.post.common.constant.PostUuidConstant.TEST_POST_UUID2;
-
 public interface SecondaryCategoryIdTestUtils {
-    SecondaryCategoryId testSecondaryCategoryId = SecondaryCategoryId.fromUuid(TEST_POST_UUID);
-    SecondaryCategoryId testSecondaryCategoryId2 = SecondaryCategoryId.fromUuid(TEST_POST_UUID2);
+    SecondaryCategoryId testSecondaryCategoryId = SecondaryCategoryId.create(1);
+    SecondaryCategoryId testSecondaryCategoryId2 = SecondaryCategoryId.create(2);
 }
