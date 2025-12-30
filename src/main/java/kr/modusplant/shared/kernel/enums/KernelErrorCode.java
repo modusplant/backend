0e8e682a1c6f2c87bd881891bcc49084b0386454
Jun Hee <umpa9327@gmail.com>
2025-12-30 21:44:02 +0900

    MP-257 :sparkles: Feat: KernelErrorCode의 code와 message 수정
    - 각 예외 코드들에 맞도록 수정함

--- src/main/java/kr/modusplant/shared/kernel/enums/KernelErrorCode.java
+++ src/main/java/kr/modusplant/shared/kernel/enums/KernelErrorCode.java
@@ -10,24 +10,24 @@ import lombok.RequiredArgsConstructor;
 public enum KernelErrorCode implements ResponseCode {
 
     // Empty
-    EMPTY_EMAIL(HttpStatus.BAD_REQUEST, "empty_email","이메일이 비었습니다"),
-    EMPTY_NICKNAME(HttpStatus.BAD_REQUEST, "nickname_empty", "닉네임이 비어 있습니다"),
-    EMPTY_PASSWORD(HttpStatus.BAD_REQUEST, "password_empty", "비밀번호가 비어 있습니다"),
+    EMPTY_EMAIL(HttpStatus.BAD_REQUEST, "empty_email","이메일 값이 비었습니다"),
+    EMPTY_NICKNAME(HttpStatus.BAD_REQUEST, "nickname_empty", "닉네임 값이 비었습니다"),
+    EMPTY_PASSWORD(HttpStatus.BAD_REQUEST, "password_empty", "비밀번호 값이 비었습니다"),
 
     // Not Found
-    NOT_FOUND_EMAIL(HttpStatus.BAD_REQUEST, "empty_email","이메일이 비었습니다"),
-    NOT_FOUND_NICKNAME(HttpStatus.BAD_REQUEST, "nickname_empty", "닉네임이 비어 있습니다"),
-    NOT_FOUND_PASSWORD(HttpStatus.BAD_REQUEST, "password_empty", "비밀번호가 비어 있습니다"),
+    NOT_FOUND_EMAIL(HttpStatus.BAD_REQUEST, "not_found_email","존재하지 않는 이메일입니다"),
+    NOT_FOUND_NICKNAME(HttpStatus.BAD_REQUEST, "nickname_empty", "존재하지 않는 닉네임입니다"),
+    NOT_FOUND_PASSWORD(HttpStatus.BAD_REQUEST, "password_empty", "존재하지 않는 비밀번호입니다"),
 
     // Exists
-    EXISTS_EMAIL(HttpStatus.BAD_REQUEST, "empty_email","이메일이 비었습니다"),
-    EXISTS_NICKNAME(HttpStatus.BAD_REQUEST, "nickname_empty", "닉네임이 비어 있습니다"),
-    EXISTS_PASSWORD(HttpStatus.BAD_REQUEST, "password_empty", "비밀번호가 비어 있습니다"),
+    EXISTS_EMAIL(HttpStatus.BAD_REQUEST, "empty_email","이메일이 이미 존재합니다"),
+    EXISTS_NICKNAME(HttpStatus.BAD_REQUEST, "nickname_empty", "닉네임이 이미 존재합니다"),
+    EXISTS_PASSWORD(HttpStatus.BAD_REQUEST, "password_empty", "비밀번호가 이미 존재합니다"),
 
     // Invalid
-    INVALID_EMAIL(HttpStatus.BAD_REQUEST, "invalid_email", "이메일 형식이 올바르지 않습니다"),
-    INVALID_NICKNAME(HttpStatus.BAD_REQUEST, "invalid_nickname", "닉네임 형식이 올바르지 않습니다"),
-    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "invalid_password", "비밀번호 형식이 올바르지 않습니다"),
+    INVALID_EMAIL_FORMAT(HttpStatus.BAD_REQUEST, "invalid_email_format", "이메일의 형식이 유효하지 않습니다"),
+    INVALID_NICKNAME_FORMAT(HttpStatus.BAD_REQUEST, "invalid_nickname_format", "닉네임의 형식이 유효하지 않습니다"),
+    INVALID_PASSWORD_FORMAT(HttpStatus.BAD_REQUEST, "invalid_password_format", "비밀번호의 형식이 유효하지 않습니다"),
 
     ;
 
