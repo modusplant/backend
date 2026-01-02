a02421130f530a80dfc98c7d1daf1676499ade82
Jun Hee <umpa9327@gmail.com>
2026-01-02 17:57:42 +0900

    MP-257 :sparkles: Feat: GeneralErrorCode의 상수 값 변경
    - 사용자 친화적인 메시지로 변경함

--- src/main/java/kr/modusplant/shared/exception/enums/GeneralErrorCode.java
+++ src/main/java/kr/modusplant/shared/exception/enums/GeneralErrorCode.java
@@ -12,7 +12,7 @@ public enum GeneralErrorCode implements ResponseCode {
     GENERIC_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "internal_server_error", "서버에 문제가 발생했습니다"),
     MISMATCH_INPUT_TYPE(HttpStatus.BAD_REQUEST, "mismatch_input_type", "입력값의 타입이 올바르지 않습니다"),
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "invalid_input", "입력값이 유효하지 않습니다"),
-    CONSTRAINT_VIOLATION(HttpStatus.BAD_REQUEST, "constraint_violation", "데이터의 조건을 위배했습니다"),
+    CONSTRAINT_VIOLATION(HttpStatus.BAD_REQUEST, "constraint_violation", "데이터의 조건이 위배되었습니다"),
     MALFORMED_INPUT(HttpStatus.BAD_REQUEST, "malformed_input", "입력값의 형식이 올바르지 않습니다"),
     UNEXPECTED_INPUT(HttpStatus.BAD_REQUEST, "unexpected_input", "서버가 알 수 없는 입력값이 발견되었습니다"),
     INVALID_STATE(HttpStatus.CONFLICT, "invalid_state", "리소스의 상태가 유효하지 않습니다"),
