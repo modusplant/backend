92f43302b1f2d560da9fdcbc1d0752d7e6b62d03
Jun Hee <umpa9327@gmail.com>
2026-01-02 17:38:06 +0900

    MP-257 :sparkles: Feat: ExistsEntityException에 EntityName 추가
    - 현존하는 엔티티의 값만 받을 수 있도록 추가함

--- src/main/java/kr/modusplant/framework/jpa/exception/ExistsEntityException.java
+++ src/main/java/kr/modusplant/framework/jpa/exception/ExistsEntityException.java
@@ -1,11 +1,35 @@
 package kr.modusplant.framework.jpa.exception;
 
-import kr.modusplant.framework.jpa.exception.enums.EntityErrorCode;
+import kr.modusplant.framework.jpa.exception.enums.EntityName;
 import kr.modusplant.shared.exception.BusinessException;
+import kr.modusplant.shared.exception.enums.supers.ResponseCode;
 
 public class ExistsEntityException extends BusinessException {
 
-    public ExistsEntityException(EntityErrorCode responseCode) {
-        super(responseCode);
+    private final EntityName entityName;
+
+    public ExistsEntityException(ResponseCode errorCode, EntityName entityName) {
+        super(errorCode);
+        this.entityName = entityName;
+    }
+
+    public ExistsEntityException(ResponseCode errorCode, EntityName entityName, String message) {
+        super(errorCode, message);
+        this.entityName = entityName;
+    }
+
+    public ExistsEntityException(ResponseCode errorCode, EntityName entityName, String message, Throwable cause) {
+        super(errorCode, message, cause);
+        this.entityName = entityName;
+    }
+
+    public ExistsEntityException(ResponseCode errorCode, EntityName entityName, Throwable cause) {
+        super(errorCode, cause);
+        this.entityName = entityName;
+    }
+
+    @Override
+    public String getMessage() {
+        return String.format("%s [entityName: %s]", super.getMessage(), entityName);
     }
 }
