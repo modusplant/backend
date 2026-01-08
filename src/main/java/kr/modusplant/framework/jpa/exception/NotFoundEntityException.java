b3899cf60d39b46c0c6daab9bdac16584bb97885
Jun Hee <umpa9327@gmail.com>
2026-01-08 20:49:05 +0900

    MP-257 :sparkles: Feat: NotFoundEntityException에 Throwable의 생성자들 추가
    - Java의 기본적인 예외 구조와 호환이 되도록 추가함

--- src/main/java/kr/modusplant/framework/jpa/exception/NotFoundEntityException.java
+++ src/main/java/kr/modusplant/framework/jpa/exception/NotFoundEntityException.java
@@ -5,6 +5,18 @@ import kr.modusplant.shared.exception.BusinessException;
 
 public class NotFoundEntityException extends BusinessException {
 
+    public NotFoundEntityException(EntityErrorCode errorCode, String message, Throwable cause) {
+        super(errorCode, message, cause);
+    }
+
+    public NotFoundEntityException(EntityErrorCode errorCode, Throwable cause) {
+        super(errorCode, cause);
+    }
+
+    public NotFoundEntityException(EntityErrorCode errorCode, String message) {
+        super(errorCode, message);
+    }
+
     public NotFoundEntityException(EntityErrorCode errorCode) {
         super(errorCode);
     }
