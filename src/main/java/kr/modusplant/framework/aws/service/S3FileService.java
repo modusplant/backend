bb67d9965a42c5bb1dd5e031dc9f7ea39b16495a
JunHyeok <akdnjs0308@gmail.com>
2026-01-12 21:52:14 +0900

    :recycle: Refactor: S3FileService 코드 리팩토링
    - 프로퍼티에 따른 endpoint 값 조정이 Spring 빈 생성 시에 한 번만 동작하도록 함

--- src/main/java/kr/modusplant/framework/aws/service/S3FileService.java
+++ src/main/java/kr/modusplant/framework/aws/service/S3FileService.java
@@ -1,5 +1,6 @@
 package kr.modusplant.framework.aws.service;
 
+import jakarta.annotation.PostConstruct;
 import kr.modusplant.framework.aws.exception.NotFoundFileKeyOnS3Exception;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -30,6 +31,13 @@ public class S3FileService {
 
     @Value("${minio.public-endpoint:#{null}}")
     private String devPublicEndpoint;
+
+    @PostConstruct
+    public void postConstruct() {
+        if (profile.equals("dev")) {
+            endpoint = devPublicEndpoint;
+        }
+    }
 
     public void uploadFile(MultipartFile file, String fileKey) throws IOException {
         PutObjectRequest request = PutObjectRequest.builder()
@@ -66,9 +74,6 @@ public class S3FileService {
     }
 
     public String generateS3SrcUrl(String fileKey) {
-        if(profile.equals("dev")){
-            endpoint = devPublicEndpoint;
-        }
         return String.format("%s/%s/%s", endpoint, bucket, fileKey);
     }
 }
