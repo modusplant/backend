ffd5950e1b4b9bc4f91e00c864f813ce1858662c
JunHyeok <akdnjs0308@gmail.com>
2026-01-27 19:04:32 +0900

    MP-574 :recycle: Refactor: submit 메소드에 제네릭 적용
    - 제네릭 타입을 사용하는 이점을 누릴 수 있도록 함

--- src/test/java/kr/modusplant/shared/concurrency/utils/VirtualThreadUtilsTest.java
+++ src/test/java/kr/modusplant/shared/concurrency/utils/VirtualThreadUtilsTest.java
@@ -1,5 +1,7 @@
 package kr.modusplant.shared.concurrency.utils;
 
+import kr.modusplant.domains.member.common.util.domain.aggregate.MemberTestUtils;
+import kr.modusplant.domains.member.domain.aggregate.Member;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,14 +10,22 @@ import java.util.concurrent.Future;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class VirtualThreadUtilsTest {
+class VirtualThreadUtilsTest implements MemberTestUtils {
     @Test
     @DisplayName("submit으로 Future 타입 값 반환")
     void testSubmit_givenCallable_willReturnFuture() throws ExecutionException, InterruptedException {
+        // 원시형 타입
         // given & when
-        Future<?> result = VirtualThreadUtils.submit(() -> 1);
+        Future<?> primitiveResult = VirtualThreadUtils.submit(() -> 1);
 
         // then
-        assertThat(result.get()).isEqualTo(1);
+        assertThat(primitiveResult.get()).isEqualTo(1);
+
+        // 제네릭 타입
+        // given & when
+        Future<Member> memberResult = VirtualThreadUtils.submit(this::createMember);
+
+        // then
+        assertThat(memberResult.get()).isEqualTo(createMember());
     }
 }
