8ca3bb1a6daacce558a07ba1ca901ae07eda35ef
JunHyeok <akdnjs0308@gmail.com>
2026-01-28 11:05:24 +0900

    MP-574 :sparkles: Feat: Runnable을 위한 VirtualThreadUtils 메소드 추가

--- src/test/java/kr/modusplant/shared/concurrency/utils/VirtualThreadUtilsTest.java
+++ src/test/java/kr/modusplant/shared/concurrency/utils/VirtualThreadUtilsTest.java
@@ -7,10 +7,25 @@ import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class VirtualThreadUtilsTest implements MemberTestUtils {
+    @Test
+    @DisplayName("submit으로 Runnable 실행")
+    void testSubmit_givenRunnable_willExecuteRunnable() throws ExecutionException, InterruptedException {
+        // given
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
+        // when
+        Future<?> result = VirtualThreadUtils.submit(() -> atomicBoolean.set(true));
+
+        // then
+        assertThat(result.get()).isNull();
+        assertThat(atomicBoolean.get()).isTrue();
+    }
+
     @Test
     @DisplayName("submit으로 Future 타입 값 반환")
     void testSubmit_givenCallable_willReturnFuture() throws ExecutionException, InterruptedException {
@@ -27,6 +42,19 @@ class VirtualThreadUtilsTest implements MemberTestUtils {
 
         // then
         assertThat(memberResult.get()).isEqualTo(createMember());
+    }
+
+    @Test
+    @DisplayName("submitAndGet으로 Runnable 실행")
+    void testSubmitAndGet_givenRunnable_willExecuteRunnable() {
+        // given
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
+        // when
+        VirtualThreadUtils.submitAndGet(() -> atomicBoolean.set(true));
+
+        // then
+        assertThat(atomicBoolean.get()).isTrue();
     }
 
     @Test
