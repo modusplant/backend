9c66441b366f59483fd4aa4b9dfbdfe473db9893
Junhee Park <qpfm111@naver.com>
2026-02-06 19:35:50 +0900

    :truck: Rename: ExistedEntityException의 이름을 ExistsEntityException으로 변경
    - 개발자들에게 익숙한 예외 클래스인 EntityExistsException의 이름을 프로젝트의 관례에 맞게 변경하는 게 기억하기 용이하므로 변경함

--- src/main/java/kr/modusplant/framework/jpa/exception/ExistsEntityException.java
+++ src/main/java/kr/modusplant/framework/jpa/exception/ExistsEntityException.java
@@ -4,26 +4,26 @@ import kr.modusplant.framework.jpa.exception.enums.EntityName;
 import kr.modusplant.shared.exception.BusinessException;
 import kr.modusplant.shared.exception.supers.ErrorCode;
 
-public class ExistedEntityException extends BusinessException {
+public class ExistsEntityException extends BusinessException {
 
     private final EntityName entityName;
 
-    public ExistedEntityException(ErrorCode errorCode, EntityName entityName) {
+    public ExistsEntityException(ErrorCode errorCode, EntityName entityName) {
         super(errorCode);
         this.entityName = entityName;
     }
 
-    public ExistedEntityException(ErrorCode errorCode, EntityName entityName, String message) {
+    public ExistsEntityException(ErrorCode errorCode, EntityName entityName, String message) {
         super(errorCode, message);
         this.entityName = entityName;
     }
 
-    public ExistedEntityException(ErrorCode errorCode, EntityName entityName, String message, Throwable cause) {
+    public ExistsEntityException(ErrorCode errorCode, EntityName entityName, String message, Throwable cause) {
         super(errorCode, message, cause);
         this.entityName = entityName;
     }
 
-    public ExistedEntityException(ErrorCode errorCode, EntityName entityName, Throwable cause) {
+    public ExistsEntityException(ErrorCode errorCode, EntityName entityName, Throwable cause) {
         super(errorCode, cause);
         this.entityName = entityName;
     }
