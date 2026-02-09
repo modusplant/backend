df13c4d94a6c1f1a9fe3f6d9babd323af7c96244
Junhee Park <qpfm111@naver.com>
2026-02-09 15:33:35 +0900

    :sparkles: Feat: InvalidValueException에 getter 추가
    - 무효한 값을 보관하는 필드에 대한 접근성을 부여하기 위해 추가함

--- src/main/java/kr/modusplant/shared/exception/InvalidValueException.java
+++ src/main/java/kr/modusplant/shared/exception/InvalidValueException.java
@@ -1,11 +1,13 @@
 package kr.modusplant.shared.exception;
 
 import kr.modusplant.shared.exception.supers.ErrorCode;
+import lombok.Getter;
 
 /**
  * 클라이언트 요청 필드의 값이 해당 값의 형식과 맞지 않는 경우에 발생하는 예외입니다.
  * 이메일, 비밀번호, 닉네임 등의 값이 각자의 정규 표현식과 맞지 않는 경우가 해당됩니다.
  */
+@Getter
 public class InvalidValueException extends BusinessException {
 
     private final String valueName;
