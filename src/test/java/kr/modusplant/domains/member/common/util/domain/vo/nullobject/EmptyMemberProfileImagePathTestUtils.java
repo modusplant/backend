8e2d3f6d8dd5f6f6d2a0f267e65642624618c6c6
JunHyeok <akdnjs0308@gmail.com>
2026-02-23 20:19:56 +0900

    MP-265 :truck: Rename: 회원 도메인의 널 객체 명명법 변경

---
+++ src/test/java/kr/modusplant/domains/member/common/util/domain/vo/nullobject/EmptyMemberProfileImagePathTestUtils.java
@@ -0,0 +1,7 @@
+package kr.modusplant.domains.member.common.util.domain.vo.nullobject;
+
+import kr.modusplant.domains.member.domain.vo.nullobject.EmptyMemberProfileImagePath;
+
+public interface EmptyMemberProfileImagePathTestUtils {
+    EmptyMemberProfileImagePath TEST_EMPTY_MEMBER_PROFILE_IMAGE_PATH = EmptyMemberProfileImagePath.create();
+}
--- src/test/java/kr/modusplant/domains/member/domain/vo/nullobject/EmptyMemberProfileIntroductionTest.java
+++ src/test/java/kr/modusplant/domains/member/domain/vo/nullobject/EmptyMemberProfileIntroductionTest.java
@@ -1,6 +1,6 @@
 package kr.modusplant.domains.member.domain.vo.nullobject;
 
-import kr.modusplant.domains.member.common.util.domain.vo.nullobject.MemberEmptyProfileIntroductionTestUtils;
+import kr.modusplant.domains.member.common.util.domain.vo.nullobject.EmptyMemberProfileIntroductionTestUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,30 +10,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-class MemberEmptyProfileIntroductionTest implements MemberEmptyProfileIntroductionTestUtils {
+class EmptyMemberProfileIntroductionTest implements EmptyMemberProfileIntroductionTestUtils {
     @Test
     @DisplayName("create로 비어 있는 회원 프로필 소개 반환")
-    void testCreate_givenNothing_willReturnMemberEmptyProfileIntroduction() {
-        assertThat(MemberEmptyProfileIntroduction.create()).isEqualTo(MemberEmptyProfileIntroduction.create());
+    void testCreate_givenNothing_willReturnEmptyMemberProfileIntroduction() {
+        assertThat(EmptyMemberProfileIntroduction.create()).isEqualTo(EmptyMemberProfileIntroduction.create());
     }
 
     @Test
     @DisplayName("같은 객체에 대한 equals 호출")
     void useEqual_givenSameObject_willReturnTrue() {
         //noinspection EqualsWithItself
-        assertEquals(testMemberEmptyProfileIntroduction, testMemberEmptyProfileIntroduction);
+        assertEquals(TEST_EMPTY_MEMBER_PROFILE_INTRODUCTION, TEST_EMPTY_MEMBER_PROFILE_INTRODUCTION);
     }
 
     @Test
     @DisplayName("다른 클래스의 인스턴스에 대한 equals 호출")
     void useEqual_givenObjectOfDifferentClass_willReturnFalse() {
         //noinspection AssertBetweenInconvertibleTypes
-        assertNotEquals(testMemberEmptyProfileIntroduction, testMemberId);
+        assertNotEquals(TEST_EMPTY_MEMBER_PROFILE_INTRODUCTION, testMemberId);
     }
 
     @Test
     @DisplayName("MemberProfileIntroduction 인스턴스에 대한 equals 호출")
     void useEqual_givenObjectContainingDifferentProperty_willReturnFalse() {
-        assertNotEquals(testMemberEmptyProfileIntroduction, testMemberProfileIntroduction);
+        assertNotEquals(TEST_EMPTY_MEMBER_PROFILE_INTRODUCTION, testMemberProfileIntroduction);
     }
 }
