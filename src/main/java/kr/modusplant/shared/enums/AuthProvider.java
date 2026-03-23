02fdaa35b51920db1714e60e212f05b039857193
JunHyeok <akdnjs0308@gmail.com>
2026-03-23 14:49:36 +0900

    MP-650 :recycle: Refactor: AuthProvider Enum 타입 추가

--- src/main/java/kr/modusplant/shared/enums/AuthProvider.java
+++ src/main/java/kr/modusplant/shared/enums/AuthProvider.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 public enum AuthProvider {
     GOOGLE("Google"),
     KAKAO("Kakao"),
-    BASIC("Basic");
+    BASIC("Basic"),
+    BASIC_GOOGLE("Basic & Google"),
+    BASIC_KAKAO("Basic & Kakao");
 
     private final String value;
 
