b5816e26080283000e024703627468e1f950f78b
JunHyeok <akdnjs0308@gmail.com>
2026-03-24 16:14:48 +0900

    MP-645 :white_check_mark: Test: 약관 테스트 실패 문제 해소

--- src/test/java/kr/modusplant/shared/persistence/common/util/constant/SiteMemberTermConstant.java
+++ src/test/java/kr/modusplant/shared/persistence/common/util/constant/SiteMemberTermConstant.java
@@ -12,12 +12,12 @@ import static kr.modusplant.shared.util.VersionUtils.createVersion;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class SiteMemberTermConstant {
     public static final UUID MEMBER_TERM_ADMIN_UUID = MEMBER_BASIC_ADMIN_UUID;
-    public static final String MEMBER_TERM_ADMIN_AGREED_TERMS_OF_USE_VERSION = createVersion(1, 0, 0);
-    public static final String MEMBER_TERM_ADMIN_AGREED_PRIVACY_POLICY_VERSION = createVersion(2, 1, 2);
-    public static final String MEMBER_TERM_ADMIN_AGREED_AD_INFO_RECEIVING_VERSION = createVersion(3, 2, 4);
+    public static final String MEMBER_TERM_ADMIN_AGREED_TERMS_OF_USE_VERSION = createVersion(1, 0, 100);
+    public static final String MEMBER_TERM_ADMIN_AGREED_PRIVACY_POLICY_VERSION = createVersion(2, 1, 102);
+    public static final String MEMBER_TERM_ADMIN_AGREED_AD_INFO_RECEIVING_VERSION = createVersion(3, 2, 104);
 
     public static final UUID MEMBER_TERM_USER_UUID = MEMBER_BASIC_USER_UUID;
-    public static final String MEMBER_TERM_USER_AGREED_TERMS_OF_USE_VERSION = createVersion(1, 1, 0);
-    public static final String MEMBER_TERM_USER_AGREED_PRIVACY_POLICY_VERSION = createVersion(2, 2, 2);
-    public static final String MEMBER_TERM_USER_AGREED_AD_INFO_RECEIVING_VERSION = createVersion(3, 3, 4);
+    public static final String MEMBER_TERM_USER_AGREED_TERMS_OF_USE_VERSION = createVersion(1, 1, 100);
+    public static final String MEMBER_TERM_USER_AGREED_PRIVACY_POLICY_VERSION = createVersion(2, 2, 102);
+    public static final String MEMBER_TERM_USER_AGREED_AD_INFO_RECEIVING_VERSION = createVersion(3, 3, 104);
 }
