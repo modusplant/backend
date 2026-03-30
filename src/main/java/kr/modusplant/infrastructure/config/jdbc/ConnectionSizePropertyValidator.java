bc714f79cbe6828af0a3713c0fc7d149fc076db1
JunHyeok <akdnjs0308@gmail.com>
2026-03-30 11:24:36 +0900

    MP-596 :sparkles: Feat: 커넥션 개수 관련 프로퍼티 검증 기능 추가

---
+++ src/main/java/kr/modusplant/infrastructure/config/jdbc/ConnectionSizePropertyValidator.java
@@ -0,0 +1,41 @@
+package kr.modusplant.infrastructure.config.jdbc;
+
+import io.micrometer.common.lang.NonNullApi;
+import kr.modusplant.infrastructure.config.exception.ConfigurationException;
+import kr.modusplant.infrastructure.config.exception.enums.ConfigurationErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@NonNullApi
+@Component
+@RequiredArgsConstructor
+@Getter
+public class ConnectionSizePropertyValidator implements SmartInitializingSingleton {
+
+    @Value("${app.semaphore.datasource.allowed-connection-size}")
+    private int allowedConnectionSize;
+
+    @Value("${spring.datasource.hikari.maximum-pool-size}")
+    private int maxPoolSize;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void afterSingletonsInstantiated() {
+        if (this.getAllowedConnectionSize() >= this.getMaxPoolSize()) {
+            throw new ConfigurationException(ConfigurationErrorCode.INCORRECT_RELATIONSHIP_BETWEEN_CONNECTION_SIZE,
+                    new String[]{"allowedConnectionSize", "maxPoolSize"});
+        }
+
+        Integer maxConnections = jdbcTemplate.queryForObject("SHOW max_connections;", Integer.class);
+
+        if (maxPoolSize >= maxConnections) {
+            throw new ConfigurationException(ConfigurationErrorCode.INCORRECT_RELATIONSHIP_BETWEEN_CONNECTION_SIZE,
+                    new String[]{"maxPoolSize", "maxConnections"});
+        }
+    }
+}
