9356c45e90802180519f02512ad5ed8e6134a589
Junhee Park <qpfm111@naver.com>
2026-03-30 20:35:41 +0900

    MP-533 :sparkles: Feat: 게시글 식별자로 댓글을 가져오는 API의 응답에 updatedAt 추가
    - 게시글 상세 페이지에 있는 댓글에 수정 일자를 표시해야 하므로 추가함

--- src/main/resources/db/migration/schema/V0.8.2__Add_updatedat_comment_table.sql
+++ src/main/resources/db/migration/schema/V0.8.2__Add_updatedat_comment_table.sql
@@ -1,11 +1,8 @@
--- Add updated_at column to comm_comment table
--- Existing rows will be filled with created_at value
-
 ALTER TABLE public.comm_comment
-    ADD COLUMN updated_at timestamp without time zone NOT NULL DEFAULT NOW();
+    ADD COLUMN updated_at timestamp without time zone;
 
 UPDATE public.comm_comment
     SET updated_at = created_at;
 
 ALTER TABLE public.comm_comment
-    ALTER COLUMN updated_at DROP DEFAULT;
+    ALTER COLUMN updated_at SET NOT NULL;
