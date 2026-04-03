b30a5b41919c230a5e1ce509fd63bc6dcea119f2
JunHyeok <akdnjs0308@gmail.com>
2026-04-03 20:20:19 +0900

    MP-675 :boom: BREAKING!: 검색 로직 개발
    - Feat: 애플리케이션 프로퍼티에 connection-init-sql 추가

---
+++ src/test/java/kr/modusplant/domains/post/common/constant/PostDoubleConstant.java
@@ -0,0 +1,11 @@
+package kr.modusplant.domains.post.common.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class PostDoubleConstant {
+    public static final Double TEST_POST_MAX_WORD_SIMILARITY_1 = 1.0;
+    public static final Double TEST_POST_MAX_WORD_SIMILARITY_0_8 = 0.8;
+    public static final Double TEST_POST_MAX_WORD_SIMILARITY_0_6 = 0.6;
+}
