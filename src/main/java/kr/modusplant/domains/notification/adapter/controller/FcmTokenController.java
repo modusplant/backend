f532fefee42923a55f55eeb5aaf5b050d29a156d
songu1 <syj0002012@naver.com>
2026-04-23 22:33:43 +0900

    MP-619 :sparkles: Feat: FcmToken 등록 API의 RestController 및 Controller 구현
    - Fcm 토큰 등록 API의 RestController 및 request 구현
    - Fcm 토큰 등록 controller 구현

---
+++ src/main/java/kr/modusplant/domains/notification/adapter/controller/FcmTokenController.java
@@ -0,0 +1,24 @@
+package kr.modusplant.domains.notification.adapter.controller;
+
+import kr.modusplant.domains.account.shared.kernel.AccountId;
+import kr.modusplant.domains.notification.usecase.port.repository.FcmTokenRepository;
+import kr.modusplant.domains.notification.usecase.request.FcmTokenRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class FcmTokenController {
+
+    private final FcmTokenRepository fcmTokenRepository;
+
+    @Transactional
+    public void register(FcmTokenRequest fcmTokenRequest, UUID currentMemberUuid) {
+        fcmTokenRepository.saveOrUpdate(fcmTokenRequest.token(), AccountId.fromUuid(currentMemberUuid), fcmTokenRequest.platform());
+    }
+
+}
