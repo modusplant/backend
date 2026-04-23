ecd6c3d5c51ff5e1b7227070311c152cd924a54a
songu1 <syj0002012@naver.com>
2026-04-23 21:08:12 +0900

    MP-619 :sparkles: Feat: 알림 이벤트 객체 구현
    - 게시글/댓글 알림을 위한 알림 이벤트 객체 구현

---
+++ src/main/java/kr/modusplant/shared/event/CommentNotificationEvent.java
@@ -0,0 +1,43 @@
+package kr.modusplant.shared.event;
+
+import kr.modusplant.framework.jpa.exception.enums.EntityErrorCode;
+import kr.modusplant.shared.enums.NotificationActionType;
+import kr.modusplant.shared.exception.InvalidValueException;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class CommentNotificationEvent {
+    private final UUID actorId;
+    private final String postUlid;
+    private final String commentPath;
+    private final String contentPreview;
+    private final NotificationActionType action;
+
+    public static CommentNotificationEvent create(UUID actorId, String postUlid, String commentPath, String contentPreview) {
+        validate(actorId, postUlid, commentPath);
+        return new CommentNotificationEvent(actorId, postUlid, commentPath, contentPreview, getActionType(commentPath));
+    }
+
+    private static void validate(UUID actorId, String postUlid, String commentPath) {
+        if (actorId == null) {
+            throw new InvalidValueException(EntityErrorCode.NOT_FOUND_ACTOR, "actorId");
+        }
+        if (postUlid == null || postUlid.isBlank()) {
+            throw new InvalidValueException(EntityErrorCode.NOT_FOUND_POST,"postUlid");
+        }
+        if (commentPath == null || commentPath.isBlank()) {
+            throw new InvalidValueException(EntityErrorCode.NOT_FOUND_COMMENT, "commentPath");
+        }
+    }
+
+    private static NotificationActionType getActionType(String commentPath) {
+        return commentPath.contains(".")
+                ? NotificationActionType.COMMENT_REPLY_ADDED
+                : NotificationActionType.COMMENT_ADDED;
+    }
+}
