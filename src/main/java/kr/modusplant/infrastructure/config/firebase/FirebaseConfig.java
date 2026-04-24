32058c599974ae3462ea757b39a1c4c8c6de5299
songu1 <syj0002012@naver.com>
2026-04-24 21:20:26 +0900

    MP-619 :sparkles: Feat: FirebaseConfig 추가
    - FCM 구현을 위한 FirebaseConfig 추가

---
+++ src/main/java/kr/modusplant/infrastructure/config/firebase/FirebaseConfig.java
@@ -0,0 +1,39 @@
+package kr.modusplant.infrastructure.config.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${firebase.credentials.path}")
+    private String credentialsPath;
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        if (!FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.getInstance();
+        }
+
+        try(InputStream is = new FileInputStream(credentialsPath)) {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(is))
+                    .build();
+            return FirebaseApp.initializeApp(options);
+        }
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
