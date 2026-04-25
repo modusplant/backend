ca7e3b73fd5c6e4602bb72ed14451a16cfe3f4cb
Junhee Park <qpfm111@naver.com>
2026-04-25 16:34:42 +0900

    :sparkles: Feat: CommentController에서 구현체 대신 인터페이스를 사용하도록 수정
    - 클래스 간 느슨한 커플링 유지

--- src/main/java/kr/modusplant/domains/comment/adapter/controller/CommentController.java
+++ src/main/java/kr/modusplant/domains/comment/adapter/controller/CommentController.java
@@ -11,9 +11,9 @@ import kr.modusplant.domains.comment.domain.vo.CommentPath;
 import kr.modusplant.domains.comment.domain.vo.PostId;
 import kr.modusplant.domains.comment.framework.in.web.cache.CommentCacheService;
 import kr.modusplant.domains.comment.framework.in.web.cache.model.CommentCacheData;
-import kr.modusplant.domains.comment.framework.out.persistence.jooq.CommentJooqRepository;
-import kr.modusplant.domains.comment.framework.out.persistence.jpa.repository.CommentRepositoryJpaAdapter;
 import kr.modusplant.domains.comment.usecase.model.CommentOfAuthorPageModel;
+import kr.modusplant.domains.comment.usecase.port.repository.CommentReadRepository;
+import kr.modusplant.domains.comment.usecase.port.repository.CommentWriteRepository;
 import kr.modusplant.domains.comment.usecase.request.CommentRegisterRequest;
 import kr.modusplant.domains.comment.usecase.request.CommentUpdateRequest;
 import kr.modusplant.domains.comment.usecase.response.CommentOfPostResponse;
@@ -41,8 +41,8 @@ import java.util.UUID;
 public class CommentController {
 
     private final CommentMapperImpl mapper;
-    private final CommentJooqRepository jooqRepository;
-    private final CommentRepositoryJpaAdapter jpaAdapter;
+    private final CommentReadRepository readRepository;
+    private final CommentWriteRepository writeRepository;
     private final CommPostJpaRepository postJpaRepository;
     private final SiteMemberJpaRepository memberJpaRepository;
     private final SwearService swearService;
@@ -65,7 +65,7 @@ public class CommentController {
             throw new NotFoundEntityException(EntityErrorCode.NOT_FOUND_POST, "post");
         }
 
-        return jooqRepository.findByPost(PostId.create(postUlid), Author.createNullable(currentMemberUuid))
+        return readRepository.findByPost(PostId.create(postUlid), Author.createNullable(currentMemberUuid))
                 .stream().map(mapper::toCommentOfPostResponse)
                 .toList();
     }
@@ -75,7 +75,7 @@ public class CommentController {
         if(!memberJpaRepository.existsById(memberUuid)) {
             throw new NotFoundEntityException(EntityErrorCode.NOT_FOUND_MEMBER, "member");
         }
-        PageImpl<CommentOfAuthorPageModel> result = jooqRepository.findByAuthor(Author.create(memberUuid), pageable);
+        PageImpl<CommentOfAuthorPageModel> result = readRepository.findByAuthor(Author.create(memberUuid), pageable);
 
         CommentPageResponse<CommentOfAuthorPageModel> response =
                 new CommentPageResponse<>(result.getContent(), result.getNumber(),
@@ -89,7 +89,7 @@ public class CommentController {
 
     @Transactional
     public void register(CommentRegisterRequest request, UUID currentMemberUuid) {
-        if(jooqRepository.existsByPostAndPath(PostId.create(request.postId()), CommentPath.create(request.path()))) {
+        if(readRepository.existsByPostAndPath(PostId.create(request.postId()), CommentPath.create(request.path()))) {
             throw new InvalidValueException(CommentErrorCode.EXIST_COMMENT);
         }
         checkPathCondition(request.postId(), request.path());
@@ -99,12 +99,12 @@ public class CommentController {
                 CommentPath.create(request.path()),
                 Author.create(currentMemberUuid),
                 CommentContent.create(swearService.filterSwear(request.content())));
-        jpaAdapter.save(comment);
+        writeRepository.save(comment);
     }
 
     @Transactional
     public void update(CommentUpdateRequest request) {
-        if(!jooqRepository.existsByPostAndPath(PostId.create(request.postId()), CommentPath.create(request.path()))) {
+        if(!readRepository.existsByPostAndPath(PostId.create(request.postId()), CommentPath.create(request.path()))) {
             throw new NotFoundEntityException(EntityErrorCode.NOT_FOUND_COMMENT, TableName.COMM_COMMENT);
         }
 
@@ -113,12 +113,12 @@ public class CommentController {
                 .path(request.path())
                 .build();
 
-        jpaAdapter.update(id, CommentContent.create(request.content()));
+        writeRepository.update(id, CommentContent.create(request.content()));
     }
 
     @Transactional
     public void delete(String postUlid, String commentPath) {
-        jpaAdapter.setCommentAsDeleted(CommCommentId.builder()
+        writeRepository.setCommentAsDeleted(CommCommentId.builder()
                 .post(postUlid)
                 .path(commentPath)
                 .build());
@@ -134,7 +134,7 @@ public class CommentController {
             // 댓글 경로가 1로 끝나는 경우, 마지막 . 이후의 값을 제거한 경로에 해당하는 댓글이 있어야 댓글 등록 가능
             // 예시: 경로가 1.2.1인 댓글을 등록하려면 경로가 1.2인 댓글이 있어야 함
             String parentCommentPath = path.substring(0, lastDotIndex);
-            if (lastNumOfPath.equals("1") && !jooqRepository.existsByPostAndPath(commentPost, CommentPath.create(parentCommentPath))) {
+            if (lastNumOfPath.equals("1") && !readRepository.existsByPostAndPath(commentPost, CommentPath.create(parentCommentPath))) {
                 throw new InvalidValueException(CommentErrorCode.NOT_EXIST_PARENT_COMMENT);
             }
 
@@ -142,20 +142,20 @@ public class CommentController {
             // 예시: 경로가 1.5.3인 댓글을 등록하려면 경로가 1.5.2인 댓글이 있어야 함
             String siblingPathLastNum = String.valueOf(Integer.parseInt(lastNumOfPath) - 1);
             String siblingCommentPath = path.substring(0, lastDotIndex + 1).concat(siblingPathLastNum);
-            if (1 < Integer.parseInt(lastNumOfPath) && !jooqRepository.existsByPostAndPath(commentPost, CommentPath.create(siblingCommentPath))) {
+            if (1 < Integer.parseInt(lastNumOfPath) && !readRepository.existsByPostAndPath(commentPost, CommentPath.create(siblingCommentPath))) {
                 throw new InvalidValueException(CommentErrorCode.NOT_EXIST_SIBLING_COMMENT);
             }
             
         } else {
             // 댓글 경로가 1인 경우 게시글에 댓글이 없어야 등록 가능
             if(path.equals("1")) {
-                if (!(jooqRepository.countPostComment(commentPost) == 0)) {
+                if (!(readRepository.countPostComment(commentPost) == 0)) {
                     throw new InvalidValueException(CommentErrorCode.EXIST_POST_COMMENT);
                 }
             } else {
                 // 댓글 경로에 .가 없고 1이 아닌 경우, 형제 댓글이 있어야 등록 가능
                 String siblingCommentPath = String.valueOf(Integer.parseInt(path) - 1);
-                if (!(jooqRepository.existsByPostAndPath(commentPost, CommentPath.create(siblingCommentPath)))) {
+                if (!(readRepository.existsByPostAndPath(commentPost, CommentPath.create(siblingCommentPath)))) {
                     throw new InvalidValueException(CommentErrorCode.NOT_EXIST_SIBLING_COMMENT);
                 }
             }
