46a5b8759214f26ec1ae57c50eba05cb54686613
Reality-out <akdnjs0308@gmail.com>
2026-05-22 11:40:49 +0900

    MP-606 :sparkles: Feat: 메서드 어노테이션 수준 보안 기능 도입

--- src/main/java/kr/modusplant/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/kr/modusplant/infrastructure/security/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -40,6 +41,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
