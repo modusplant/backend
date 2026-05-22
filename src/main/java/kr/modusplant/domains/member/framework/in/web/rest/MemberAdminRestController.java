d7b33e7296ca4dcda91dad2de1c11f79a6c82a4a
Reality-out <akdnjs0308@gmail.com>
2026-05-22 11:40:49 +0900

    MP-606 :sparkles: Feat: 메서드 어노테이션 수준 보안 기능 도입

--- src/main/java/kr/modusplant/domains/member/framework/in/web/rest/MemberAdminRestController.java
+++ src/main/java/kr/modusplant/domains/member/framework/in/web/rest/MemberAdminRestController.java
@@ -13,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import static kr.modusplant.shared.constant.Regex.REGEX_ULID;
 @RequiredArgsConstructor
 @Validated
 @Slf4j
+@PreAuthorize("hasAuthority('ADMIN')")
 public class MemberAdminRestController {
     private final MemberAdminController memberAdminController;
 
