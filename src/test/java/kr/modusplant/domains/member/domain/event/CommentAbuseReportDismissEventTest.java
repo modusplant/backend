67cd882675c8f6b413f02fd04edb40005aa16c56
Reality-out <akdnjs0308@gmail.com>
2026-06-05 11:33:10 +0900

    :recycle: Refactor: 컨트롤러에 이벤트 발생 로직 추가

--- src/test/java/kr/modusplant/domains/member/domain/event/CommentAbuseReportDismissEventTest.java
+++ src/test/java/kr/modusplant/domains/member/domain/event/CommentAbuseReportDismissEventTest.java
@@ -6,14 +6,14 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static kr.modusplant.domains.comment.common.constant.CommentConstant.TEST_COMMENT_PATH;
-import static kr.modusplant.domains.member.common.util.domain.event.CommentAbuseReportApproveEventTestUtils.testCommentAbuseReportApproveEvent;
+import static kr.modusplant.domains.member.common.util.domain.event.CommentAbuseReportDismissEventTestUtils.testCommentAbuseReportDismissEvent;
 import static kr.modusplant.domains.post.common.constant.PostConstant.TEST_POST_ULID;
 import static kr.modusplant.shared.framework.jpa.exception.enums.EntityErrorCode.NOT_FOUND_COMMENT;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class CommentAbuseReportApproveEventTest {
+class CommentAbuseReportDismissEventTest {
 
     @Nested
     @DisplayName("create 테스트")
@@ -22,7 +22,7 @@ class CommentAbuseReportApproveEventTest {
         @Test
         @DisplayName("유효한 파라미터로 객체 생성 성공")
         void testCreate_givenValidParameters_willReturnEvent() {
-            assertNotNull(testCommentAbuseReportApproveEvent);
+            assertNotNull(testCommentAbuseReportDismissEvent);
         }
 
         @Test
@@ -30,7 +30,7 @@ class CommentAbuseReportApproveEventTest {
         void testCreate_givenBlankPostUlid_willThrowException() {
             // given & when
             InvalidValueException exception = assertThrows(InvalidValueException.class, () ->
-                    CommentAbuseReportApproveEvent.create("", TEST_COMMENT_PATH));
+                    CommentAbuseReportDismissEvent.create("", TEST_COMMENT_PATH));
 
             // then
             assertThat(exception.getErrorCode()).isEqualTo(NOT_FOUND_COMMENT);
@@ -41,7 +41,7 @@ class CommentAbuseReportApproveEventTest {
         void testCreate_givenBlankPath_willThrowException() {
             // given & when
             InvalidValueException exception = assertThrows(InvalidValueException.class, () ->
-                    CommentAbuseReportApproveEvent.create(TEST_POST_ULID, " "));
+                    CommentAbuseReportDismissEvent.create(TEST_POST_ULID, " "));
 
             // then
             assertThat(exception.getErrorCode()).isEqualTo(NOT_FOUND_COMMENT);
---
+++ src/test/java/kr/modusplant/domains/member/domain/event/PostAbuseReportDismissEventTest.java
@@ -0,0 +1,37 @@
+package kr.modusplant.domains.member.domain.event;
+
+import kr.modusplant.shared.exception.InvalidValueException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static kr.modusplant.domains.member.common.util.domain.event.PostAbuseReportDismissEventTestUtils.testPostAbuseReportDismissEvent;
+import static kr.modusplant.shared.framework.jpa.exception.enums.EntityErrorCode.NOT_FOUND_POST;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PostAbuseReportDismissEventTest {
+
+    @Nested
+    @DisplayName("create 테스트")
+    class CreateTest {
+
+        @Test
+        @DisplayName("유효한 파라미터로 객체 생성 성공")
+        void testCreate_givenValidParameters_willReturnEvent() {
+            assertNotNull(testPostAbuseReportDismissEvent);
+        }
+
+        @Test
+        @DisplayName("postUlid가 비어 있을 때 오류 발생")
+        void testCreate_givenBlankPostUlid_willThrowException() {
+            // given & when
+            InvalidValueException exception = assertThrows(InvalidValueException.class, () ->
+                    PostAbuseReportDismissEvent.create(""));
+
+            // then
+            assertThat(exception.getErrorCode()).isEqualTo(NOT_FOUND_POST);
+        }
+    }
+}
