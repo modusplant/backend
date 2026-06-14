c136ae14f9d7addc40c3983e1dbee6fa638d7986
Reality-out <akdnjs0308@gmail.com>
2026-06-15 01:16:00 +0900

    MP-760 :bug: Fix: Flyway 빌드 오류 해소

--- src/main/resources/db/migration/schema/V5.0.3__Alter_add_created_at_and_updated_at_to_plant.sql
+++ src/main/resources/db/migration/schema/V5.0.3__Alter_add_created_at_and_updated_at_to_plant.sql
@@ -1,8 +1,5 @@
 -- 현재 시각을 기준으로 created_at과 updated_at 컬럼 데이터에 디폴트 값 입력
+-- V5.0.1 Flyway 마이그레이션 파일이 무사히 동작하기 위해서는 디폴트 값을 제거해서는 안 된다.
 ALTER TABLE public.plant
 ADD COLUMN created_at timestamp without time zone NOT NULL DEFAULT NOW(),
 ADD COLUMN updated_at timestamp without time zone NOT NULL DEFAULT NOW();
-
--- 디폴트 설정 삭제
-ALTER TABLE public.plant ALTER COLUMN created_at DROP DEFAULT;
-ALTER TABLE public.plant ALTER COLUMN updated_at DROP DEFAULT;
