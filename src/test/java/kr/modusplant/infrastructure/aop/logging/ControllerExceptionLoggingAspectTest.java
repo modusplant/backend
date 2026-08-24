93cf2c578bffe1e3a9af701681b780d3f8c181d7
Reality-out <akdnjs0308@gmail.com>
2026-08-25 00:34:08 +0900

    :white_check_mark: Test: 권한 관련 테스트 에러 해소

--- src/test/java/kr/modusplant/infrastructure/aop/logging/ControllerExceptionLoggingAspectTest.java
+++ src/test/java/kr/modusplant/infrastructure/aop/logging/ControllerExceptionLoggingAspectTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -29,7 +30,7 @@ public class ControllerExceptionLoggingAspectTest {
         LogCaptor logCaptor = LogCaptor.forClass(ControllerExceptionLoggingAspect.class);
         logCaptor.setLogLevelToInfo();
         mockMvc.perform(get("/api/admin/v1/monitor/monitor-error-controller")
-                        .with(user("admin").roles("ADMIN")))
+                        .with(user("admin").authorities(new SimpleGrantedAuthority("ADMIN"))))
                 .andExpect(status().is5xxServerError());
 
         // then
