dffcce37636f7943facc67f76559ccbeffb38307
Reality-out <akdnjs0308@gmail.com>
2026-09-14 19:07:19 +0900

    SCRUM-5 :recycle: Refactor: 더 이상 유용하지 않은 version 매개변수의 네이밍 및 타입 변경

--- src/test/java/kr/modusplant/domains/member/adapter/mapper/MemberProfileMapperImplTest.java
+++ src/test/java/kr/modusplant/domains/member/adapter/mapper/MemberProfileMapperImplTest.java
@@ -1,9 +1,7 @@
 package kr.modusplant.domains.member.adapter.mapper;
 
 import kr.modusplant.domains.member.common.util.domain.aggregate.MemberProfileTestUtils;
-import kr.modusplant.domains.member.domain.exception.enums.MemberErrorCode;
 import kr.modusplant.domains.member.usecase.port.mapper.MemberProfileMapper;
-import kr.modusplant.shared.exception.InvalidValueException;
 import kr.modusplant.shared.framework.aws.service.AmazonS3Service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,7 +16,6 @@ import static kr.modusplant.domains.member.common.util.usecase.response.MemberPr
 import static kr.modusplant.domains.member.common.util.usecase.response.MemberProfileResponseTestUtils.testMemberProfileResponseWithNullImagePath;
 import static kr.modusplant.domains.member.common.util.usecase.response.MemberProfileResponseTestUtils.testMemberProfileResponseWithNullImageUrl;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.never;
@@ -29,51 +26,40 @@ class MemberProfileMapperImplTest implements MemberProfileTestUtils {
     private final MemberProfileMapper memberProfileMapper = new MemberProfileMapperImpl(amazonS3Service);
 
     @Test
-    @DisplayName("버전 1과 이미지 경로가 있을 때 toMemberProfileResponse로 응답 반환")
-    void testToMemberProfileResponse_givenVersion1AndImagePath_willReturnResponse() {
+    @DisplayName("저장소 URL 반환이 필요하고 이미지 경로가 있을 때 toMemberProfileResponse로 응답 반환")
+    void testToMemberProfileResponse_givenNeedsToReturnStorageUrlTrueAndImagePath_willReturnResponse() {
         // given
         given(amazonS3Service.generateS3SrcUrl(any())).willReturn(MEMBER_PROFILE_BASIC_USER_IMAGE_URL);
 
         // when & then
-        assertThat(memberProfileMapper.toMemberProfileResponse(createMemberProfile(), 1)).isEqualTo(testMemberProfileResponseWithImageUrlV1);
+        assertThat(memberProfileMapper.toMemberProfileResponse(createMemberProfile(), true)).isEqualTo(testMemberProfileResponseWithImageUrlV1);
     }
 
     @Test
-    @DisplayName("버전 1과 이미지 경로가 없을 때 toMemberProfileResponse로 응답 반환")
-    void testToMemberProfileResponse_givenVersion1AndNoImagePath_willReturnResponse() {
+    @DisplayName("저장소 URL 반환이 필요하고 이미지 경로가 없을 때 toMemberProfileResponse로 응답 반환")
+    void testToMemberProfileResponse_givenNeedsToReturnStorageUrlTrueAndNoImagePath_willReturnResponse() {
         // when
-        assertThat(memberProfileMapper.toMemberProfileResponse(createMemberProfileWithoutImage(), 1)).isEqualTo(testMemberProfileResponseWithNullImageUrl);
+        assertThat(memberProfileMapper.toMemberProfileResponse(createMemberProfileWithoutImage(), true)).isEqualTo(testMemberProfileResponseWithNullImageUrl);
 
         // then
         verify(amazonS3Service, never()).generateS3SrcUrl(any());
     }
 
     @Test
-    @DisplayName("버전 3과 이미지 경로가 있을 때 toMemberProfileResponse로 응답 반환")
-    void testToMemberProfileResponse_givenVersion3AndImagePath_willReturnResponse() {
+    @DisplayName("저장소 URL 반환이 필요하지 않고 이미지 경로가 있을 때 toMemberProfileResponse로 응답 반환")
+    void testToMemberProfileResponse_givenNeedsToReturnStorageUrlFalseAndImagePath_willReturnResponse() {
         // when
-        assertThat(memberProfileMapper.toMemberProfileResponse(createMemberProfile(), 3)).isEqualTo(testMemberProfileResponseWithImagePathV3);
+        assertThat(memberProfileMapper.toMemberProfileResponse(createMemberProfile(), false)).isEqualTo(testMemberProfileResponseWithImagePathV3);
 
         // then
         verify(amazonS3Service, never()).generateS3SrcUrl(any());
     }
 
     @Test
-    @DisplayName("버전 3과 이미지 경로가 없을 때 toMemberProfileResponse로 응답 반환")
-    void testToMemberProfileResponse_givenVersion3AndNoImagePath_willReturnResponse() {
+    @DisplayName("저장소 URL 반환이 필요하지 않고 이미지 경로가 없을 때 toMemberProfileResponse로 응답 반환")
+    void testToMemberProfileResponse_givenNeedsToReturnStorageUrlFalseAndNoImagePath_willReturnResponse() {
         // when & then
-        assertThat(memberProfileMapper.toMemberProfileResponse(createMemberProfileWithoutImage(), 3)).isEqualTo(testMemberProfileResponseWithNullImagePath);
-    }
-
-    @Test
-    @DisplayName("올바르지 않은 버전으로 toMemberProfileResponse를 호출하여 오류 발생")
-    void testToMemberProfileResponse_givenInvalidVersion_willThrowException() {
-        // when
-        InvalidValueException exception = assertThrows(
-                InvalidValueException.class, () -> memberProfileMapper.toMemberProfileResponse(createMemberProfile(), 4));
-
-        // then
-        assertThat(exception.getErrorCode()).isEqualTo(MemberErrorCode.INVALID_MEMBER_PROFILE_OVERRIDE_VERSION);
+        assertThat(memberProfileMapper.toMemberProfileResponse(createMemberProfileWithoutImage(), false)).isEqualTo(testMemberProfileResponseWithNullImagePath);
     }
 
     @Test
