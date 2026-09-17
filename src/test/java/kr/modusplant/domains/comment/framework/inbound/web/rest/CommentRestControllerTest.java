7547e4b853fd58fab219282adfafb89fcea1e38c
Reality-out <akdnjs0308@gmail.com>
2026-09-17 11:59:48 +0900

    SCRUM-6 :white_check_mark: Test: 댓글 제거 API 로직 보강에 따른 테스트 코드 작성

--- src/test/java/kr/modusplant/domains/comment/framework/inbound/web/rest/CommentRestControllerTest.java
+++ src/test/java/kr/modusplant/domains/comment/framework/inbound/web/rest/CommentRestControllerTest.java
@@ -199,15 +199,16 @@ public class CommentRestControllerTest implements PostIdTestUtils,
 
     @Test
     @DisplayName("유효한 삭제 요청으로 controller.delete 호출 후 200 반환")
-    public void testDeleteComment_Comment_givenValidPathAndUlid_willReturnResponseEntity() {
+    public void testDeleteComment_givenValidPathAndUlid_willReturnResponseEntity() {
         // given
-        doNothing().when(controller).delete(TEST_POST_ULID, "1");
+        DefaultUserDetails userDetails = DefaultUserDetails.builder().uuid(MEMBER_BASIC_USER_UUID).build();
+        doNothing().when(controller).delete(TEST_POST_ULID, "1", MEMBER_BASIC_USER_UUID);
 
         // when
-        ResponseEntity<DataResponse<Void>> result = restController.deleteComment(TEST_POST_ULID, "1");
+        ResponseEntity<DataResponse<Void>> result = restController.deleteComment(userDetails, TEST_POST_ULID, "1");
 
         // then
         assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
-        then(controller).should(times(1)).delete(TEST_POST_ULID, "1");
+        then(controller).should(times(1)).delete(TEST_POST_ULID, "1", MEMBER_BASIC_USER_UUID);
     }
 }
