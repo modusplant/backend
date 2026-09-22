c592357446d1cf003650a79faa2cde203ba1175d
Reality-out <akdnjs0308@gmail.com>
2026-09-23 01:23:52 +0900

    SCRUM-7 :white_check_mark: Test: 비어 있는 테스트 추가

--- src/test/java/kr/modusplant/domains/member/adapter/translator/MemberSocialTranslatorTest.java
+++ src/test/java/kr/modusplant/domains/member/adapter/translator/MemberSocialTranslatorTest.java
@@ -7,6 +7,8 @@ import kr.modusplant.domains.account.social.usecase.record.SocialUserInfo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 
 import static kr.modusplant.domains.account.social.common.constant.SocialStringConstant.*;
 import static kr.modusplant.domains.account.social.domain.exception.enums.SocialIdentityErrorCode.UNSUPPORTED_SOCIAL_PROVIDER;
@@ -21,7 +23,8 @@ import static org.mockito.Mockito.verify;
 
 class MemberSocialTranslatorTest {
     private final SocialIdentityLinkController socialIdentityLinkController = Mockito.mock(SocialIdentityLinkController.class);
-    private final MemberSocialTranslator memberSocialTranslator = new MemberSocialTranslator(socialIdentityLinkController);
+    private final Environment environment = Mockito.mock(Environment.class);
+    private final MemberSocialTranslator memberSocialTranslator = new MemberSocialTranslator(socialIdentityLinkController, environment);
 
     @Test
     @DisplayName("Kakao 소셜 제공자로 getSocialAccessToken으로 소셜 접근 토큰 반환")
@@ -99,5 +102,39 @@ class MemberSocialTranslatorTest {
 
         // then
         assertThat(unsupportedSocialProviderException.getErrorCode()).isEqualTo(UNSUPPORTED_SOCIAL_PROVIDER);
+    }
+
+    @Test
+    @DisplayName("local 프로필일 때 initIsLocal으로 활동 수행")
+    void testInitIsLocal_givenLocalProfile_willProcessAction() {
+        // given
+        given(environment.acceptsProfiles(any(Profiles.class))).willReturn(true);
+        SocialUserInfo userInfo = Mockito.mock(SocialUserInfo.class);
+        given(socialIdentityLinkController.issueSocialToken(SocialProvider.KAKAO, TEST_SOCIAL_KAKAO_CODE, true)).willReturn(userInfo);
+        given(userInfo.socialAccessToken()).willReturn(TEST_SOCIAL_KAKAO_SOCIAL_ACCESS_TOKEN);
+
+        // when
+        memberSocialTranslator.initIsLocal();
+        String socialAccessToken = memberSocialTranslator.getSocialAccessToken(TEST_SOCIAL_KAKAO_CODE, SocialProvider.KAKAO.getValue());
+
+        // then
+        assertThat(socialAccessToken).isEqualTo(TEST_SOCIAL_KAKAO_SOCIAL_ACCESS_TOKEN);
+    }
+
+    @Test
+    @DisplayName("local이 아닌 프로필일 때 initIsLocal으로 활동 수행")
+    void testInitIsLocal_givenNotLocalProfile_willProcessAction() {
+        // given
+        given(environment.acceptsProfiles(any(Profiles.class))).willReturn(false);
+        SocialUserInfo userInfo = Mockito.mock(SocialUserInfo.class);
+        given(socialIdentityLinkController.issueSocialToken(SocialProvider.KAKAO, TEST_SOCIAL_KAKAO_CODE, false)).willReturn(userInfo);
+        given(userInfo.socialAccessToken()).willReturn(TEST_SOCIAL_KAKAO_SOCIAL_ACCESS_TOKEN);
+
+        // when
+        memberSocialTranslator.initIsLocal();
+        String socialAccessToken = memberSocialTranslator.getSocialAccessToken(TEST_SOCIAL_KAKAO_CODE, SocialProvider.KAKAO.getValue());
+
+        // then
+        assertThat(socialAccessToken).isEqualTo(TEST_SOCIAL_KAKAO_SOCIAL_ACCESS_TOKEN);
     }
 }
